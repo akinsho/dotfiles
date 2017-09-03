ca8b3603c655bcaec5aeb62eb93a99feb5e5616b
Akin909 <akin.sowemimo@gmail.com>
2017-09-03 18:09:36 +0100

    Update install script Remove redundant function

--- install.sh
+++ install.sh
@@ -24,13 +24,15 @@ packages=(
 
 for i in "${packages[@]}"
 do
-  brew install $i
+  "brew install $i"
   echo "---------------------------------------------------------"
 done
 
 echo "installing RCM, for dotfiles management"
 brew tap thoughtbot/formulae
 brew install rcm
+echo "Installing brew bundle"
+brew tap Homebrew/bundle
 echo "---------------------------------------------------------"
 
 localGit="/usr/local/bin/git"
@@ -42,16 +44,16 @@ else
 fi
 # Okay so everything should be good
 # Fingers cross at least
-# Now lets clone my dotfiles repo into .dotfiles/
+# Now lets clone my dotfiles repo into ~/Dotfiles/
 echo "---------------------------------------------------------"
 
 echo "Cloning Akin's dotfiles into Dotfiles"
 git clone https://github.com/Akin909/Dotfiles.git ~/Dotfiles
-
-cd Dotfiles
+export DOTFILES="$HOME/Dotfiles"
+cd Dotfiles || "Didn't cd into dotfiles this will be bad :("
 git submodule update --init --recursive
 
-cd $HOME
+"cd $HOME"
 echo "running RCM's rcup command"
 echo "This is symlink the rc files in Dotfiles"
 echo "with the rc files in $HOME"
@@ -62,18 +64,22 @@ rcup
 echo "---------------------------------------------------------"
 
 echo "Changing to zsh"
-chsh -s $(which zsh)
+"chsh -s $(which zsh)"
 
 echo "You'll need to log out for this to take effect"
 echo "---------------------------------------------------------"
 
-# echo "running oxs defaults"
-# ~./osx.sh
+echo "running macos defaults"
+"cd $DOTFILES || echo 'Oh god didn't cd exiting || exit"
+./configs/macosdefaults.sh
+"cd $DOTFILES/configs/homebrew/ || echo 'Could get into Homebrew subdir'"
+brew bundle
+echo "Installing Homebrew apps from brew file"
 
 echo 'done'
 echo "---------------------------------------------------------"
 echo "All done!"
-echo "and change your terminal font to source code pro"
+echo "and change your terminal font to Operator mono book or lig"
 echo "Cheers"
 echo "---------------------------------------------------------"
 
