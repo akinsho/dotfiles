ac1d018ecb1abb8a569ede941c063d9d376e798c
Akin909 <akin.sowemimo@gmail.com>
2017-10-02 23:16:37 +0100

    update iterm2 settings delete useless fzf function

--- runcom/fzf/fzf.sh
+++ runcom/fzf/fzf.sh
@@ -91,19 +91,6 @@ fe() {
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
 
-# Modified version where you can press
-#   - CTRL-O to open with `open` command,
-#   - CTRL-E or Enter key to open with the $EDITOR
-# fo() {
-#   local out file key
-#   IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
-#   key=$(head -1 <<< "$out")
-#   file=$(head -2 <<< "$out" | tail -1)
-#   if [ -n "$file" ]; then
-#     [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
-#   fi
-# }
-
 # fstash - easier way to deal with stashes
 # type fstash to get a list of your stashes
 # enter shows you the contents of the stash
