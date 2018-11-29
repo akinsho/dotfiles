c1c1b90bd5f83605cf1ce52f552feb40f5157b42
Akin909 <akin.sowemimo@gmail.com>
2018-11-29 20:33:43 +0000

    add command to linux zshrc to backup-gnome

--- zsh/.zshrc-Linux.zsh
+++ zsh/.zshrc-Linux.zsh
@@ -1,10 +1,14 @@
 # Reference: https://bbs.archlinux.org/viewtopic.php?id=163075
 # This script runs pacman with sudo only if required
-pacman() 
-{
+pacman() {
     case $1 in
         -S | -D | -S[^sih]* | -R* | -U*)
             /usr/bin/sudo /usr/bin/pacman "$@" ;;
     *)      /usr/bin/pacman "$@" ;;
     esac
+}
+
+backup-gnome() {
+    dconf dump / > dconf-settings.ini
+    mv dconf-settings.ini ~/Dotfiles/linux/gnome/
 }
