53c0765912bee9f13bfd8c955e0943d76fea4863
Akin909 <akin.sowemimo@gmail.com>
2018-12-02 11:25:59 +0000

    add section to symlink vim

--- linux/install.sh
+++ linux/install.sh
@@ -5,6 +5,11 @@ link() {
 DOTFILES='~/Dotfiles'
 LINUXDIR='~/Dotfiles/linux'
 
+# Create the nvim dir and symlink vim dir to it 
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+link ~/.vim $XDG_CONFIG_HOME/nvim
+link ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
 sudo sh
 pacman -S noto-fonts-emoji
 pacman -S nodejs
