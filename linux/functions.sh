3ce4b302e48ba20a7bd9a601796074667fa3dbc3
Akin Sowemimo <akin.sowemimo@gmail.com>
2020-05-29 22:30:24 +0100

    Update script to backup gnome settings

--- linux/functions.sh
+++ linux/functions.sh
@@ -10,10 +10,16 @@ pacman() {
     esac
 }
 
+# https://www.addictivetips.com/ubuntu-linux-tips/back-up-the-gnome-shell-desktop-settings-linux/
 backup-gnome() {
     dconf dump / > $HOME/Dropbox/gnome/dconf-settings.ini
+    tar -cpf icons.tar.gz --absolute-names ~/.icons
+    tar -cpf themes.tar.gz --absolute-names ~/.themes
+    mv --force *.tar.gz $HOME/Dropbox/gnome/
 }
 
 restore-gnome() {
     dconf load / < $HOME/Dropbox/gnome/dconf-settings.ini
+    tar --extract --file $HOME/Dropbox/gnome/icons.tar.gz -C ~/ --strip-components=2
+    tar --extract --file $HOME/Dropbox/gnome/themes.tar.gz -C ~/ --strip-components=2
 }
