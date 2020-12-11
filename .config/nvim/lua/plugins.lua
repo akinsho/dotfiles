0baaa3134171e79dbd9e51f506b589e7a5a41d46
Akin Sowemimo <akin.sowemimo@gmail.com>
2020-12-11 19:48:48 +0000

    use files for lua modules unless a dir is needed

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,37 @@
+local U = require "utils"
+
+if U.is_plugin_loaded("gitsigns") then
+  require("gitsigns").setup {
+    signs = {
+      add = {hl = "GitGutterAdd", text = "▌"},
+      change = {hl = "GitGutterChange", text = "▌"},
+      delete = {hl = "GitGutterDelete", text = "▌"},
+      topdelete = {hl = "GitGutterDelete", text = "▌"},
+      changedelete = {hl = "GitGutterChange", text = "▌"}
+    },
+    keymaps = {
+      -- Default keymap options
+      noremap = true,
+      buffer = true,
+      ["n ]h"] = {
+        expr = true,
+        '&diff ? \']h\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''
+      },
+      ["n [h"] = {
+        expr = true,
+        '&diff ? \'[h\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''
+      },
+      ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+      ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+      ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+      ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+      ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
+    },
+    status_formatter = function(status)
+      local added = status.added > 0 and " " .. status.added or ""
+      local changed = status.changed > 0 and "  " .. status.changed or ""
+      local removed = status.removed > 0 and "  " .. status.removed or ""
+      return status.head .. added .. changed .. removed .. " "
+    end
+  }
+end
