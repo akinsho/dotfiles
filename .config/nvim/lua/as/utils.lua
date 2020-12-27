ee544875f48f0d501afbcf44e5d4c2d580edeb3f
Akin Sowemimo <akin.sowemimo@gmail.com>
2020-12-27 13:16:15 +0000

    Add profile utility function for lua

--- .config/nvim/lua/as/utils.lua
+++ .config/nvim/lua/as/utils.lua
@@ -12,4 +12,29 @@ end
 
 M.plugins_count = M.total_plugins()
 
+--- Usage:
+--- 1. Call `local stop = utils.profile('my-log')` at the top of the file
+--- 2. At the bottom of the file call `stop()`
+--- 3. Restart neovim, the newly created log file should open
+function M.profile(filename)
+  local base = "/tmp/config/profile/"
+  fn.mkdir(base, "p")
+  local success, profile = pcall(require, "plenary.profile.lua_profiler")
+  if not success then
+    return vim.cmd [[echomsg "Plenary is not installed."]]
+  end
+  profile.start()
+  return function()
+    profile.stop()
+    local logfile = base .. filename .. ".log"
+    profile.report(logfile)
+    vim.defer_fn(
+      function()
+        vim.cmd("tabedit " .. logfile)
+      end,
+      1000
+    )
+  end
+end
+
 return M
