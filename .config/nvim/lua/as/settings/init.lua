cadf5fbb79cff62928302d2ab88dcbc9a6c10251
Akin Sowemimo <akin.sowemimo@gmail.com>
2020-12-27 11:49:46 +0000

    refactor: dynamically load all lua plugin settings

--- .config/nvim/lua/as/settings/init.lua
+++ .config/nvim/lua/as/settings/init.lua
@@ -1,5 +1,13 @@
-require("as.settings.completion")
-require("as.settings.treesitter")
-require("as.settings.gitsigns")
-require("as.settings.vim-vsnip")
-require("as.settings.nvim-bufferline")
+
+--- dynamically load all the plugin settings in this module
+--- this essentially replicates the "runtime" call in vimscript
+local function load_settings(dir)
+  for filename in io.popen('dir "' .. dir .. '" -1'):lines() do
+    if not filename:match("init") then
+      local mod = filename:gsub("%.lua", "")
+      require("as.settings." .. mod)
+    end
+  end
+end
+
+load_settings(vim.fn.stdpath("config") .. "/lua/as/settings")
