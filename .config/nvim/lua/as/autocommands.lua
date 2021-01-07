3968f42f478256ecf354a77e4f0c83a53c8cf386
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-01-07 12:27:25 +0000

    autocommands: remove commandline messages
    after a 2 second delay

--- .config/nvim/lua/as/autocommands.lua
+++ .config/nvim/lua/as/autocommands.lua
@@ -1,4 +1,5 @@
 local M = {}
+local fn = vim.fn
 -- Source: https://teukka.tech/luanvim.html
 function M.create(definitions)
   for group_name, definition in pairs(definitions) do
@@ -11,5 +12,29 @@ function M.create(definitions)
     vim.cmd("augroup END")
   end
 end
+
+--- automatically clear commandline messages after a few seconds delay
+--- source: https://unix.stackexchange.com/a/613645
+local function clear_messages()
+  local id
+  return function()
+    if id then
+      fn.timer_stop(id)
+    end
+    id = fn.timer_start(2000, function()
+      if fn.mode() == "n" then
+        vim.cmd [[echon '']]
+      end
+    end)
+  end
+end
+
+M.clear_messages = clear_messages()
+
+M.create({
+  ClearCommandMessages = {
+    {"CmdlineLeave", ":","lua require('as.autocommands').clear_messages()"}
+  }
+})
 
 return M
