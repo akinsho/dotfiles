cfcae4f702fdcf8b4781dd14db4110a3e2eb4ee5
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-01-17 21:08:38 +0000

    feat: add telescope dotfiles picker mapping

--- .config/nvim/lua/as/plugins/telescope.lua
+++ .config/nvim/lua/as/plugins/telescope.lua
@@ -41,6 +41,7 @@ return function()
   -- Find files using Telescope command-line sugar.
   map("n", "<C-P>", "<cmd>lua __telescope_files()<CR>")
   map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+  map("n", "<leader>fd", "<cmd>Telescope git_files cwd=~/.dotfiles<cr>")
   map("n", "<leader>fh", "<cmd>Telescope oldfiles<cr>")
   map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
   map("n", "<leader>fc", "<cmd>Telescope git_commits<cr>")
