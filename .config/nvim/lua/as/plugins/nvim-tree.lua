05fcfd6ef164d3930f1425843bb7ad4a5e558892
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-02-24 18:02:31 +0000

    fix(nvim-tree): remove vim.g.nvim_tree_bindings
    this no longer works the same way, causes errors written in the old
    style and is confusing to refactor to the new style.

--- .config/nvim/lua/as/plugins/nvim-tree.lua
+++ .config/nvim/lua/as/plugins/nvim-tree.lua
@@ -1,43 +1,40 @@
 return function()
   vim.g.nvim_tree_icons = {
-    default = '',
+    default = "",
     git = {
       unstaged = "",
       staged = "",
       unmerged = "",
       renamed = "",
       untracked = "",
-      deleted  = ""
-    },
-  }
-
-  vim.g.nvim_tree_bindings = {
-    cd = {"cd"},
-    edit = {"<CR>", "o", "l"},
-    close_node = {"<BS>", "h"}
+      deleted = ""
+    }
   }
 
   as_utils.map("n", "<c-n>", [[<cmd>NvimTreeToggle<CR>]])
 
-  vim.g.nvim_tree_indent_markers       = 1
-  vim.g.nvim_tree_git_hl               = 1
-  vim.g.nvim_tree_auto_close           = 0 -- closes the tree when it's the last window
-  vim.g.nvim_tree_follow               = 1 -- show selected file on open
-  vim.g.nvim_tree_width                = 30
-  vim.g.nvim_tree_width_allow_resize   = 1
-  vim.g.nvim_tree_disable_netrw        = 0
-  vim.g.nvim_tree_hijack_netrw         = 0
-  vim.g.nvim_tree_root_folder_modifier = ':t'
-  vim.g.nvim_tree_ignore               = {'.DS_Store', 'fugitive:', '.git'}
+  vim.g.nvim_tree_indent_markers = 1
+  vim.g.nvim_tree_git_hl = 1
+  vim.g.nvim_tree_auto_close = 0 -- closes the tree when it's the last window
+  vim.g.nvim_tree_follow = 1 -- show selected file on open
+  vim.g.nvim_tree_width = 30
+  vim.g.nvim_tree_width_allow_resize = 1
+  vim.g.nvim_tree_disable_netrw = 0
+  vim.g.nvim_tree_hijack_netrw = 0
+  vim.g.nvim_tree_root_folder_modifier = ":t"
+  vim.g.nvim_tree_ignore = {".DS_Store", "fugitive:", ".git"}
 
   vim.cmd [[highlight link NvimTreeIndentMarker Comment]]
   vim.cmd [[highlight NvimTreeRootFolder gui=bold,italic guifg=LightMagenta]]
 
-  require('as.autocommands').augroup("NvimTreeOverrides", {
+  require("as.autocommands").augroup(
+    "NvimTreeOverrides",
     {
-      events = {"ColorScheme"},
-      targets = {"*"},
-      command = "highlight NvimTreeRootFolder gui=bold,italic guifg=LightMagenta",
+      {
+        events = {"ColorScheme"},
+        targets = {"*"},
+        command = "highlight NvimTreeRootFolder gui=bold,italic guifg=LightMagenta"
+      }
     }
-  })
+  )
 end
