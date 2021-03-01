7fdf3c8f1908c274f6dc90cbdfb893f175c28b9a
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-03-01 20:58:46 +0000

    refactor(lsp): make helper functions global
    This stops them being redefined when the lsp config is reloaded by
    packer

--- .config/nvim/lua/as/plugins/lspconfig.lua
+++ .config/nvim/lua/as/plugins/lspconfig.lua
@@ -1,4 +1,26 @@
-function _G._lsp_tagfunc(pattern, flags)
+LspConfig = {}
+
+local command = as_utils.command
+
+function LspConfig.highlight()
+  -----------------------------------------------------------------------------//
+  -- Highlights
+  -----------------------------------------------------------------------------//
+  require("as.highlights").all {
+    {"LspReferenceText", {link = "CursorLine"}},
+    {"LspReferenceRead", {link = "CursorLine"}},
+    {"LspDiagnosticsDefaultHint", {guifg = "#fab005"}},
+    {"LspDiagnosticsDefaultError", {guifg = "#E06C75"}},
+    {"LspDiagnosticsDefaultWarning", {guifg = "#ff922b"}},
+    {"LspDiagnosticsDefaultInformation", {guifg = "#15aabf"}},
+    {"LspDiagnosticsUnderlineError", {gui = "undercurl", guisp = "#E06C75"}},
+    {"LspDiagnosticsUnderlineHint", {gui = "undercurl", guisp = "#fab005"}},
+    {"LspDiagnosticsUnderlineWarning", {gui = "undercurl", guisp = "orange"}},
+    {"LspDiagnosticsUnderlineInformation", {gui = "undercurl", guisp = "#15aabf"}}
+  }
+end
+
+function LspConfig.tagfunc(pattern, flags)
   if flags ~= "c" then
     return vim.NIL
   end
@@ -24,194 +46,162 @@ function _G._lsp_tagfunc(pattern, flags)
   return results
 end
 
+-----------------------------------------------------------------------------//
+-- Autocommands
+-----------------------------------------------------------------------------//
+local function setup_autocommands(client)
+  local autocommands = require "as.autocommands"
+  autocommands.augroup(
+    "LspHighlights",
+    {
+      {
+        events = {"VimEnter", "ColorScheme"},
+        targets = {"*"},
+        command = [[lua LspFunctions.highlight()]]
+      }
+    }
+  )
+  if client and client.resolved_capabilities.document_highlight then
+    autocommands.augroup(
+      "LspCursorCommands",
+      {
+        {
+          events = {"CursorHold"},
+          targets = {"<buffer>"},
+          command = "lua vim.lsp.buf.document_highlight()"
+        },
+        {
+          events = {"CursorHoldI"},
+          targets = {"<buffer>"},
+          command = "lua vim.lsp.buf.document_highlight()"
+        },
+        {
+          events = {"CursorMoved"},
+          targets = {"<buffer>"},
+          command = "lua vim.lsp.buf.clear_references()"
+        }
+      }
+    )
+  end
+  if client and client.resolved_capabilities.document_formatting then
+    -- format on save
+    autocommands.augroup(
+      "LspFormat",
+      {
+        {
+          events = {"BufWritePre"},
+          targets = {"<buffer>"},
+          command = "lua vim.lsp.buf.formatting_sync(nil, 1000)"
+        }
+      }
+    )
+  end
+end
+-----------------------------------------------------------------------------//
+-- Mappings
+-----------------------------------------------------------------------------//
+local function setup_mappings(client)
+  local buf_map = as_utils.buf_map
+  buf_map(0, "n", "<c-]>", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  buf_map(0, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  if client.resolved_capabilities.implementation then
+    buf_map(0, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  end
+  buf_map(0, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  buf_map(0, "n", "gI", "<cmd>vim.lsp.buf.incoming_calls()<CR>")
+  buf_map(0, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  buf_map(0, "n", "<leader>cs", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+  buf_map(0, "n", "<leader>cw", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
+  buf_map(0, "n", "<leader>rf", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+end
+
+function LspConfig.on_attach(client, bufnr)
+  setup_autocommands(client)
+  setup_mappings(client)
+
+  require("vim.lsp.protocol").CompletionItemKind = {
+    " [Text]", -- Text
+    " [Method]", -- Method
+    "ƒ [Function]", -- Function
+    " [Constructor]", -- Constructor
+    "識 [Field]", -- Field
+    " [Variable]", -- Variable
+    "\u{f0e8} [Class]", -- Class
+    "ﰮ [Interface]", -- Interface
+    " [Module]", -- Module
+    " [Property]", -- Property
+    " [Unit]", -- Unit
+    " [Value]", -- Value
+    "了 [Enum]", -- Enum
+    " [Keyword]", -- Keyword
+    " [Snippet]", -- Snippet
+    " [Color]", -- Color
+    " [File]", -- File
+    "渚 [Reference]", -- Reference
+    " [Folder]", -- Folder
+    " [Enum]", -- Enum
+    " [Constant]", -- Constant
+    " [Struct]", -- Struct
+    "鬒 [Event]", -- Event
+    "\u{03a8} [Operator]", -- Operator
+    " [Type Parameter]" -- TypeParameter
+  }
+
+  if client.resolved_capabilities.goto_definition then
+    vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.LspFunctions.tagfunc")
+  end
+  require("lsp-status").on_attach(client)
+end
+
+-----------------------------------------------------------------------------//
+-- Commands
+-----------------------------------------------------------------------------//
+command {
+  "ReloadLSP",
+  function()
+    vim.lsp.stop_client(vim.lsp.get_active_clients())
+    vim.cmd [[edit]]
+  end
+}
+command {
+  "LogLSP",
+  function()
+    local path = vim.lsp.get_log_path()
+    vim.cmd("edit " .. path)
+  end
+}
+command {
+  "DebugLSP",
+  function()
+    print(vim.inspect(vim.lsp.get_active_clients()))
+  end
+}
+command {
+  "Format",
+  function()
+    vim.lsp.buf.formatting_sync(nil, 1000)
+  end
+}
+
 return function()
   local has = as_utils.has
   local fn = vim.fn
-  local command = as_utils.command
 
   local lspconfig = require "lspconfig"
   local lsp_status = require "lsp-status"
   local flutter = require "flutter-tools"
 
-  -----------------------------------------------------------------------------//
-  -- Highlights
-  -----------------------------------------------------------------------------//
-  require("as.highlights").all {
-    {"LspReferenceText", {link = "CursorLine"}},
-    {"LspReferenceRead", {link = "CursorLine"}},
-    {"LspDiagnosticsDefaultHint", {guifg = "#fab005"}},
-    {"LspDiagnosticsDefaultError", {guifg = "#E06C75"}},
-    {"LspDiagnosticsDefaultWarning", {guifg = "#ff922b"}},
-    {"LspDiagnosticsDefaultInformation", {guifg = "#15aabf"}},
-    {"LspDiagnosticsUnderlineError", {gui = "undercurl", guisp = "#E06C75"}},
-    {"LspDiagnosticsUnderlineHint", {gui = "undercurl", guisp = "#fab005"}},
-    {"LspDiagnosticsUnderlineWarning", {gui = "undercurl", guisp = "orange"}},
-    {"LspDiagnosticsUnderlineInformation", {gui = "undercurl", guisp = "#15aabf"}}
-  }
-
-  -----------------------------------------------------------------------------//
-  -- Helpers
-  -----------------------------------------------------------------------------//
-  function _G.reload_lsp()
-    vim.lsp.stop_client(vim.lsp.get_active_clients())
-    vim.cmd [[edit]]
-  end
-
-  function _G.open_lsp_log()
-    local path = vim.lsp.get_log_path()
-    vim.cmd("edit " .. path)
-  end
-
-  command {
-    "Format",
-    function()
-      vim.lsp.buf.formatting_sync(nil, 1000)
-    end
-  }
-
-  command {
-    "ReloadLSP",
-    function()
-      reload_lsp()
-    end
-  }
-
-  command {
-    "DebugLSP",
-    function()
-      print(vim.inspect(vim.lsp.get_active_clients()))
-    end
-  }
-
-  command {
-    "LogLSP",
-    function()
-      open_lsp_log()
-    end
-  }
-
-  -----------------------------------------------------------------------------//
-  -- Autocommands
-  -----------------------------------------------------------------------------//
-  local function setup_autocommands(client)
-    local autocommands = require "as.autocommands"
-    autocommands.augroup(
-      "LspHighlights",
-      {
-        {
-          events = {"VimEnter", "ColorScheme"},
-          targets = {"*"},
-          command = [[lua require('as.lsp').highlight()]]
-        }
-      }
-    )
-    if client and client.resolved_capabilities.document_highlight then
-      autocommands.augroup(
-        "LspCursorCommands",
-        {
-          {
-            events = {"CursorHold"},
-            targets = {"<buffer>"},
-            command = "lua vim.lsp.buf.document_highlight()"
-          },
-          {
-            events = {"CursorHoldI"},
-            targets = {"<buffer>"},
-            command = "lua vim.lsp.buf.document_highlight()"
-          },
-          {
-            events = {"CursorMoved"},
-            targets = {"<buffer>"},
-            command = "lua vim.lsp.buf.clear_references()"
-          }
-        }
-      )
-    end
-    if client and client.resolved_capabilities.document_formatting then
-      -- format on save
-      autocommands.augroup(
-        "LspFormat",
-        {
-          {
-            events = {"BufWritePre"},
-            targets = {"<buffer>"},
-            command = "lua vim.lsp.buf.formatting_sync(nil, 1000)"
-          }
-        }
-      )
-    end
-  end
-  -----------------------------------------------------------------------------//
-  -- Mappings
-  -----------------------------------------------------------------------------//
-  local function setup_mappings(client)
-    local buf_map = as_utils.buf_map
-    buf_map(0, "n", "<c-]>", "<cmd>lua vim.lsp.buf.definition()<CR>")
-    buf_map(0, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-    if client.resolved_capabilities.implementation then
-      buf_map(0, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-    end
-    buf_map(0, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-    buf_map(0, "n", "gI", "<cmd>vim.lsp.buf.incoming_calls()<CR>")
-    buf_map(0, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-    buf_map(0, "n", "<leader>cs", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
-    buf_map(0, "n", "<leader>cw", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
-    buf_map(0, "n", "<leader>rf", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-  end
+  LspConfig.highlight()
   -----------------------------------------------------------------------------//
   -- Signs
   -----------------------------------------------------------------------------//
-  local signs = {
-    {"LspDiagnosticsSignError", {text = "✗", texthl = "LspDiagnosticsSignError"}},
-    {"LspDiagnosticsSignWarning", {text = "", texthl = "LspDiagnosticsSignWarning"}},
-    {"LspDiagnosticsSignInformation", {text = "", texthl = "LspDiagnosticsSignInformation"}},
-    {"LspDiagnosticsSignHint", {text = "", texthl = "LspDiagnosticsSignHint"}}
-  }
-
-  -----------------------------------------------------------------------------//
-  -- Buffer attach
-  -----------------------------------------------------------------------------//
-  local function on_attach(client, bufnr)
-    setup_autocommands(client)
-    setup_mappings(client)
-
-    require("vim.lsp.protocol").CompletionItemKind = {
-      " [Text]", -- Text
-      " [Method]", -- Method
-      "ƒ [Function]", -- Function
-      " [Constructor]", -- Constructor
-      "識 [Field]", -- Field
-      " [Variable]", -- Variable
-      "\u{f0e8} [Class]", -- Class
-      "ﰮ [Interface]", -- Interface
-      " [Module]", -- Module
-      " [Property]", -- Property
-      " [Unit]", -- Unit
-      " [Value]", -- Value
-      "了 [Enum]", -- Enum
-      " [Keyword]", -- Keyword
-      " [Snippet]", -- Snippet
-      " [Color]", -- Color
-      " [File]", -- File
-      "渚 [Reference]", -- Reference
-      " [Folder]", -- Folder
-      " [Enum]", -- Enum
-      " [Constant]", -- Constant
-      " [Struct]", -- Struct
-      "鬒 [Event]", -- Event
-      "\u{03a8} [Operator]", -- Operator
-      " [Type Parameter]" -- TypeParameter
-    }
-
-    if client.resolved_capabilities.goto_definition then
-      vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua._lsp_tagfunc")
-    end
-    require("lsp-status").on_attach(client)
-  end
-
-  for _, sign in pairs(signs) do
-    fn.sign_define(unpack(sign))
-  end
+  fn.sign_define("LspDiagnosticsSignError", {text = "✗", texthl = "LspDiagnosticsSignError"})
+  fn.sign_define("LspDiagnosticsSignHint", {text = "", texthl = "LspDiagnosticsSignHint"})
+  fn.sign_define("LspDiagnosticsSignWarning", {text = "", texthl = "LspDiagnosticsSignWarning"})
+  fn.sign_define(
+    "LspDiagnosticsSignInformation",
+    {text = "", texthl = "LspDiagnosticsSignInformation"}
+  )
 
   -----------------------------------------------------------------------------//
   -- Setup plugins
@@ -224,6 +214,7 @@ return function()
     status_symbol = ""
   }
   lsp_status.register_progress()
+
   -----------------------------------------------------------------------------//
   -- Handler overrides
   -----------------------------------------------------------------------------//
@@ -306,13 +297,13 @@ return function()
     },
     dev_log = {open_cmd = "tabedit"},
     lsp = {
-      on_attach = on_attach,
+      on_attach = LspConfig.on_attach,
       capabilities = status_capabilities
     }
   }
 
   for server, config in pairs(servers) do
-    config.on_attach = on_attach
+    config.on_attach = LspConfig.on_attach
     if not config.capabilities then
       config.capabilities = vim.lsp.protocol.make_client_capabilities()
     end
