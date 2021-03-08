aa1338e02e90880460194ebe5fb3f2dde2f48bcb
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-03-08 09:43:09 +0000

    fix(switch): fix switch.vim config

--- .config/nvim/lua/as/plugins/switch.lua
+++ .config/nvim/lua/as/plugins/switch.lua
@@ -1,15 +1,14 @@
 return function()
-  vim.g.switch_custom_definitions = {
-    {
-      [[\<\(\l\)\(\l\+\(\u\l\+\)\+\)\>]]= [[\=toupper(submatch(1)) . submatch(2)]],
-      [[\<\(\u\l\+\)\(\u\l\+\)\+\>]]= [[\\=tolower(substitute(submatch(0), '\\(\\l\\)\\(\\u\\)', '\\1_\\2', 'g'))]],
-      [[\<\(\l\+\)\(_\l\+\)\+\>]]= [[\U\0]],
-      [[\<\(\u\+\)\(_\u\+\)\+\>]]= [[\\=tolower(substitute(submatch(0), '_', '-', 'g'))]],
-      [[\<\(\l\+\)\(-\l\+\)\+\>]]= [[\\=substitute(submatch(0), '-\\(\\l\\)', '\\u\\1', 'g')]],
-    }
-  }
 
 vim.api.nvim_exec [[
+let g:switch_custom_definitions = [
+  \'\<\(\l\)\(\l\+\(\u\l\+\)\+\)\>' : '\=toupper(submatch(1)) . submatch(2)',
+  \'\<\(\u\l\+\)\(\u\l\+\)\+\>': '\\=tolower(substitute(submatch(0), '\\(\\l\\)\\(\\u\\)', '\\1_\\2', 'g'))',
+  \'\<\(\l\+\)\(_\l\+\)\+\>': '\U\0',
+  \'\<\(\u\+\)\(_\u\+\)\+\>': '\\=tolower(substitute(submatch(0), '_', '-', 'g'))',
+  \'\<\(\l\+\)\(-\l\+\)\+\>': '\\=substitute(submatch(0), '-\\(\\l\\)', '\\u\\1', 'g')',
+  \]
+
 augroup SwitchFiletypes
   autocmd!
   " - [ ] → - [x] → - [-] → loops back to - [ ]
