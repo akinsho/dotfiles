73d2d37cc55c5ce10bf6c7f909bad1bc16c44ba0
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-03-22 14:13:35 +0000

    style(indentline): change current context highlight

--- .config/nvim/lua/as/plugins/indentline.lua
+++ .config/nvim/lua/as/plugins/indentline.lua
@@ -24,6 +24,7 @@ return function()
   }
   vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
   vim.g.indent_blankline_show_current_context = true
+  vim.g.indent_blankline_context_highlight = 'MoreMsg'
   vim.g.indent_blankline_context_patterns = {
     "class",
     "function",
