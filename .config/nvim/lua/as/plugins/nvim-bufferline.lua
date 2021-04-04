4ca0859d84ba417d8a12f7412a7a50da57d219a0
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-04 12:11:09 +0100

    refactor(bufferline): move functions out of config
    into separate variables

--- .config/nvim/lua/as/plugins/nvim-bufferline.lua
+++ .config/nvim/lua/as/plugins/nvim-bufferline.lua
@@ -6,6 +6,39 @@ return function()
     return vim.bo[b].filetype == ft
   end
 
+  local function diagnostics_indicator(_, _, diagnostics)
+    local result = {}
+    local symbols = {error = " ", warning = " ", info = ""}
+    for name, count in pairs(diagnostics) do
+      if symbols[name] and count > 0 then
+        table.insert(result, symbols[name] .. count)
+      end
+    end
+    result = table.concat(result, " ")
+    return #result > 0 and " " .. result or ""
+  end
+
+  local function custom_filter(buf, buf_nums)
+    local logs =
+      vim.tbl_filter(
+      function(b)
+        return is_ft(b, "log")
+      end,
+      buf_nums
+    )
+    if vim.tbl_isempty(logs) then
+      return true
+    end
+    local tab_num = vim.fn.tabpagenr()
+    local last_tab = vim.fn.tabpagenr("$")
+    local is_log = is_ft(buf, "log")
+    if last_tab == 1 then
+      return true
+    end
+    -- only show log buffers in secondary tabs
+    return (tab_num == last_tab and is_log) or (tab_num ~= last_tab and not is_log)
+  end
+
   require("bufferline").setup {
     options = {
       mappings = false,
@@ -13,36 +46,8 @@ return function()
       show_close_icon = false,
       separator_style = "slant",
       diagnostics = not has("mac") and "nvim_lsp" or false,
-      diagnostics_indicator = function(_, _, diagnostics)
-        local result = {}
-        local symbols = {error = " ", warning = " ", info = ""}
-        for name, count in pairs(diagnostics) do
-          if symbols[name] and count > 0 then
-            table.insert(result, symbols[name] .. count)
-          end
-        end
-        return " " .. table.concat(result, " ")
-      end,
-      custom_filter = function(buf, buf_nums)
-        local logs =
-          vim.tbl_filter(
-          function(b)
-            return is_ft(b, "log")
-          end,
-          buf_nums
-        )
-        if vim.tbl_isempty(logs) then
-          return true
-        end
-        local tab_num = vim.fn.tabpagenr()
-        local last_tab = vim.fn.tabpagenr("$")
-        local is_log = is_ft(buf, "log")
-        if last_tab == 1 then
-          return true
-        end
-        -- only show log buffers in secondary tabs
-        return (tab_num == last_tab and is_log) or (tab_num ~= last_tab and not is_log)
-      end
+      diagnostics_indicator = diagnostics_indicator,
+      custom_filter = custom_filter
     }
   }
 
