524a310ed3da3f16a27fad2164b833d135987e3f
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-04 21:23:09 +0100

    refactor(tmux): use current dir for statusline
    it is much simpler than trying to parse the session variable which could
    be manipulated in all sorts of ways by the session plugin de jour

--- .config/nvim/lua/as/tmux.lua
+++ .config/nvim/lua/as/tmux.lua
@@ -2,7 +2,6 @@ local M = {}
 
 local fn = vim.fn
 local fmt = string.format
-local empty = as_utils.is_empty
 local loaded, devicons = pcall(require, "nvim-web-devicons")
 
 function M.statusline_colors()
@@ -13,38 +12,18 @@ function M.statusline_colors()
 end
 
 function M.on_enter()
-  local fname = fn.expand("%:t")
-  local session = ""
-  if fn.strlen(fname) then
-    local this_session = vim.v.this_session
-    local session_file = not empty(this_session) and this_session or "Neovim"
-    local window_title = session
+  local session = fn.fnamemodify(vim.loop.cwd(), ":t") or "Neovim"
+  local window_title = session
 
-    if session_file:match("_") then
-      local parts = vim.split(session_file, "%.")
-      session = parts[#parts - 1]
-    elseif session_file:match("%%") then
-      -- NOTE this is here for plugins like "prosession/obsession"
-      -- which modify the session name into "path/to/session/file%name%.vim
-      -- explainer ->
-      -- :t -> get the tail
-      -- :gs?subject?replacement?
-      -- get the tail of that -> :r remove the extension
-      -- see :h filename-modifier for details
-      session = fn.fnamemodify(session_file, ":t:gs?%?/?:t:r")
-    else
-      session = fn.fnamemodify(session_file, ":t")
+  if loaded then
+    local name = fn.bufname()
+    local icon, hl = devicons.get_icon(name, fn.fnamemodify(name, ":e"), {default = true})
+    if icon and hl then
+      local hl_value = require("as.highlights").hl_value
+      window_title = fmt("%s • #[fg=%s]%s", session, hl_value(hl, "fg"), icon)
     end
-    if loaded then
-      local name = fn.bufname()
-      local icon, hl = devicons.get_icon(name, fn.fnamemodify(name, ":e"), {default = true})
-      if icon and hl then
-        local hl_value = require("as.highlights").hl_value
-        window_title = fmt("%s • #[fg=%s]%s", session, hl_value(hl, "fg"), icon)
-      end
-    end
-    fn.jobstart(fmt("tmux rename-window '%s'", window_title))
   end
+  fn.jobstart(fmt("tmux rename-window '%s'", window_title))
 end
 
 function M.on_leave()
