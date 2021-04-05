8456f64d6398bae9e2fa2ab2a996305ff58a1bc5
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-05 13:00:24 +0100

    refactor(lua-ftplugin): handle api/fn aliases

--- .config/nvim/lua/as/ftplugin/lua.lua
+++ .config/nvim/lua/as/ftplugin/lua.lua
@@ -1,7 +1,17 @@
+local function find(word, ...)
+  for _, str in ipairs({...}) do
+    local match_start, match_end = string.find(word, str)
+    if match_start then
+      return str, match_start, match_end
+    end
+  end
+end
+
 --- Stolen from nlua.nvim this function attempts to open
 --- vim help docs if an api or vim.fn function otherwise it
 --- shows the lsp hover doc
----@param word string
+--- @param word string
+--- @param callback function
 local function keyword(word, callback)
   local original_iskeyword = vim.bo.iskeyword
 
@@ -10,15 +20,18 @@ local function keyword(word, callback)
 
   vim.bo.iskeyword = original_iskeyword
 
-  -- TODO: This is kind of a lame hack... since you could rename `vim.api` -> `a` or similar
-  if string.find(word, "vim.api") then
-    local _, finish = string.find(word, "vim.api.")
+  -- TODO: This is a sub par work around, since I usually rename `vim.api` -> `api` or similar
+  -- consider maybe using treesitter in the future
+  local api_match = find(word, "api", "vim.api")
+  local fn_match = find(word, "fn", "vim.fn")
+  if api_match then
+    local _, finish = string.find(word, api_match..".")
     local api_function = string.sub(word, finish + 1)
 
     vim.cmd(string.format("help %s", api_function))
     return
-  elseif string.find(word, "vim.fn") then
-    local _, finish = string.find(word, "vim.fn.")
+  elseif fn_match then
+    local _, finish = string.find(word, fn_match..".")
     local api_function = string.sub(word, finish + 1) .. "()"
 
     vim.cmd(string.format("help %s", api_function))
