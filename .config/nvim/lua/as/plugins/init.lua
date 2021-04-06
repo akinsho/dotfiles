366c9c418ad2a259a06e28e4f584fc4a766c1246
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-06 16:34:39 +0100

    fix(neoscroll): don't map <c-e>
    I use this for vim-visual-multi

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -264,7 +264,9 @@ return require("packer").startup {
     use {
       "karb94/neoscroll.nvim",
       config = function()
-        require("neoscroll").setup {}
+        require("neoscroll").setup {
+          mappings = {"<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "zt", "zz", "zb"}
+        }
       end
     }
     use {
