39446e531e04a664c26e961d2b8db90a2e6151a2
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-08 15:59:56 +0100

    chore(telescope): use main branch

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -153,7 +153,6 @@ return require("packer").startup {
     use {
       "nvim-telescope/telescope.nvim",
       config = conf("telescope"),
-      branch = "use_plenary_async",
       requires = {
         "nvim-lua/popup.nvim",
         "nvim-telescope/telescope-fzf-writer.nvim",
