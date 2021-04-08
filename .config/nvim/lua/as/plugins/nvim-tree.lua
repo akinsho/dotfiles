9aa4d962f7496beac83cc240e07cfa8071fcc244
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-08 22:43:06 +0100

    feat(tree): add lsp diagnostics

--- .config/nvim/lua/as/plugins/nvim-tree.lua
+++ .config/nvim/lua/as/plugins/nvim-tree.lua
@@ -13,6 +13,7 @@ return function()
 
   as.nnoremap("<c-n>", [[<cmd>NvimTreeToggle<CR>]])
 
+  vim.g.nvim_tree_lsp_diagnostics = 1
   vim.g.nvim_tree_indent_markers = 1
   vim.g.nvim_tree_group_empty = 1
   vim.g.nvim_tree_git_hl = 1
