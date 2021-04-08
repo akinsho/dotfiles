34110bf4f0989b5e6381ad1ce069e8985fa6bcb0
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-08 08:42:48 +0100

    chore: remove unnecessary delayed module loading

--- .config/nvim/lua/as/init.lua
+++ .config/nvim/lua/as/init.lua
@@ -7,9 +7,4 @@ require("as.numbers")
 require("as.mappings")
 require("as.folds")
 require("as.localrc").setup()
-
-function _G.__as_setup_configs()
-  require("as.whitespace").setup()
-end
--- delay setting up of some configs like lsp till vim has started
-vim.cmd [[autocmd! VimEnter * ++once lua __as_setup_configs()]]
+require("as.whitespace").setup()
