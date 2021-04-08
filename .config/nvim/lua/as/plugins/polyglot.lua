438e6be94902398e5ca388c19fbede74a2ea3a39
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-08 22:26:12 +0100

    chore(treesitter): disable wonky indent

--- .config/nvim/lua/as/plugins/polyglot.lua
+++ .config/nvim/lua/as/plugins/polyglot.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-  vim.g.polyglot_disabled = {"sensible", "autoindent"}
+  vim.g.polyglot_disabled = {"sensible"} -- , "autoindent"
 end
 
 function M.config()
--- .config/nvim/lua/as/plugins/treesitter.lua
+++ .config/nvim/lua/as/plugins/treesitter.lua
@@ -35,7 +35,7 @@ return function()
       }
     },
     indent = {
-      enable = true
+      enable = false
     },
     textobjects = {
       select = {
