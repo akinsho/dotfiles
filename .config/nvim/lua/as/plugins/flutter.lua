8176972f13c3d0f761d7bd3424746a7b8795b652
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-05-11 21:46:52 +0100

    chore(flutter): remove unneeded config opts

--- .config/nvim/lua/as/plugins/flutter.lua
+++ .config/nvim/lua/as/plugins/flutter.lua
@@ -13,11 +13,7 @@ return function()
     dev_log = {open_cmd = "tabedit"},
     lsp = {
       on_attach = as.lsp and as.lsp.on_attach,
-      capabilities = require("lsp-status").capabilities or {},
-      settings = {
-        showTodos = true,
-        completeFunctionCalls = true
-      }
+      capabilities = require("lsp-status").capabilities or {}
     }
   }
 end
