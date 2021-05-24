9a5cb2ca3d1b415a6492bcd4d638214ec2c0f05b
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-05-24 20:45:52 +0100

    refactor(highlights): remove dependency
    on bufferline plugin

--- .config/nvim/lua/as/highlights.lua
+++ .config/nvim/lua/as/highlights.lua
@@ -6,6 +6,37 @@ local M = {}
 
 local ts_playground_loaded, ts_hl_info
 
+---Convert a hex color to rgb
+---@param color string
+---@return number
+---@return number
+---@return number
+local function hex_to_rgb(color)
+  local hex = color:gsub("#", "")
+  return tonumber(hex:sub(1, 2), 16), tonumber(hex:sub(3, 4), 16), tonumber(hex:sub(5), 16)
+end
+
+local function alter(attr, percent)
+  return math.floor(attr * (100 + percent) / 100)
+end
+
+---@source https://stackoverflow.com/q/5560248
+---@see: https://stackoverflow.com/a/37797380
+---Darken a specified hex color
+---@param color string
+---@param percent number
+---@return string
+function M.darken_color(color, percent)
+  local r, g, b = hex_to_rgb(color)
+  if not r or not g or not b then
+    return "NONE"
+  end
+  r, g, b = alter(r, percent), alter(g, percent), alter(b, percent)
+  r, g, b = math.min(r, 255), math.min(g, 255), math.min(b, 255)
+  r, g, b = string.format("%0x", r), string.format("%0x", g), string.format("%0x", b)
+  return "#" .. r .. g .. b
+end
+
 -----------------------------------------------------------------------------//
 -- CREDIT: @Cocophon
 -- This function allows you to see the syntax highlight token of the cursor word and that token's links
@@ -261,16 +292,6 @@ local function general_overrides()
     {"NvimNotificationError", {link = "ErrorMsg"}},
     {"NvimNotificationInfo", {guifg = "#51afef"}}
   }
-end
-
-function M.darken_color(color, amount)
-  local success, module = pcall(require, "bufferline")
-  if not success then
-    vim.notify("Failed to load bufferline", 2, {})
-    return color
-  else
-    return module.shade_color(color, amount)
-  end
 end
 
 local function set_sidebar_highlight()
