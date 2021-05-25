e4a3ceca5d842b6a5dc66c328782c50fd5baddea
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-05-25 11:55:55 +0100

    chore(dart): don't lazy load syntax plugin

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -609,7 +609,7 @@ require("packer").startup {
         require("spellsitter").setup {hl = "SpellBad", captures = {"comment"}}
       end
     }
-    use {"dart-lang/dart-vim-plugin", ft = "dart"}
+    use "dart-lang/dart-vim-plugin"
     use "plasticboy/vim-markdown"
     use "mtdl9/vim-log-highlighting"
     ---}}}
