ed62c48dc040e807089f77575f7fba3de37fa321
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-05-27 18:52:07 +0100

    fix(lsp_signature): disable the hint thingy

--- .config/nvim/lua/as/plugins/lspconfig.lua
+++ .config/nvim/lua/as/plugins/lspconfig.lua
@@ -164,6 +164,7 @@ function as.lsp.on_attach(client, bufnr)
   require("lsp_signature").on_attach(
     {
       bind = true,
+      hint_enable = false,
       handler_opts = {
         border = "single"
       }
