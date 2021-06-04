349068923b6f09e86b5f475ba10f5b69c202817a
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-06-04 14:38:20 +0100

    fix(swapfile): save files using the absolute path
    This should prevent conflicts with files with the same name from
    different directories

--- .config/nvim/lua/as/settings.lua
+++ .config/nvim/lua/as/settings.lua
@@ -244,7 +244,10 @@ if fn.isdirectory(vim.o.undodir) == 0 then
 end
 vim.opt.undofile = true
 vim.opt.swapfile = true
-vim.opt.directory = fn.stdpath("data") .. "/swap/"
+-- The // at the end tells Vim to use the absolute path to the file to create the swap file.
+-- This will ensure that swap file name is unique, so there are no collisions between files
+-- with the same name from different directories.
+vim.opt.directory = fn.stdpath("data") .. "/swap//"
 if fn.isdirectory(vim.o.directory) == 0 then
   fn.mkdir(vim.o.directory, "p")
 end
