58f3a92c587007c868b94d5d980b99c80358e8db
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-06-08 16:41:05 +0100

    fix(diffview): remove unneeded mappings
    since the plugin now support config mapping merging

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -679,7 +679,6 @@ require("packer").startup {
       module = "diffview",
       keys = "<localleader>gd",
       config = function()
-        local cb = require("diffview.config").diffview_callback
         require("which-key").register(
           {gd = {"<Cmd>DiffviewOpen<CR>", "diff ref"}},
           {prefix = "<localleader>"}
@@ -688,28 +687,10 @@ require("packer").startup {
           {
             key_bindings = {
               file_panel = {
-                ["q"] = "<Cmd>DiffviewClose<CR>",
-                ["j"] = cb("next_entry"), -- Bring the cursor to the next file entry
-                ["<down>"] = cb("next_entry"),
-                ["k"] = cb("prev_entry"), -- Bring the cursor to the previous file entry.
-                ["<up>"] = cb("prev_entry"),
-                ["<cr>"] = cb("select_entry"), -- Open the diff for the selected entry.
-                ["o"] = cb("select_entry"),
-                ["-"] = cb("toggle_stage_entry"), -- Stage / unstage the selected entry.
-                ["S"] = cb("stage_all"), -- Stage all entries.
-                ["U"] = cb("unstage_all"), -- Unstage all entries.
-                ["R"] = cb("refresh_files"), -- Update stats and entries in the file list.
-                ["<tab>"] = cb("select_next_entry"),
-                ["<s-tab>"] = cb("select_prev_entry"),
-                ["<leader>e"] = cb("focus_files"),
-                ["<leader>b"] = cb("toggle_files")
+                ["q"] = "<Cmd>DiffviewClose<CR>"
               },
               view = {
-                ["q"] = "<Cmd>DiffviewClose<CR>",
-                ["<tab>"] = cb("select_next_entry"), -- Open the diff for the next file
-                ["<s-tab>"] = cb("select_prev_entry"), -- Open the diff for the previous file
-                ["<leader>e"] = cb("focus_files"), -- Bring focus to the files panel
-                ["<leader>b"] = cb("toggle_files") -- Toggle the files panel.
+                ["q"] = "<Cmd>DiffviewClose<CR>"
               }
             }
           }
