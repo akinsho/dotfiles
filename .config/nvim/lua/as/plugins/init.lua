5f81c710bd9254a5923b533dc773b8dc7be369bf
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-06-13 22:47:11 +0100

    chore(neoscroll): don't stop at EOF

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -416,7 +416,8 @@ require("packer").startup {
       "karb94/neoscroll.nvim",
       config = function()
         require("neoscroll").setup {
-          mappings = {"<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "zt", "zz", "zb"}
+          mappings = {"<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "zt", "zz", "zb"},
+          stop_eof = false
         }
       end
     }
