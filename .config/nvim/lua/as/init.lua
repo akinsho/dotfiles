361e9950aa63538b62237b2765b914c50d40148e
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-06-14 10:22:06 +0100

    REFACTOR(lua): move lua modules to plugin/
    as these are now automatically sourced

--- .config/nvim/lua/as/init.lua
+++ .config/nvim/lua/as/init.lua
@@ -2,12 +2,5 @@ require("as.globals")
 require("as.settings")
 require("as.highlights")
 require("as.statusline")
-require("as.numbers")
-require("as.mappings")
-require("as.folds")
-require("as.whitespace")
-require("as.dev").setup(false) -- disabled
 require("as.localrc").setup()
 require("as.plugins") -- source plugins after config but during startup
-require("as.quickfix")
-require("as.autocommands")
