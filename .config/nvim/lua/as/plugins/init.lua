b96c42d70e4f02099e88e1690f5652d92f39a133
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-06-15 09:39:26 +0100

    fix(packer): use rounded border

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -913,7 +913,7 @@ require("packer").startup {
   config = {
     compile_path = PACKER_COMPILED_PATH,
     display = {
-      prompt_border = "curved",
+      prompt_border = "rounded",
       open_cmd = "silent topleft 65vnew Packer"
     },
     profile = {
