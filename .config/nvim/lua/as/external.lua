393de490cd0bb6b88cc96399405d96182e4ce9ab
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-06-16 17:56:52 +0100

    refactor(external.lua): verify highlights loaded
    before running any functions

--- .config/nvim/lua/as/external.lua
+++ .config/nvim/lua/as/external.lua
@@ -3,6 +3,8 @@ local M = {
   kitty = {},
 }
 
+local highlights_loaded, H = pcall(require, "as.highlights")
+
 local fn = vim.fn
 local fmt = string.format
 local loaded, devicons = pcall(require, "nvim-web-devicons")
@@ -10,23 +12,29 @@ local loaded, devicons = pcall(require, "nvim-web-devicons")
 --- Get the color of the current vim background and update tmux accordingly
 ---@param reset boolean?
 function M.tmux.set_statusline(reset)
+  if not highlights_loaded then
+    return
+  end
   local hl = reset and "Normal" or "MsgArea"
-  local bg = require("as.highlights").hl_value(hl, "bg")
+  local bg = H.hl_value(hl, "bg")
   -- TODO: we should correctly derive the previous bg value
   fn.jobstart(fmt("tmux set-option -g status-style bg=%s", bg))
 end
 
 function M.kitty.set_background()
   if vim.env.KITTY_LISTEN_ON then
-    local bg = require("as.highlights").hl_value("MsgArea", "bg")
+    local bg = H.hl_value("MsgArea", "bg")
     fn.jobstart(fmt("kitty @ --to %s set-colors background=%s", vim.env.KITTY_LISTEN_ON, bg))
   end
 end
 
 ---Reset the kitty terminal colors
 function M.kitty.clear_background()
+  if not highlights_loaded then
+    return
+  end
   if vim.env.KITTY_LISTEN_ON then
-    local bg = require("as.highlights").hl_value("Normal", "bg")
+    local bg = H.hl_value("Normal", "bg")
     -- this is intentionally synchronous so it has time to execute fully
     fn.system(fmt("kitty @ --to %s set-colors background=%s", vim.env.KITTY_LISTEN_ON, bg))
   end
@@ -42,10 +50,12 @@ local function fileicon()
 end
 
 function M.title_string()
+  if not highlights_loaded then
+    return
+  end
   local dir = fn.fnamemodify(fn.getcwd(), ":t")
   local icon, hl = fileicon()
-  -- TODO make sure this doesn't block new systems
-  return fmt("%s #[fg=%s]%s ", dir, require("as.highlights").hl_value(hl, "fg"), icon)
+  return fmt("%s #[fg=%s]%s ", dir, H.hl_value(hl, "fg"), icon)
 end
 
 function M.tmux.clear_pane_title()
