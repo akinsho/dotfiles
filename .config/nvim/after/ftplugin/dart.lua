5f698ff5c463967e57f1117b72405f9e0152b8a2
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-06-17 11:49:29 +0100

    fix: remove heredoc from dart ftplugin

--- .config/nvim/after/ftplugin/dart.lua
+++ .config/nvim/after/ftplugin/dart.lua
@@ -1,24 +1,26 @@
-lua << EOF
 -- Open flutter only commands in dart files
-local success, wk = pcall(require, 'which-key')
-if not success then return end
+local success, wk = pcall(require, "which-key")
+if not success then
+  return
+end
 wk.register({
-  cc = {"<Cmd>Telescope flutter commands<CR>", "flutter: commands"},
+  cc = { "<Cmd>Telescope flutter commands<CR>", "flutter: commands" },
   d = {
     name = "+flutter",
-    d = {"<Cmd>FlutterDevices<CR>", "flutter: devices"},
+    d = { "<Cmd>FlutterDevices<CR>", "flutter: devices" },
     b = {
-        "<cmd>TermExec cmd='flutter pub run build_runner build --delete-conflicting-outputs'<CR>",
-        "flutter: run code generation"
+      "<cmd>TermExec cmd='flutter pub run build_runner build --delete-conflicting-outputs'<CR>",
+      "flutter: run code generation",
     },
-    e = {"<Cmd>FlutterEmulators<CR>", "flutter: emulators"},
-    o = {"<Cmd>FlutterOutline<CR>", "flutter: outline"},
-    q = {"<Cmd>FlutterQuit<CR>", "flutter: quit"},
+    e = { "<Cmd>FlutterEmulators<CR>", "flutter: emulators" },
+    o = { "<Cmd>FlutterOutline<CR>", "flutter: outline" },
+    q = { "<Cmd>FlutterQuit<CR>", "flutter: quit" },
     r = {
       name = "+dev-server",
-      n = {"<Cmd>FlutterRun<CR>", "run"},
-      s = {"<Cmd>FlutterRestart<CR>", "restart"}
-      }
-    }
-}, {prefix = "<leader>"})
-EOF
+      n = { "<Cmd>FlutterRun<CR>", "run" },
+      s = { "<Cmd>FlutterRestart<CR>", "restart" },
+    },
+  },
+}, {
+  prefix = "<leader>",
+})
