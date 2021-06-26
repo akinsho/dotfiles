2bfb32a8a2970c5d61f6a62e62d0bd3067c9adeb
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-06-26 13:09:57 +0100

    chore(settings): disable swap files again
    they just cause too much random noise

--- .config/nvim/lua/as/settings.lua
+++ .config/nvim/lua/as/settings.lua
@@ -234,7 +234,7 @@ if fn.isdirectory(vim.o.undodir) == 0 then
   fn.mkdir(vim.o.undodir, "p")
 end
 vim.opt.undofile = true
-vim.opt.swapfile = true
+vim.opt.swapfile = false
 -- The // at the end tells Vim to use the absolute path to the file to create the swap file.
 -- This will ensure that swap file name is unique, so there are no collisions between files
 -- with the same name from different directories.
