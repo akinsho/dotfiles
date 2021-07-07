1329ba00d4c159bb10cbac52393a9efcef399336
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-07-07 14:22:04 +0100

    feat(gitsigns): add visual mappings for hunks

--- .config/nvim/lua/as/plugins/gitsigns.lua
+++ .config/nvim/lua/as/plugins/gitsigns.lua
@@ -25,7 +25,6 @@ return function()
   }
 
   gitsigns.setup {
-    debug_mode = false,
     signs = {
       add = { hl = "GitGutterAdd", text = "▌" },
       change = { hl = "GitGutterChange", text = "▌" },
@@ -39,14 +38,8 @@ return function()
       -- Default keymap options
       noremap = true,
       buffer = true,
-      ["n [h"] = {
-        expr = true,
-        "&diff ? ']h' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'",
-      },
-      ["n ]h"] = {
-        expr = true,
-        "&diff ? '[h' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'",
-      },
+      ["n [h"] = { expr = true, "&diff ? ']h' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
+      ["n ]h"] = { expr = true, "&diff ? '[h' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
       ["n <localleader>gw"] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
       ["n <localleader>gre"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
       ["n <localleader>gbl"] = '<cmd>lua require"gitsigns".blame_line()<CR>',
@@ -55,8 +48,10 @@ return function()
       ["o ih"] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
       ["x ih"] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
       ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+      ["v <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
       ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
       ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+      ["v <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
       ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
     },
   }
