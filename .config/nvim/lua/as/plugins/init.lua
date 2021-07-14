2b24b276e740847c28affcd258690379e9d67dea
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-07-14 15:30:42 +0100

    chore(gitmessage): remove blank line from start

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -1009,7 +1009,6 @@ require('packer').startup {
     }
     use_local {
       'akinsho/nvim-bufferline.lua',
-      branch = 'feature/refactor-startup',
       config = conf 'nvim-bufferline',
       local_path = 'personal',
       requires = 'nvim-web-devicons',
