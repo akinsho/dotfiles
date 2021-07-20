3c5bcf0975ec2ccbffad5d92375196a8d447b50d
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-07-20 17:30:30 +0100

    chore(bufferline): use mtime sorter

--- .config/nvim/lua/as/plugins/nvim-bufferline.lua
+++ .config/nvim/lua/as/plugins/nvim-bufferline.lua
@@ -45,7 +45,7 @@ return function()
   require('bufferline').setup {
     options = {
       mappings = false,
-      sort_by = 'mru',
+      sort_by = sort_by_mtime,
       right_mouse_command = 'vert sbuffer %d',
       show_close_icon = false,
       ---based on https://github.com/kovidgoyal/kitty/issues/957
