e5b31b84cd2c92160e1bae2934f955110f74647c
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-07-21 15:19:10 +0100

    fix: disable lsp diagnostic cursor autocommand
    this overrides the hover popup I manually opened and is super annoying

--- .config/nvim/lua/as/plugins/lspconfig.lua
+++ .config/nvim/lua/as/plugins/lspconfig.lua
@@ -44,15 +44,16 @@ local function setup_autocommands(client, _)
       },
     })
   end
-  as.augroup('LspDiagnosticsCursor', {
-    {
-      events = { 'CursorHold', 'CursorHoldI' },
-      targets = { '*' },
-      command = function()
-        vim.lsp.diagnostic.show_line_diagnostics { border = 'rounded', focusable = false }
-      end,
-    },
-  })
+  -- FIXME: when the diagnostic popup opens it overrides the hover popup
+  -- as.augroup('LspDiagnosticsCursor', {
+  --   {
+  --     events = { 'CursorHold', 'CursorHoldI' },
+  --     targets = { '*' },
+  --     command = function()
+  --       vim.lsp.diagnostic.show_line_diagnostics { border = 'rounded', focusable = false }
+  --     end,
+  --   },
+  -- })
   if client and client.resolved_capabilities.document_formatting then
     -- format on save
     as.augroup('LspFormat', {
