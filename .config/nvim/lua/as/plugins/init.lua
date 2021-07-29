d357da29f54930c46f0489e1f82845392349c326
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-07-29 11:34:56 +0100

    feat(trouble): add next/previous jump mappings

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -256,6 +256,8 @@ require('packer').startup {
             d = 'trouble: toggle',
             r = 'trouble: lsp references',
           },
+          ['[d'] = 'trouble: next item',
+          [']d'] = 'trouble: previous item',
         }
       end,
       requires = 'nvim-web-devicons',
@@ -269,9 +271,16 @@ require('packer').startup {
           { 'TroubleFoldIcon', { guifg = 'yellow', gui = 'bold' } },
           { 'TroubleLocation', { guifg = H.get_hl('Comment', 'fg') } }
         )
+        local trouble = require 'trouble'
         as.nnoremap('<leader>ld', '<cmd>TroubleToggle lsp_workspace_diagnostics<CR>')
         as.nnoremap('<leader>lr', '<cmd>TroubleToggle lsp_references<CR>')
-        require('trouble').setup { auto_close = true, auto_preview = false }
+        as.nnoremap(']d', function()
+          trouble.previous { skip_groups = true, jump = true }
+        end)
+        as.nnoremap('[d', function()
+          trouble.next { skip_groups = true, jump = true }
+        end)
+        trouble.setup { auto_close = true, auto_preview = false }
       end,
     }
 
