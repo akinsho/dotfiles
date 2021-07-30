9f2d3f810b2bf58978ff43058364084729d93e68
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-07-30 11:45:50 +0100

    fix(autocommands.lua): fix commandline autocmd
    from overly aggressively clearing the command line because old timers
    were not cancelled so previous change timers would overlap with
    current/new timers

--- .config/nvim/plugin/autocommands.lua
+++ .config/nvim/plugin/autocommands.lua
@@ -128,18 +128,30 @@ as.augroup('Templates', {
 })
 
 --- automatically clear commandline messages after a few seconds delay
---- source: http//unix.stackexchange.com/a/613645
+--- source: http://unix.stackexchange.com/a/613645
+---@return function
+local function clear_commandline()
+  --- Track the timer object and stop any previous timers before setting
+  --- a new one so that each change waits for 10secs and that 10secs is
+  --- deferred each time
+  local timer
+  return function()
+    if timer then
+      timer:stop()
+    end
+    timer = vim.defer_fn(function()
+      if fn.mode() == 'n' then
+        vim.cmd [[echon '']]
+      end
+    end, 10000)
+  end
+end
+
 as.augroup('ClearCommandMessages', {
   {
     events = { 'CmdlineLeave', 'CmdlineChanged' },
     targets = { ':' },
-    command = function()
-      vim.defer_fn(function()
-        if fn.mode() == 'n' then
-          vim.cmd [[echon '']]
-        end
-      end, 10000)
-    end,
+    command = clear_commandline(),
   },
 })
 
