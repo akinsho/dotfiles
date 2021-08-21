10eb10974b7f22bf9b0787bc9441ddc6fcf7b577
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-08-22 00:23:38 +0100

    chore(nvim-tree): allow it to hijack netrw

--- .config/nvim/lua/as/plugins/nvim-tree.lua
+++ .config/nvim/lua/as/plugins/nvim-tree.lua
@@ -23,7 +23,7 @@ return function()
   vim.g.nvim_tree_width = '20%'
   vim.g.nvim_tree_width_allow_resize = 1
   vim.g.nvim_tree_disable_netrw = 0
-  vim.g.nvim_tree_hijack_netrw = 0
+  vim.g.nvim_tree_hijack_netrw = 1
   vim.g.nvim_tree_root_folder_modifier = ':t'
   vim.g.nvim_tree_ignore = { '.DS_Store', 'fugitive:', '.git' }
   vim.g.nvim_tree_highlight_opened_files = 1
