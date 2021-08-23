ee1b137959d89216a06cc18751eb96b45e9286f1
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-08-23 23:26:25 +0100

    fix(treesitter): add offset to fix vim injections

--- .config/nvim/queries/lua/injections.scm
+++ .config/nvim/queries/lua/injections.scm
@@ -2,8 +2,9 @@
   (function_call
     (field_expression
       (property_identifier) @_cmd_identifier)
-    (arguments (string) @vim)
+      (arguments (string) @vim
+      (#offset! @vim 0 1 0 -1)
+    )
   )
-
   (#eq? @_cmd_identifier "cmd")
 )
