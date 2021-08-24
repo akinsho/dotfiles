f6100409b008a6c5db31b35cccb7961fa6b6d6f3
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-08-24 17:39:16 +0100

    chore(toggleterm): add winblend to float terms

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -999,7 +999,7 @@ require('packer').startup {
           shade_filetypes = { 'none' },
           direction = 'vertical',
           start_in_insert = true,
-          float_opts = { border = 'curved' },
+          float_opts = { border = 'curved', winblend = 3 },
           size = function(term)
             if term.direction == 'horizontal' then
               return 15
