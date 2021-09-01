a37934e9d9c981d39e90b346079f7cdbd779a458
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-09-01 12:57:41 +0100

    fix(multicursors): update theme
    To make cursors more legible

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -535,6 +535,7 @@ require('packer').startup {
       'mg979/vim-visual-multi',
       config = function()
         vim.g.VM_highlight_matches = 'underline'
+        vim.g.VM_theme = 'codedark'
         vim.g.VM_maps = {
           ['Find Under'] = '<C-e>',
           ['Find Subword Under'] = '<C-e>',
