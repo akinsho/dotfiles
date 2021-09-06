b4638a17a498ef4cd93aa98772a177260bdefa15
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-09-06 23:43:36 +0100

    feat(diffview): enabled enhanced hl

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -843,6 +843,7 @@ require('packer').startup {
       config = function()
         as.nnoremap('<localleader>gd', '<Cmd>DiffviewOpen<CR>')
         require('diffview').setup {
+          enhanced_diff_hl = true,
           key_bindings = {
             file_panel = { q = '<Cmd>DiffviewClose<CR>' },
             view = { q = '<Cmd>DiffviewClose<CR>' },
