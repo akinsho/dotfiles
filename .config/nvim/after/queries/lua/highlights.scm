ec367e09ed0d5139f99731fa3c720730a31233c6
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-09-25 18:42:15 +0100

    fix(treesitter): disable lua viml injections
    as these queries cause nvim to hang when certain vimscript lines are
    encountered. See: https://github.com/vigoux/tree-sitter-viml/issues/29
    for details

--- .config/nvim/after/queries/lua/highlights.scm
+++ .config/nvim/after/queries/lua/highlights.scm
@@ -1,7 +1,8 @@
-(
-  (function_call
-    (field_expression (property_identifier) @_cmd_identifier)
-    (arguments (string) @none)
-  )
-  (#eq? @_cmd_identifier "cmd")
-)
+; FIXME: disable until https://github.com/vigoux/tree-sitter-viml/issues/29 is fixed
+; (
+;   (function_call
+;     (field_expression (property_identifier) @_cmd_identifier)
+;     (arguments (string) @none)
+;   )
+;   (#eq? @_cmd_identifier "cmd")
+; )
--- .config/nvim/queries/lua/injections.scm
+++ .config/nvim/queries/lua/injections.scm
@@ -1,7 +1,22 @@
-(
-  (function_call
-    (field_expression (property_identifier) @_cmd_identifier)
-    (arguments (string) @vim (#offset! @vim 0 1 0 -1))
-  )
-  (#eq? @_cmd_identifier "cmd")
-)
+; FIXME: disable until https://github.com/vigoux/tree-sitter-viml/issues/29 is fixed
+;(
+;  (function_call
+;    (field_expression (property_identifier) @_cmd_identifier)
+;    (arguments (string) @vim)
+;  )
+
+;  (#eq? @_cmd_identifier "cmd")
+;  (#match? @vim "^[\"']")
+;  (#offset! @vim 0 1 0 -1)
+;)
+
+;(
+;  (function_call
+;    (field_expression (property_identifier) @_cmd_identifier)
+;    (arguments (string) @vim)
+;  )
+
+;  (#eq? @_cmd_identifier "cmd")
+;  (#match? @vim "^\\[\\[")
+;  (#offset! @vim 0 2 0 -2)
+;)
