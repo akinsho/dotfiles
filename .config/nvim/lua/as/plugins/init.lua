f1d2b8bcd15ca56ab3977ff9f5dda75b999d3b0e
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-10-04 15:38:19 +0100

    fix(ultest): override ([/])t in test files

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -416,11 +416,11 @@ require('packer').startup {
             command = 'UltestNearest',
           },
         })
-        as.nmap(']T', '<Plug>(ultest-next-fail)', {
+        as.nmap(']t', '<Plug>(ultest-next-fail)', {
           label = 'ultest: next failure',
           buffer = 0,
         })
-        as.nmap('[T', '<Plug>(ultest-prev-fail)', {
+        as.nmap('[t', '<Plug>(ultest-prev-fail)', {
           label = 'ultest: previous failure',
           buffer = 0,
         })
