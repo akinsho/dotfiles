c1e3ea5d4fdb308a55d021e9b9af8470c9fc927f
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-10-07 11:41:34 +0100

    fix(autocommands): change event for cursor last place
    based on the vim-lastplace plugin

--- .config/nvim/plugin/autocommands.lua
+++ .config/nvim/plugin/autocommands.lua
@@ -352,7 +352,7 @@ as.augroup('Utilities', {
     -- When editing a file, always jump to the last known cursor position.
     -- Don't do it for commit messages, when the position is invalid, or when
     -- inside an event handler (happens when dropping a file on gvim).
-    events = { 'BufReadPost' },
+    events = { 'BufWinEnter' },
     targets = { '*' },
     command = function()
       local pos = fn.line [['"]]
