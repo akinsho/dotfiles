0d3f7622022d5ff0e395cda7a119f1ca4a11ba04
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-10-08 10:45:51 +0100

    fix(doom-one): remove setup function

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -1072,21 +1072,7 @@ require('packer').startup {
     --------------------------------------------------------------------------------
     -- Themes  {{{1
     --------------------------------------------------------------------------------
-    use {
-      'NTBBloodbath/doom-one.nvim',
-      config = function()
-        require('doom-one').setup {
-          plugins_integrations = {
-            telescope = false,
-            neogit = true,
-            gitsigns = true,
-            nvim_tree = true,
-            whichkey = true,
-            indent_blankline = true,
-          },
-        }
-      end,
-    }
+    use 'NTBBloodbath/doom-one.nvim'
     use 'monsonjeremy/onedark.nvim'
     use 'marko-cerovac/material.nvim'
     use 'projekt0n/github-nvim-theme'
