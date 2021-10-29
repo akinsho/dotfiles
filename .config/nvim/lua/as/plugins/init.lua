c81dadf0c570ce39543a9b43a75f41256ecd03fc
Akin Sowemimo <akin@email.com>
2021-10-29 10:56:37 +0100

    fix(autopairs): remove deprecated setup

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -519,11 +519,6 @@ require('packer').startup {
           close_triple_quotes = true,
           check_ts = false,
         }
-        require('nvim-autopairs.completion.cmp').setup {
-          map_cr = true, --  map <CR> on insert mode
-          map_complete = true, -- it will auto insert `(` after select function or method item
-          auto_select = true, -- automatically select the first item
-        }
       end,
     }
     use {
