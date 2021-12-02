a7b18b235d20c1f8cc4322ab04e049f70e2f3e86
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-12-02 09:30:02 +0000

    REFACTOR(treesittter): remove custom queries

--- .config/nvim/after/queries/lua/highlights.scm
+++
@@ -1,8 +0,0 @@
-; FIXME: disable until https://github.com/vigoux/tree-sitter-viml/issues/29 is fixed
-; (
-;   (function_call
-;     (field_expression (property_identifier) @_cmd_identifier)
-;     (arguments (string) @none)
-;   )
-;   (#eq? @_cmd_identifier "cmd")
-; )
