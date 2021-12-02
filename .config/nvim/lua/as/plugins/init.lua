f714d4cdd2de74c7393ca3ae69bdbb3619e06174
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-12-02 09:32:48 +0000

    chore(plugins): remove fastaction
    it no longer correctly triggers the code action function. I now use
    telescope ui select which does

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -291,42 +291,6 @@ require('packer').startup {
     }
 
     use {
-      'windwp/lsp-fastaction.nvim',
-      local_path = 'contributing',
-      disable = true,
-      config = function()
-        local fastaction = require 'lsp-fastaction'
-        fastaction.setup {
-          action_data = {
-            dart = {
-              { pattern = 'import library', key = 'i', order = 1 },
-              { pattern = 'wrap with widget', key = 'w', order = 2 },
-              { pattern = 'column', key = 'c', order = 3 },
-              { pattern = 'row', key = 'r', order = 3 },
-              { pattern = 'container', key = 'C', order = 4 },
-              { pattern = 'center', key = 'E', order = 4 },
-              { pattern = 'padding', key = 'p', order = 4 },
-              { pattern = 'remove', key = 'r', order = 5 },
-              -- range code action
-              { pattern = "surround with %'if'", key = 'i', order = 2 },
-              { pattern = 'try%-catch', key = 't', order = 2 },
-              { pattern = 'for%-in', key = 'f', order = 2 },
-              { pattern = 'setstate', key = 's', order = 2 },
-            },
-          },
-        }
-        as.xnoremap(
-          '<leader>ca',
-          "<esc><Cmd>lua require('lsp-fastaction').range_code_action()<CR>",
-          'lsp: code action'
-        )
-        require('which-key').register {
-          ['<leader>ca'] = { fastaction.code_action, 'lsp: code action' },
-        }
-      end,
-    }
-
-    use {
       'ray-x/lsp_signature.nvim',
       config = function()
         require('lsp_signature').setup {
