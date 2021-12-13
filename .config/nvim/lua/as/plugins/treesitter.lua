2009cc63dc17095d2764dfd6e7f7d7dc1a7b432a
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-12-13 19:44:13 +0000

    chore(orgmode): use stable revision

--- .config/nvim/lua/as/plugins/treesitter.lua
+++ .config/nvim/lua/as/plugins/treesitter.lua
@@ -25,7 +25,7 @@ return function()
   parser_configs.org = {
     install_info = {
       url = 'https://github.com/milisims/tree-sitter-org',
-      revision = 'main',
+      revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
       files = { 'src/parser.c', 'src/scanner.cc' },
     },
     filetype = 'org',
