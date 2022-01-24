513e85da500542f2e658878812b9638802393686
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-01-24 10:11:59 +0000

    chore(treesitter): remove unused parser setup

--- .config/nvim/lua/as/plugins/treesitter.lua
+++ .config/nvim/lua/as/plugins/treesitter.lua
@@ -1,12 +1,5 @@
 return function()
   local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-  parser_configs.norg = {
-    install_info = {
-      url = 'https://github.com/vhyrro/tree-sitter-norg',
-      files = { 'src/parser.c', 'src/scanner.cc' },
-      branch = 'main',
-    },
-  }
   parser_configs.norg_meta = {
     install_info = {
       url = 'https://github.com/nvim-neorg/tree-sitter-norg-meta',
