a49e92e4fdc602505e14508eb7640fa8850af540
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-02-16 23:14:36 +0000

    fix(telescope): disable prefix whilst buggy

--- .config/nvim/lua/as/plugins/telescope.lua
+++ .config/nvim/lua/as/plugins/telescope.lua
@@ -40,7 +40,11 @@ return function()
     defaults = {
       set_env = { ['TERM'] = vim.env.TERM },
       borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
-      prompt_prefix = ' ',
+      -- BUG: remove prefix as it is currently broken
+      -- seems to relate to prompt buffers although currently it isn't being set to
+      -- a prompt buffer
+      --@see: https://github.com/nvim-telescope/telescope.nvim/issues/1251
+      prompt_prefix = '', -- 
       selection_caret = '» ',
       mappings = {
         i = {
