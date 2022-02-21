a0788690c977df7e96c432c2c651e414650a5f85
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-02-21 15:00:03 +0000

    refactor(ftdetect): move into filetype.lua

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -7,5 +7,8 @@ vim.g.do_filetype_lua = 1
 vim.filetype.add {
   filename = {
     ['.gitignore'] = 'conf',
+    ['*.gradle'] = 'groovy',
+    ['Podfile'] = 'ruby',
+    ['*.env.*'] = 'sh'
   },
 }
--- .config/nvim/ftdetect/ruby.vim
+++
@@ -1,1 +0,0 @@
-au BufRead,BufNewFile Podfile setfiletype ruby
