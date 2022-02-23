e1de147c59da7d43fdb5a8ef88e10d639edd9f66
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-02-23 12:07:45 +0000

    fix(filetype): use patterns key

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -7,8 +7,10 @@ vim.g.do_filetype_lua = 1
 vim.filetype.add {
   filename = {
     ['.gitignore'] = 'conf',
-    ['*.gradle'] = 'groovy',
     ['Podfile'] = 'ruby',
-    ['*.env.*'] = 'sh'
   },
+  pattern = {
+    ['*.gradle'] = 'groovy',
+    ['*.env.*'] = 'env'
+  }
 }
