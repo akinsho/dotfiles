0bd8a7150f30ff2e81e5a75675fe09de08a7e863
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-02-23 09:17:14 +0000

    feat(lsp): add show message handler using notify

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -147,3 +147,15 @@ lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_hel
   max_width = max_width,
   max_height = max_height,
 })
+
+lsp.handlers['window/showMessage'] = function(_, result, ctx)
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
+  local lvl = ({ 'ERROR', 'WARN', 'INFO', 'DEBUG' })[result.type]
+  vim.notify(result.message, lvl, {
+    title = 'LSP | ' .. client.name,
+    timeout = 10000,
+    keep = function()
+      return lvl == 'ERROR' or lvl == 'WARN'
+    end,
+  })
+end
