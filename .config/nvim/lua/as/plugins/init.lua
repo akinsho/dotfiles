fa5f0a59585670923ded0ce727ab7e3a524c17f6
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-02-24 09:09:36 +0000

    chore(dressing): reduce winblend

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -403,8 +403,10 @@ require('packer').startup {
         require('dressing').setup {
           input = {
             insert_only = false,
+            winblend = 2,
           },
           select = {
+            winblend = 2,
             telescope = {
               theme = 'dropdown',
             },
