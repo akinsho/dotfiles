834152e2316a1bb2a121f4867ff7afb5198b9a19
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-02-28 19:26:19 +0000

    feat(plugins): trial dial.nvim

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -526,6 +526,46 @@ require('packer').startup {
     }
 
     use {
+      'monaqa/dial.nvim',
+      config = function()
+        local dial = require 'dial.map'
+        local augend = require 'dial.augend'
+        local map = vim.keymap.set
+        map('n', '<C-a>', dial.inc_normal(), { remap = false })
+        map('n', '<C-x>', dial.dec_normal(), { remap = false })
+        map('v', '<C-a>', dial.inc_visual(), { remap = false })
+        map('v', '<C-x>', dial.dec_visual(), { remap = false })
+        map('v', 'g<C-a>', dial.inc_gvisual(), { remap = false })
+        map('v', 'g<C-x>', dial.dec_gvisual(), { remap = false })
+        require('dial.config').augends:register_group {
+          -- default augends used when no group name is specified
+          default = {
+            augend.integer.alias.decimal,
+            augend.integer.alias.hex,
+            augend.date.alias['%Y/%m/%d'],
+            augend.constant.alias.bool,
+            augend.constant.new {
+              elements = { '&&', '||' },
+              word = false,
+              cyclic = true,
+            },
+          },
+          dep_files = {
+            augend.semver.alias.semver,
+          },
+        }
+
+        vim.api.nvim_create_autocmd {
+          pattern = { 'yaml', 'toml' },
+          event = 'FileType',
+          callback = function()
+            map('n', '<C-a>', require('dial.map').inc_normal 'dep_files', { remap = true })
+          end,
+        }
+      end,
+    }
+
+    use {
       'arecarn/vim-fold-cycle',
       config = function()
         vim.g.fold_cycle_default_mapping = 0
