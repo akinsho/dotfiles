77928b140e8e3bd108b577e7ef273720bd80a273
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-15 16:11:41 +0000

    chore(beacon): replace specs with beacon
    as beacon doesn't have the same issues with duplicate windows

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -534,23 +534,10 @@ packer.startup {
       end,
     }
 
-    use {
-      'edluffy/specs.nvim',
-      config = function()
-        -- NOTE: 'DanilaMihailov/beacon.nvim' is an alternative
-        local specs = require 'specs'
-        specs.setup {
-          popup = {
-            delay_ms = 10,
-            inc_ms = 10,
-            blend = 10,
-            width = 50,
-            winhl = 'PmenuSbar',
-            resizer = specs.slide_resizer,
-          },
-        }
-      end,
-    }
+    -- NOTE: 'edluffy/specs.nvim' is an alternative
+    -- except it doesn't play nicely with telescope
+    -- showing duplicate beacons and isn't well maintained
+    use 'DanilaMihailov/beacon.nvim'
 
     use {
       'monaqa/dial.nvim',
