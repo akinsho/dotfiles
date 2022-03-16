d3fb809816ff33f81c36e2ae7d07eb122cceb53f
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-16 22:41:20 +0000

    revert(headlines.nvim): re-add as I miss it
    This reverts commit d017b92490a016cc2a39e0bc879b6d85482847b2.

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -798,6 +798,31 @@ packer.startup {
       end,
     }
 
+    use {
+      'lukas-reineke/headlines.nvim',
+      setup = function()
+        -- https://observablehq.com/@d3/color-schemes?collection=@d3/d3-scale-chromatic
+        -- NOTE: this must be set in the setup function or it will crash nvim...
+        require('as.highlights').plugin(
+          'Headlines',
+          { 'Headline1', { background = '#003c30', foreground = 'White' } },
+          { 'Headline2', { background = '#00441b', foreground = 'White' } },
+          { 'Headline3', { background = '#084081', foreground = 'White' } },
+          { 'Dash', { background = '#0b60a1', bold = true } }
+        )
+      end,
+      config = function()
+        require('headlines').setup {
+          markdown = {
+            headline_highlights = { 'Headline1', 'Headline2', 'Headline3' },
+          },
+          yaml = {
+            dash_pattern = '^---+$',
+            dash_highlight = 'Dash',
+          },
+        }
+      end,
+    }
     -- }}}
     --------------------------------------------------------------------------------
     -- Profiling & Startup {{{1
