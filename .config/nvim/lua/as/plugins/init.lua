75f593598096bb6f6acadf91d8ccc4d6cc1a4990
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-16 11:52:11 +0000

    refactor(dapui): require module when needed
    rather than saving it to a variable

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -411,20 +411,24 @@ packer.startup {
           'rcarriga/nvim-dap-ui',
           after = 'nvim-dap',
           config = function()
-            local dapui = require 'dapui'
-            dapui.setup()
-            as.nnoremap('<localleader>duc', dapui.close, 'dap-ui: close')
-            as.nnoremap('<localleader>dut', dapui.toggle, 'dap-ui: toggle')
-            local dap = require 'dap'
+            require('dapui').setup()
+            as.nnoremap('<localleader>duc', function()
+              require('dapui').close()
+            end, 'dap-ui: close')
+            as.nnoremap('<localleader>dut', function()
+              require('dapui').toggle()
+            end, 'dap-ui: toggle')
+
             -- NOTE: this opens dap UI automatically when dap starts
+            local dap = require 'dap'
             -- dap.listeners.after.event_initialized['dapui_config'] = function()
             --   dapui.open()
             -- end
             dap.listeners.before.event_terminated['dapui_config'] = function()
-              dapui.close()
+              require('dapui').close()
             end
             dap.listeners.before.event_exited['dapui_config'] = function()
-              dapui.close()
+              require('dapui').close()
             end
           end,
         },
