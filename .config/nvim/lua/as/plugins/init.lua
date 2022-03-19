8af8ebe71cd2d3ec327da4e412355cb2dc594aaf
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-19 11:00:08 +0000

    feat(marks): add some mappings to encourage usage

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -552,6 +552,14 @@ packer.startup {
       'chentau/marks.nvim',
       config = function()
         require('as.highlights').plugin('marks', { 'MarkSignHL', { foreground = 'Red' } })
+        require('which-key').register({
+          m = {
+            name = '+marks',
+            b = { '<Cmd>MarksListBuf<CR>', 'list buffer' },
+            g = { '<Cmd>MarksQFListGlobal<CR>', 'list global' },
+            ['0'] = { '<Cmd>BookmarksQFList 0<CR>', 'list bookmark' },
+          },
+        }, { prefix = '<leader>' })
         require('marks').setup {
           bookmark_0 = {
             sign = '⚑',
