b836f5f9acd97b1c3ccf73feeba94bc2331197d9
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-20 20:47:55 +0000

    refactor(dressing): use telescope's max lines

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -488,11 +488,13 @@ packer.startup {
           select = {
             telescope = require('telescope.themes').get_cursor {
               layout_config = {
-                height = function(self, _, _)
+                -- NOTE: the limit is half the max lines because this is the cursor theme so
+                -- unless the cursor is at the top or bottom it realistically most often will
+                -- only have half the screen available
+                height = function(self, _, max_lines)
                   local results = #self.finder.results
-                  local tabline, statusline = 1, 1
-                  local PADDING = 4 -- NOTE: this represents the size of the telescope window
-                  local LIMIT = math.floor((vim.o.lines - tabline - statusline) / 2)
+                  local PADDING = 4 -- this represents the size of the telescope window
+                  local LIMIT = math.floor(max_lines / 2)
                   return (results <= (LIMIT - PADDING) and results + PADDING or LIMIT)
                 end,
               },
