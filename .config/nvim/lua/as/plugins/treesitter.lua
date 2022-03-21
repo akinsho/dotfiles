637bfea51e44f50ff743d5419a71b8d5c989b999
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-21 17:17:29 +0000

    refactor(treesitter): remove neorg parser config
    since this is now handled internally by norg

--- .config/nvim/lua/as/plugins/treesitter.lua
+++ .config/nvim/lua/as/plugins/treesitter.lua
@@ -1,21 +1,4 @@
 return function()
-  local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-  parser_configs.norg_meta = {
-    install_info = {
-      url = 'https://github.com/nvim-neorg/tree-sitter-norg-meta',
-      files = { 'src/parser.c' },
-      branch = 'main',
-    },
-  }
-
-  parser_configs.norg_table = {
-    install_info = {
-      url = 'https://github.com/nvim-neorg/tree-sitter-norg-table',
-      files = { 'src/parser.c' },
-      branch = 'main',
-    },
-  }
-
   require('nvim-treesitter.configs').setup {
     ensure_installed = 'maintained',
     highlight = {
