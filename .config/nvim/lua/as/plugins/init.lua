61e2576f57bda3efaddab0f6f4fc899ffa6cc3b7
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-24 18:15:10 +0000

    refactor(plugins): pull out notify config

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -572,30 +572,8 @@ packer.startup {
       end,
     }
 
-    use {
-      'rcarriga/nvim-notify',
-      cond = utils.not_headless, -- TODO: causes blocking output in headless mode
-      config = function()
-        -- this plugin is not safe to reload
-        if vim.g.packer_compiled_loaded then
-          return
-        end
-        local notify = require 'notify'
-        ---@type table<string, fun(bufnr: number, notif: table, highlights: table)>
-        local renderer = require 'notify.render'
-        notify.setup {
-          stages = 'fade_in_slide_out',
-          timeout = 3000,
-          render = function(bufnr, notif, highlights)
-            local style = notif.title[1] == '' and 'minimal' or 'default'
-            renderer[style](bufnr, notif, highlights)
-          end,
-        }
-        vim.notify = notify
-        require('telescope').load_extension 'notify'
-        as.nnoremap('<leader>nd', notify.dismiss, { label = 'dismiss notifications' })
-      end,
-    }
+    -- TODO: causes blocking output in headless mode
+    use { 'rcarriga/nvim-notify', cond = utils.not_headless, config = conf 'notify' }
 
     use {
       'mbbill/undotree',
