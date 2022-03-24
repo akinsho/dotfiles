eeb3a9ea5034565669a093a0c454ad35f0459a13
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-24 09:59:25 +0000

    chore(plugins): factor out toggleterm config

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -1271,61 +1271,9 @@ packer.startup {
     use_local {
       'akinsho/toggleterm.nvim',
       local_path = 'personal',
-      config = function()
-        require('toggleterm').setup {
-          open_mapping = [[<c-\>]],
-          shade_filetypes = { 'none' },
-          direction = 'horizontal',
-          insert_mappings = false,
-          start_in_insert = true,
-          float_opts = { border = 'curved', winblend = 3 },
-          size = function(term)
-            if term.direction == 'horizontal' then
-              return 15
-            elseif term.direction == 'vertical' then
-              return math.floor(vim.o.columns * 0.4)
-            end
-          end,
-        }
-
-        local float_handler = function(term)
-          if vim.fn.mapcheck('jk', 't') ~= '' then
-            vim.api.nvim_buf_del_keymap(term.bufnr, 't', 'jk')
-            vim.api.nvim_buf_del_keymap(term.bufnr, 't', '<esc>')
-          end
-        end
-
-        local Terminal = require('toggleterm.terminal').Terminal
-
-        local lazygit = Terminal:new {
-          cmd = 'lazygit',
-          dir = 'git_dir',
-          hidden = true,
-          direction = 'float',
-          on_open = float_handler,
-        }
-
-        local htop = Terminal:new {
-          cmd = 'htop',
-          hidden = 'true',
-          direction = 'float',
-          on_open = float_handler,
-        }
-
-        as.command('Htop', function()
-          htop:toggle()
-        end)
-
-        require('which-key').register {
-          ['<leader>lg'] = {
-            function()
-              lazygit:toggle()
-            end,
-            'toggleterm: toggle lazygit',
-          },
-        }
-      end,
+      config = conf 'toggleterm',
     }
+
     use_local {
       'akinsho/bufferline.nvim',
       config = conf 'bufferline',
