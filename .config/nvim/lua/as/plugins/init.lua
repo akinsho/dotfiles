e66fc2e48564bfc26dd7944b7f8e7c8215557a9e
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-24 18:03:39 +0000

    refactor(neorg): factor out config

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -788,51 +788,7 @@ packer.startup {
       'vhyrro/neorg',
       -- tag = '*', FIXME: add tag once neorg reaches 0.1
       requires = { 'vhyrro/neorg-telescope' },
-      config = function()
-        as.nnoremap('<localleader>oc', '<Cmd>Neorg gtd capture<CR>')
-        as.nnoremap('<localleader>ov', '<Cmd>Neorg gtd views<CR>')
-        require('neorg').setup {
-          configure_parsers = true,
-          load = {
-            ['core.defaults'] = {},
-            ['core.integrations.telescope'] = {},
-            ['core.keybinds'] = {
-              config = {
-                default_keybinds = true,
-                neorg_leader = '<localleader>',
-                hook = function(keybinds)
-                  keybinds.unmap('norg', 'n', '<C-s>')
-                  keybinds.map_event(
-                    'norg',
-                    'n',
-                    '<C-x>',
-                    'core.integrations.telescope.find_linkable'
-                  )
-                end,
-              },
-            },
-            ['core.norg.completion'] = {
-              config = {
-                engine = 'nvim-cmp',
-              },
-            },
-            ['core.norg.concealer'] = {},
-            ['core.norg.dirman'] = {
-              config = {
-                workspaces = {
-                  notes = vim.fn.expand '$SYNC_DIR/neorg/main/',
-                  tasks = vim.fn.expand '$SYNC_DIR/neorg/tasks/',
-                },
-              },
-            },
-            ['core.gtd.base'] = {
-              config = {
-                workspace = 'tasks',
-              },
-            },
-          },
-        }
-      end,
+      config = conf('neorg'),
     }
 
     use {
---
+++ .config/nvim/lua/as/plugins/neorg.lua
@@ -0,0 +1,40 @@
+return function()
+  as.nnoremap('<localleader>oc', '<Cmd>Neorg gtd capture<CR>')
+  as.nnoremap('<localleader>ov', '<Cmd>Neorg gtd views<CR>')
+  require('neorg').setup {
+    configure_parsers = true,
+    load = {
+      ['core.defaults'] = {},
+      ['core.integrations.telescope'] = {},
+      ['core.keybinds'] = {
+        config = {
+          default_keybinds = true,
+          neorg_leader = '<localleader>',
+          hook = function(keybinds)
+            keybinds.unmap('norg', 'n', '<C-s>')
+            keybinds.map_event('norg', 'n', '<C-x>', 'core.integrations.telescope.find_linkable')
+          end,
+        },
+      },
+      ['core.norg.completion'] = {
+        config = {
+          engine = 'nvim-cmp',
+        },
+      },
+      ['core.norg.concealer'] = {},
+      ['core.norg.dirman'] = {
+        config = {
+          workspaces = {
+            notes = vim.fn.expand '$SYNC_DIR/neorg/main/',
+            tasks = vim.fn.expand '$SYNC_DIR/neorg/tasks/',
+          },
+        },
+      },
+      ['core.gtd.base'] = {
+        config = {
+          workspace = 'tasks',
+        },
+      },
+    },
+  }
+end
