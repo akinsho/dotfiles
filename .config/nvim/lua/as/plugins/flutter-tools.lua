6ad9fa295e9294fae0b0e60085907abeeb91e95a
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-24 18:01:43 +0000

    refactor(plugins) :factor out flutter tools config

---
+++ .config/nvim/lua/as/plugins/flutter-tools.lua
@@ -0,0 +1,27 @@
+return function()
+  require('flutter-tools').setup {
+    ui = { border = as.style.border.line },
+    debugger = {
+      enabled = true,
+      run_via_dap = true,
+    },
+    outline = { auto_open = false },
+    decorations = {
+      statusline = { device = true, app_version = true },
+    },
+    widget_guides = { enabled = true, debug = true },
+    dev_log = { enabled = false, open_cmd = 'tabedit' },
+    lsp = {
+      color = {
+        enabled = true,
+        background = true,
+        virtual_text = false,
+      },
+      settings = {
+        showTodos = true,
+        renameFilesWithClasses = 'prompt',
+      },
+      on_attach = as.lsp and as.lsp.on_attach or nil,
+    },
+  }
+end
