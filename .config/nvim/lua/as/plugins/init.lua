7c3645ed4c38f7ba4fbf69b3ff4d6269c84211af
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-25 07:59:53 +0000

    refactor(plugins): factor out surround-funk config

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -874,43 +874,7 @@ packer.startup {
       end,
     }
 
-    use {
-      'Matt-A-Bennett/vim-surround-funk',
-      config = function()
-        vim.g.surround_funk_create_mappings = 0
-        local map = vim.keymap.set
-        -- operator pending mode: grip surround
-        map({ 'n', 'v' }, 'gs', '<Plug>(GripSurroundObject)')
-        map({ 'n', 'v' }, 'gS', '<Plug>(GripSurroundObjectNoPaste)')
-        map({ 'o', 'x' }, 'sF', '<Plug>(SelectWholeFUNCTION)')
-        require('which-key').register {
-          y = {
-            name = '+ysf: yank ',
-            s = {
-              f = { '<Plug>(YankSurroundingFUNCTION)', 'yank surrounding function call' },
-              F = {
-                '<Plug>(YankSurroundingFunction)',
-                'yank surrounding function call (partial)',
-              },
-            },
-          },
-          d = {
-            name = '+dsf: function text object',
-            s = {
-              F = { '<Plug>(DeleteSurroundingFunction)', 'delete surrounding function' },
-              f = { '<Plug>(DeleteSurroundingFUNCTION)', 'delete surrounding outer function' },
-            },
-          },
-          c = {
-            name = '+dsf: function text object',
-            s = {
-              F = { '<Plug>(ChangeSurroundingFunction)', 'change surrounding function' },
-              f = { '<Plug>(ChangeSurroundingFUNCTION)', 'change outer surrounding function' },
-            },
-          },
-        }
-      end,
-    }
+    use { 'Matt-A-Bennett/vim-surround-funk', config = conf 'surround-funk' }
 
     use {
       'protex/better-digraphs.nvim',
