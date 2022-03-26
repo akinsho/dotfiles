522272d48f90553f301de480cbf4255926cfaa59
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-26 12:31:43 +0000

    refactor(luasnippets/all): add comment to todo
    snippet

--- .config/nvim/luasnippets/all.lua
+++ .config/nvim/luasnippets/all.lua
@@ -2,12 +2,19 @@
 
 return {
   snippet({ trig = 'td', name = 'TODO' }, {
-    c(1, {
-      t 'TODO: ',
-      t 'FIXME: ',
-      t 'HACK: ',
-      t 'BUG: ',
-    }),
+    d(1, function()
+      local function with_cmt(cmt)
+        return string.format(vim.bo.commentstring, ' ' .. cmt)
+      end
+      return snippet('', {
+        c(1, {
+          t(with_cmt 'TODO: '),
+          t(with_cmt 'FIXME: '),
+          t(with_cmt 'HACK: '),
+          t(with_cmt 'BUG: '),
+        }),
+      })
+    end),
     i(0),
   }),
   snippet(
