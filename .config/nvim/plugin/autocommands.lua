1fe961ffcbdad7bb7e1596f62285f9e415de44c3
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-28 23:56:44 +0100

    fix(autocommands): correctly jump to last position

--- .config/nvim/plugin/autocommands.lua
+++ .config/nvim/plugin/autocommands.lua
@@ -352,22 +352,17 @@ as.augroup('Utilities', {
       vim.cmd(fmt('bd!|edit %s', vim.uri_from_fname '<afile>'))
     end,
   },
-  -- BUG: this causes the cursor to jump to the top on VimEnter
   {
     -- When editing a file, always jump to the last known cursor position.
-    -- Don't do it for commit messages, when the position is invalid, or when
-    -- inside an event handler (happens when dropping a file on gvim).
+    -- Don't do it for commit messages, when the position is invalid.
     event = { 'BufWinEnter' },
     pattern = { '*' },
     command = function()
-      local pos = fn.line [['"]]
-      if
-        vim.bo.ft ~= 'gitcommit'
-        and vim.fn.win_gettype() ~= 'popup'
-        and pos > 0
-        and pos <= fn.line '$'
-      then
-        vim.cmd 'keepjumps normal g`"'
+      if vim.bo.ft ~= 'gitcommit' and vim.fn.win_gettype() ~= 'popup' then
+        local row, col = unpack(api.nvim_buf_get_mark(0, '"'))
+        if { row, col } ~= { 0, 0 } then
+          api.nvim_win_set_cursor(0, { row, 0 })
+        end
       end
     end,
   },
