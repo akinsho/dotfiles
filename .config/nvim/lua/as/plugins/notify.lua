97d361c3f8fcdf508dc7147c94cc778899bf96db
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-30 13:23:02 +0100

    feat(nvim-notify): use current border styles

--- .config/nvim/lua/as/plugins/notify.lua
+++ .config/nvim/lua/as/plugins/notify.lua
@@ -1,13 +1,28 @@
 return function()
+  local api = vim.api
   -- this plugin is not safe to reload
   if vim.g.packer_compiled_loaded then
     return
   end
+  require('as.highlights').plugin(
+    'notify',
+    { 'NotifyERRORBody', { link = 'NormalFloat' } },
+    { 'NotifyWARNBody', { link = 'NormalFloat' } },
+    { 'NotifyINFOBody', { link = 'NormalFloat' } },
+    { 'NotifyDEBUGBody', { link = 'NormalFloat' } },
+    { 'NotifyTRACEBody', { link = 'NormalFloat' } }
+  )
   local notify = require 'notify'
   ---@type table<string, fun(bufnr: number, notif: table, highlights: table)>
   local renderer = require 'notify.render'
   notify.setup {
+    background_colour = 'NormalFloat',
     stages = 'fade_in_slide_out',
+    on_open = function(win)
+      if api.nvim_win_is_valid(win) then
+        vim.api.nvim_win_set_config(win, { border = as.style.current.border })
+      end
+    end,
     timeout = 3000,
     render = function(bufnr, notif, highlights)
       local style = notif.title[1] == '' and 'minimal' or 'default'
