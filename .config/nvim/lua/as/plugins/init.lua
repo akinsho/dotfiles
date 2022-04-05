65179784000718cc90d6d110c99d50eff3a0cf18
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-05 14:07:21 +0100

    chore(plugins): autoformat

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -663,15 +663,12 @@ packer.startup {
       setup = function()
         -- https://observablehq.com/@d3/color-schemes?collection=@d3/d3-scale-chromatic
         -- NOTE: this must be set in the setup function or it will crash nvim...
-        require('as.highlights').plugin(
-          'Headlines',
-          {
-            Headline1 = { background = '#003c30', foreground = 'White' },
-            Headline2 = { background = '#00441b', foreground = 'White' },
-            Headline3 = { background = '#084081', foreground = 'White' },
-            Dash = { background = '#0b60a1', bold = true },
-          }
-        )
+        require('as.highlights').plugin('Headlines', {
+          Headline1 = { background = '#003c30', foreground = 'White' },
+          Headline2 = { background = '#00441b', foreground = 'White' },
+          Headline3 = { background = '#084081', foreground = 'White' },
+          Dash = { background = '#0b60a1', bold = true },
+        })
       end,
       config = function()
         require('headlines').setup {
