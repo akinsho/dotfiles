66d44b320c33ac61faa7bbe104936822a3a61256
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-07 22:34:16 +0100

    chore(toggleterm): use rounded border

--- .config/nvim/lua/as/plugins/toggleterm.lua
+++ .config/nvim/lua/as/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return function()
     direction = 'horizontal',
     insert_mappings = false,
     start_in_insert = true,
-    float_opts = { border = 'curved', winblend = 3 },
+    float_opts = { border = 'rounded', winblend = 3 },
     size = function(term)
       if term.direction == 'horizontal' then
         return 15
