9d731a01b674e2e95c682decd1c9593e297dd7a6
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-21 09:05:15 +0100

    fix(settings): remove specifying directories
    nvim has since improved it's defaults so all of this work is unnecessary
    see the nvim #15433 for details

--- .config/nvim/lua/as/settings.lua
+++ .config/nvim/lua/as/settings.lua
@@ -239,19 +239,8 @@ vim.opt.shada = "!,'0,f0,<50,s10,h"
 -- BACKUP AND SWAPS {{{
 -------------------------------------------------------------------------------
 vim.opt.backup = false
-vim.opt.writebackup = false
-if fn.isdirectory(vim.o.undodir) == 0 then
-  fn.mkdir(vim.o.undodir, 'p')
-end
 vim.opt.undofile = true
 vim.opt.swapfile = false
--- The // at the end tells Vim to use the absolute path to the file to create the swap file.
--- This will ensure that swap file name is unique, so there are no collisions between files
--- with the same name from different directories.
-vim.opt.directory = fn.stdpath 'data' .. '/swap//'
-if fn.isdirectory(vim.o.directory) == 0 then
-  fn.mkdir(vim.o.directory, 'p')
-end
 --}}}
 -----------------------------------------------------------------------------//
 -- Match and search {{{1
