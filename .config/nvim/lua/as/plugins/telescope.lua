24d317605895b7fe7bb1ef7bb60ff262c9900112
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-22 12:06:58 +0100

    feat(telescope): enable dynamic preview title

--- .config/nvim/lua/as/plugins/telescope.lua
+++ .config/nvim/lua/as/plugins/telescope.lua
@@ -38,6 +38,7 @@ return function()
     defaults = {
       set_env = { ['TERM'] = vim.env.TERM },
       borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+      dynamic_preview_title = true,
       prompt_prefix = icons.misc.telescope .. ' ',
       selection_caret = icons.misc.chevron_right .. ' ',
       mappings = {
