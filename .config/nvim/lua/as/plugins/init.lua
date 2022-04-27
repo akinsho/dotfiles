ad71337d1307cbb7a6acb09b275af2fab4beff20
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-27 17:24:14 +0100

    feat(incline.nvim): re-style window name
    to look more like vsc***'s tab with each section pointing to the next

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -366,16 +366,22 @@ packer.startup {
     use {
       'b0o/incline.nvim',
       config = function()
+        -- BUG: winhighlight  is currently broken due to a bug in Neovim
+        -- @see: https://github.com/neovim/neovim/issues/18283
         require('incline').setup {
-          hide = {
-            focused_win = true,
-          },
-          -- BUG: this is currently broken due to a bug in Neovim
-          -- window = {
-          --   options = {
-          --     winhighlight = 'Normal:Search',
-          --   },
-          -- },
+          render = function(props)
+            ---@diagnostic disable-next-line: redefined-local
+            local fmt, icons = string.format, as.style.icons.misc
+            local bufname = vim.api.nvim_buf_get_name(props.buf)
+            if bufname == '' then
+              return '[No name]'
+            end
+            local parts = vim.split(vim.fn.fnamemodify(bufname, ':.'), '/')
+            local icon, _ = require('nvim-web-devicons').get_icon(bufname, nil, { default = true })
+            parts[#parts] = fmt('%s %s', icon, parts[#parts])
+            return table.concat(parts, fmt(' %s ', icons.chevron_right))
+          end,
+          hide = { focused_win = true },
         }
       end,
     }
