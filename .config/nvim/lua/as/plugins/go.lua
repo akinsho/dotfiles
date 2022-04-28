50f282e960ea8ab6b9bcc1a0486f29d095e4b739
Akin Sowemimo <akin@email.com>
2022-04-28 16:43:06 +0100

    refactor(go.nvim): remove lsp installer path override
    lsp installer now manipulates the path from lspconfig automagically

--- .config/nvim/lua/as/plugins/go.lua
+++ .config/nvim/lua/as/plugins/go.lua
@@ -1,10 +1,4 @@
 return function()
-  local install_ok, path = as.safe_require 'nvim-lsp-installer.path'
-  local gopls_cmd
-  if install_ok then
-    local install_root_dir = path.concat { vim.fn.stdpath 'data', 'lsp_servers' }
-    gopls_cmd = { install_root_dir .. '/go/gopls' }
-  end
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   local lsp_ok, cmp_nvim_lsp = as.safe_require 'cmp_nvim_lsp'
   if lsp_ok then
@@ -12,7 +6,6 @@ return function()
   end
 
   require('go').setup {
-    gopls_cmd = gopls_cmd,
     max_line_len = 100,
     goimport = 'gopls',
     icons = false,
