4dde1e4e80b7ac3207e113f34994afa06a12bb62
Akin Sowemimo <akin@email.com>
2022-04-29 12:00:45 +0100

    fix(incline.nvim): always return an icon
    set default to true where no appropriate one exists

--- .config/nvim/lua/as/plugins/incline.lua
+++ .config/nvim/lua/as/plugins/incline.lua
@@ -9,7 +9,9 @@ return function()
       end
       local directory_color = require('as.highlights').get_hl('Directory', 'fg')
       local parts = vim.split(vim.fn.fnamemodify(bufname, ':.'), '/')
-      local icon, color = require('nvim-web-devicons').get_icon_color(bufname)
+      local icon, color = require('nvim-web-devicons').get_icon_color(bufname, nil, {
+        default = true,
+      })
       local result = {}
       for idx, part in ipairs(parts) do
         if next(parts, idx) then
