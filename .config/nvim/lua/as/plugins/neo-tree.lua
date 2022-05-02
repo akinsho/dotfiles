6f005fca39b4e47492494a9d27fc76a00779c2c1
Akin Sowemimo <akin@email.com>
2022-05-02 11:20:06 +0200

    feat(neo-tree): add empty dir groupings

--- .config/nvim/lua/as/plugins/neo-tree.lua
+++ .config/nvim/lua/as/plugins/neo-tree.lua
@@ -30,6 +30,8 @@ return function()
     filesystem = {
       hijack_netrw_behavior = 'open_current',
       use_libuv_file_watcher = true,
+      group_empty_dirs = true,
+      follow_current_file = true,
       filtered_items = {
         visible = true,
         hide_dotfiles = false,
