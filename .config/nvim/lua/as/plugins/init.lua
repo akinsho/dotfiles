c28e4f9efad594b5b869d8f44a62ea1045912f3b
Akin Sowemimo <akin@email.com>
2022-05-05 15:45:09 +0200

    feat(auto-session): add sessions per git branch

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -102,6 +102,7 @@ packer.startup({
         require('auto-session').setup({
           log_level = 'error',
           auto_session_root_dir = ('%s/session/auto/'):format(vim.fn.stdpath('data')),
+          auto_session_use_git_branch = true,
         })
       end,
     })
