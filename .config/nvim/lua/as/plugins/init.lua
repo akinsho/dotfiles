cca9ce67e9ad63317be2c0e5b506cd55270313b8
Akin Sowemimo <akin@email.com>
2022-05-05 08:54:22 +0200

    style(plugins): minor key ordering change

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -787,10 +787,10 @@ packer.startup({
     use({
       'rlch/github-notifications.nvim',
       -- don't load this plugin if the gh cli is not installed
+      requires = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
       cond = function()
         return as.executable('gh')
       end,
-      requires = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
     })
     ---}}}
     --------------------------------------------------------------------------------
