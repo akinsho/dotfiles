a7ac672082111033e2b9c62bfda4b58d54f9cc12
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-05-14 09:38:45 +0200

    chore(mappings): remove URI opening block
    this is already handled by a buf read autocommand I have

--- .config/nvim/plugin/mappings.lua
+++ .config/nvim/plugin/mappings.lua
@@ -422,13 +422,9 @@ local function open_link()
   if fn.isdirectory(file) > 0 then
     return vim.cmd('edit ' .. file)
   end
+
   if file:match('https://') then
     return open(file)
-  end
-  -- Any URI with a protocol segment
-  local protocol_uri_regex = '%a*:%/%/[%a%d%#%[%]%-%%+:;!$@/?&=_.,~*()]*'
-  if file:match(protocol_uri_regex) then
-    return vim.cmd('norm! gf')
   end
 
   -- consider anything that looks like string/string a github link
