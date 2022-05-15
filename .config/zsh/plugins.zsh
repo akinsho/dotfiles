042815c1356ab3103817d58f811f082e8b16f557
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-05-15 19:21:33 +0200

    fix(zsh): ensure plugins are installed to correct dir
    I can't use the $ZDOTDIR as the command for adding a submodule even if
    it is symlinked because that path expands to somewhere that is outside
    the repository technically.

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,6 +1,8 @@
 #-------------------------------------------------------------------------------
 #  PLUGIN MANAGEMENT
 #-------------------------------------------------------------------------------
+PLUGIN_DIR="$DOTFILES/.config/zsh/plugins"
+
 function zsh_add_file() {
   [ -f "$ZDOTDIR/$1" ] && source "$ZDOTDIR/$1"
 }
@@ -16,6 +18,6 @@ function zsh_add_plugin() {
     zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh" || \
     zsh_add_file "plugins/$PLUGIN_NAME/$2.zsh"
   else
-    git submodule add "git@github.com:$1.git" "$ZDOTDIR/plugins/$PLUGIN_NAME"
+    git submodule add "git@github.com:$1.git" "$PLUGIN_DIR/$PLUGIN_NAME"
   fi
 }
