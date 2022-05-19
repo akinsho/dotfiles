553d5da42ed39d67d3c361b5146f2aa48efc5ecf
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-05-19 23:48:56 +0200

    feat(winbar): use nvim-gps to show breadcrumbs
    fallback to the directory path if there aren't any

--- .config/nvim/plugin/winbar.lua
+++ .config/nvim/plugin/winbar.lua
@@ -7,10 +7,16 @@ local devicons = require('nvim-web-devicons')
 local highlights = require('as.highlights')
 local icons = as.style.icons.misc
 
-highlights.plugin('winbar', {
-  WinbarDirectory = { inherit = 'Directory' },
-  WinbarCurrent = { bold = true, underline = true, sp = { from = 'Directory', attr = 'fg' } },
-})
+-- Count the number of windows but ignore floating ones.
+local function get_valid_wins()
+  local wins = {}
+  for _, win in ipairs(api.nvim_tabpage_list_wins(0)) do
+    if fn.win_gettype(api.nvim_win_get_number(win)) == '' then
+      table.insert(wins, win)
+    end
+  end
+  return wins
+end
 
 local function hl(str)
   return '%#' .. str .. '#'
@@ -18,8 +24,41 @@ end
 
 local hl_end = '%*'
 
+local hl_map = {
+  ['class'] = 'Class',
+  ['function'] = 'Function',
+  ['method'] = 'Method',
+  ['container'] = 'Typedef',
+  ['tag'] = 'Tag',
+  ['array'] = 'Directory',
+  ['object'] = 'Structure',
+  ['null'] = 'Comment',
+  ['boolean'] = 'Boolean',
+  ['number'] = 'Number',
+  ['string'] = 'String',
+}
+
+local function get_icon_hl(t)
+  if not t then
+    return hl('WinbarIcon')
+  end
+  local icon_type = vim.split(t, '-')[1]
+  return hl(hl_map[icon_type] or 'WinbarIcon')
+end
+
+local hls = {
+  WinbarIcon = { inherit = 'Function' },
+  WinbarDirectory = { inherit = 'Directory' },
+  WinbarCurrent = { bold = true, underline = true, sp = { from = 'Directory', attr = 'fg' } },
+}
+for name, hl_name in pairs(hl_map) do
+  hls[fmt('Winbar%sIcon', name:gsub('^%l', string.upper))] = { foreground = { from = hl_name } }
+end
+
+highlights.plugin('winbar', hls)
+
 function as.winbar.render()
-  local buf = api.nvim_win_get_buf(vim.g.statusline_winid)
+  local buf = api.nvim_get_current_buf()
   local bufname = api.nvim_buf_get_name(buf)
   local winline = ' ' -- 1 space padding
   if bufname == '' then
@@ -41,34 +80,45 @@ function as.winbar.render()
   return winline
 end
 
--- Count the number of windows but ignore floating ones.
-local function get_valid_wins()
-  local wins = {}
-  for _, win in ipairs(api.nvim_tabpage_list_wins(0)) do
-    if fn.win_gettype(api.nvim_win_get_number(win)) == '' then
-      table.insert(wins, win)
+local function breadcrumbs_available()
+  return require('nvim-gps').is_available()
+end
+
+function as.winbar.breadcrumbs()
+  local gps = require('nvim-gps')
+  local data = gps.get_data()
+  local winline = ' '
+  if vim.tbl_isempty(data) then
+    return as.winbar.render()
+  end
+  for index, item in ipairs(data) do
+    winline = winline .. get_icon_hl(item.type) .. item.icon .. hl_end .. item.text .. hl_end
+    if next(data, index) then
+      winline = winline .. hl('WinbarDirectory') .. fmt(' %s ', icons.chevron_right) .. hl_end
     end
   end
-  return wins
+  return winline
 end
 
 local excluded = { 'NeogitStatus', 'NeogitCommitMessage' }
 
 as.augroup('AttachWinbar', {
   {
-    event = { 'WinEnter', 'BufEnter', 'WinClosed', 'BufDelete' },
+    event = { 'WinEnter', 'BufEnter', 'WinClosed' },
     desc = 'Toggle winbar',
     command = function()
       local valid_wins = get_valid_wins()
       for _, win in ipairs(valid_wins) do
         local buf = api.nvim_win_get_buf(win)
+        local is_current = win == api.nvim_get_current_win()
         if
           not vim.tbl_contains(excluded, vim.bo[buf].filetype)
           and vim.bo[buf].buftype == ''
           and vim.bo[buf].filetype ~= ''
-          and #valid_wins > 1
+          and (not is_current or breadcrumbs_available())
         then
-          vim.wo[win].winbar = '%!v:lua.as.winbar.render()'
+          local str = is_current and 'breadcrumbs' or 'render'
+          vim.wo[win].winbar = '%{%v:lua.as.winbar.' .. str .. '()%}'
         else
           vim.wo[win].winbar = ''
         end
