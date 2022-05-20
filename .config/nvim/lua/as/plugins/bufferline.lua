43afdd7f0607192077a24c34812e228fc4e76e74
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-05-20 13:30:20 +0200

    chore(bufferline): remove unused custom filter

--- .config/nvim/lua/as/plugins/bufferline.lua
+++ .config/nvim/lua/as/plugins/bufferline.lua
@@ -13,23 +13,6 @@ return function()
     return #result > 0 and result or ''
   end
 
-  local function custom_filter(buf, buf_nums)
-    local logs = vim.tbl_filter(function(b)
-      return vim.bo[b].filetype == 'log'
-    end, buf_nums)
-    if vim.tbl_isempty(logs) then
-      return true
-    end
-    local tab_num = vim.fn.tabpagenr()
-    local last_tab = vim.fn.tabpagenr('$')
-    local is_log = vim.bo[buf].filetype == 'log'
-    if last_tab == 1 then
-      return true
-    end
-    -- only show log buffers in secondary tabs
-    return (tab_num == last_tab and is_log) or (tab_num ~= last_tab and not is_log)
-  end
-
   local groups = require('bufferline.groups')
 
   require('bufferline').setup({
@@ -47,7 +30,6 @@ return function()
       diagnostics = 'nvim_lsp',
       diagnostics_indicator = diagnostics_indicator,
       diagnostics_update_in_insert = false,
-      custom_filter = custom_filter,
       offsets = {
         {
           filetype = 'dbui',
