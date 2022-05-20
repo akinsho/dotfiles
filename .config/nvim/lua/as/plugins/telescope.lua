1b42e055fb8254008df8494a4d875a7d42fdefa2
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-05-20 16:49:08 +0200

    chore(telescope): ignore irrelevant files in live grep
    lock and svg count as irrelevant in this case

--- .config/nvim/lua/as/plugins/telescope.lua
+++ .config/nvim/lua/as/plugins/telescope.lua
@@ -120,7 +120,7 @@ return function()
       live_grep = {
         -- NOTE: previewing html seems to cause some stalling/blocking whilst live grepping
         -- so filter out html.
-        file_ignore_patterns = { '.git/', '%.html' },
+        file_ignore_patterns = { '.git/', '%.html', '%.svg', '%.lock' },
         on_input_filter_cb = function(prompt)
           -- AND operator for live_grep like how fzf handles spaces with wildcards in rg
           return { prompt = prompt:gsub('%s', '.*') }
