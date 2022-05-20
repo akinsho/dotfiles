96b32d92ea82639eb7425cb44e3e2d48749a032d
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-05-20 22:01:38 +0200

    fix(lsp): add highlights for info line

--- .config/nvim/lua/as/highlights.lua
+++ .config/nvim/lua/as/highlights.lua
@@ -183,6 +183,7 @@ local function general_overrides()
   local normal_bg = M.get_hl('Normal', 'bg')
   local code_block = M.alter_color(normal_bg, 30)
   local msg_area_bg = M.alter_color(normal_bg, -10)
+  local info_line = M.alter_color(L.info, -70)
   local hint_line = M.alter_color(L.hint, -70)
   local error_line = M.alter_color(L.error, -80)
   local warn_line = M.alter_color(L.warn, -80)
@@ -300,6 +301,7 @@ local function general_overrides()
       sp = L.info,
       foreground = 'none',
     },
+    DiagnosticSignInfoLine = { background = info_line },
     DiagnosticSignHintLine = { background = hint_line },
     DiagnosticSignErrorLine = { background = error_line },
     DiagnosticSignWarnLine = { background = warn_line },
