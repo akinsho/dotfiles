bf7de089ca9c5e774d3e11ea9e90e1abaa8b2274
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-05-20 14:00:31 +0200

    refactor(cmp): use fold to create highlights

--- .config/nvim/lua/as/plugins/cmp.lua
+++ .config/nvim/lua/as/plugins/cmp.lua
@@ -11,16 +11,21 @@ return function()
 
   -- Make the source information less prominent
   local faded = h.alter_color(h.get_hl('Pmenu', 'bg'), 30)
-  local kind_hls = {
-    CmpItemAbbr = { foreground = 'fg', background = 'NONE', italic = false, bold = false },
-    CmpItemMenu = { foreground = faded, italic = true, bold = false },
-    CmpItemAbbrMatch = { foreground = { from = 'Keyword' } },
-    CmpItemAbbrDeprecated = { strikethrough = true, inherit = 'Comment' },
-    CmpItemAbbrMatchFuzzy = { italic = true, foreground = { from = 'Keyword' } },
-  }
-  for key, _ in pairs(lsp_hls) do
-    kind_hls['CmpItemKind' .. key] = { foreground = { from = lsp_hls[key] } }
-  end
+
+  local kind_hls = as.fold(
+    function(accum, value, key)
+      accum['CmpItemKind' .. key] = { foreground = { from = value } }
+      return accum
+    end,
+    lsp_hls,
+    {
+      CmpItemAbbr = { foreground = 'fg', background = 'NONE', italic = false, bold = false },
+      CmpItemMenu = { foreground = faded, italic = true, bold = false },
+      CmpItemAbbrMatch = { foreground = { from = 'Keyword' } },
+      CmpItemAbbrDeprecated = { strikethrough = true, inherit = 'Comment' },
+      CmpItemAbbrMatchFuzzy = { italic = true, foreground = { from = 'Keyword' } },
+    }
+  )
 
   h.plugin('Cmp', kind_hls)
 
