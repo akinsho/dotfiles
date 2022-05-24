b143ffedf93d92279f483bc40ee764bdd0c41954
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-05-24 10:31:44 +0200

    refactor(lspconfig): simplify lspconfig stupe
    Don't use some overly complex mechanism and specify lsps as either key
    = boolean or key = function or key = table.
    
    Also remove all the custom global handlers which frankly just complicate
    things

--- .config/nvim/lua/as/plugins/lspconfig.lua
+++ .config/nvim/lua/as/plugins/lspconfig.lua
@@ -3,67 +3,21 @@ as.lsp = {}
 -----------------------------------------------------------------------------//
 -- Language servers
 -----------------------------------------------------------------------------//
-
---- This is a product of over-engineering dear reader. The LSP servers table
---- can contain a server specified in a bunch of different ways, which is arguably
---- barely more convenient. This function is a helper than then marshals things
---- into the correct shape. All this is more for fun and experimentation with lua
---- than because it's remotely necessary.
----@param name string | number
----@param config table<string, any> | function | string
----@return table<string, any>
-function as.lsp.convert_config(name, config)
-  if type(name) == 'number' then
-    name = config
-  end
-  local config_type = type(config)
-  local data = ({
-    ['string'] = function()
-      return {}
-    end,
-    ['boolean'] = function()
-      return {}
-    end,
-    ['table'] = function()
-      return config
-    end,
-    ['function'] = function()
-      return config()
-    end,
-  })[config_type]()
-  return name, data
-end
-
----Logic to (re)start installed language servers for use initialising lsps
----and restarting them on installing new ones
----@param config table<string, any>
----@return string, table<string, any>
-function as.lsp.get_server_config(config)
-  config.capabilities = config.capabilities or vim.lsp.protocol.make_client_capabilities()
-  local nvim_lsp_ok, cmp_nvim_lsp = as.safe_require('cmp_nvim_lsp')
-  if nvim_lsp_ok then
-    cmp_nvim_lsp.update_capabilities(config.capabilities)
-  end
-  return config
-end
-
 --- LSP server configs are setup dynamically as they need to be generated during
 --- startup so things like the runtimepath for lua is correctly populated
 as.lsp.servers = {
-  'sourcekit',
-  'sqls',
-  'tsserver',
-  'graphql',
-  'jsonls',
-  'bashls',
-  'vimls',
-  'yamlls',
-  'terraformls',
-  gopls = false, -- NOTE: this is loaded by it's own plugin
-  ---  NOTE: This is the secret sauce that allows reading requires and variables
-  --- between different modules in the nvim lua context
+  sourcekit = true,
+  sqls = true,
+  tsserver = true,
+  graphql = true,
+  jsonls = true,
+  bashls = true,
+  vimls = true,
+  yamlls = true,
+  terraformls = true,
+  rust_analyzer = true,
+  gopls = false,
   --- @see https://gist.github.com/folke/fe5d28423ea5380929c3f7ce674c41d8
-  --- if I ever decide to move away from lua dev then use the above
   sumneko_lua = function()
     local settings = {
       settings = {
@@ -93,9 +47,18 @@ return function()
     return
   end
   for name, config in pairs(as.lsp.servers) do
-    name, config = as.lsp.convert_config(name, config)
+    if type(config) == 'boolean' then
+      config = {}
+    elseif config and type(config) == 'function' then
+      config = config()
+    end
     if config then
-      require('lspconfig')[name].setup(as.lsp.get_server_config(config))
+      config.capabilities = config.capabilities or vim.lsp.protocol.make_client_capabilities()
+      local ok, cmp_nvim_lsp = as.safe_require('cmp_nvim_lsp')
+      if ok then
+        cmp_nvim_lsp.update_capabilities(config.capabilities)
+      end
+      require('lspconfig')[name].setup(config)
     end
   end
 end
