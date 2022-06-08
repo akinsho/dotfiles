0c72c9ac79e87fb557a1f63a6feca54f92959190
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-06-08 12:00:29 +0200

    refactor(diffview): add q mapping to each panel

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -839,10 +839,24 @@ packer.startup({
       end,
       config = function()
         require('diffview').setup({
+          hooks = {
+            diff_buf_read = function()
+              vim.opt_local.wrap = false
+              vim.opt_local.list = false
+              vim.opt_local.colorcolumn = ''
+            end,
+          },
           enhanced_diff_hl = true,
-          key_bindings = {
-            file_panel = { q = '<Cmd>DiffviewClose<CR>' },
-            view = { q = '<Cmd>DiffviewClose<CR>' },
+          keymaps = {
+            view = {
+              q = '<Cmd>DiffviewClose<CR>',
+            },
+            file_panel = {
+              q = '<Cmd>DiffviewClose<CR>',
+            },
+            file_history_panel = {
+              q = '<Cmd>DiffviewClose<CR>',
+            },
           },
         })
       end,
