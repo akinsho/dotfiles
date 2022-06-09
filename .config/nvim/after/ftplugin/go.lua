7db11e1ed7ab6123edc50afdf3015c070f39c712
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-06-09 13:22:47 +0200

    chore(go): remove textwidth

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -1,6 +1,6 @@
 vim.opt_local.spell = true
 vim.opt_local.expandtab = false
-vim.opt_local.textwidth = 100
+vim.opt_local.textwidth = 0 -- Go doesn't specify a max line length so don't force one
 vim.opt_local.softtabstop = 0
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
