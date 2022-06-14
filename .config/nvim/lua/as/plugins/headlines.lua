3f8d7c401ec7a46c098060e9b9d8a0cfd5275fd2
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-06-14 10:50:25 +0200

    chore(headlines): disable for orgmode
    Can't think of good non-distracting colours for now

--- .config/nvim/lua/as/plugins/headlines.lua
+++ .config/nvim/lua/as/plugins/headlines.lua
@@ -1,9 +1,10 @@
 local M = {}
 
 function M.setup()
+  local highlights = require('as.highlights')
   -- https://observablehq.com/@d3/color-schemes?collection=@d3/d3-scale-chromatic
   -- NOTE: this must be set in the setup function or it will crash nvim...
-  require('as.highlights').plugin('Headlines', {
+  highlights.plugin('Headlines', {
     Headline1 = { background = '#003c30', foreground = 'White' },
     Headline2 = { background = '#00441b', foreground = 'White' },
     Headline3 = { background = '#084081', foreground = 'White' },
@@ -15,6 +16,9 @@ function M.config()
   require('headlines').setup({
     markdown = {
       headline_highlights = { 'Headline1', 'Headline2', 'Headline3' },
+    },
+    org = {
+      headline_highlights = false,
     },
   })
 end
