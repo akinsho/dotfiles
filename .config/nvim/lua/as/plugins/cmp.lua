90d358d49f41478f9fbf4aefc35f592367d7c0ea
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-06-17 21:58:49 +0200

    fix(nvim-cmp): increase max width of completion window

--- .config/nvim/lua/as/plugins/cmp.lua
+++ .config/nvim/lua/as/plugins/cmp.lua
@@ -91,7 +91,7 @@ return function()
       deprecated = true,
       fields = { 'abbr', 'kind', 'menu' },
       format = function(entry, vim_item)
-        local MAX = 20
+        local MAX = math.floor(vim.o.columns / 0.4)
         vim_item.abbr = #vim_item.abbr >= MAX and string.sub(vim_item.abbr, 1, MAX) .. ellipsis
           or vim_item.abbr
         vim_item.kind = fmt('%s %s', as.style.current.lsp_icons[vim_item.kind], vim_item.kind)
