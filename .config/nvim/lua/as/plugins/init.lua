2c9e2acc044cc34f92e25d68b390a9b38eb09090
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-06-18 22:22:27 +0200

    feat(neodim): decrease alpha for dimmed variables

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -172,8 +172,10 @@ packer.startup({
       'zbirenbaum/neodim',
       config = function()
         require('neodim').setup({
+          blend_color = require('as.highlights').get_hl('Normal', 'bg'),
+          alpha = 0.45,
           hide = {
-            underline = true,
+            underline = false,
           },
         })
       end,
