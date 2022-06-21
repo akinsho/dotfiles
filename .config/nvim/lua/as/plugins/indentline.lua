a22c54cc898321f8a310024dd401bcc519b30260
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-06-21 08:26:52 +0200

    feat(indent-line): add extmark priority
    so that indent lines show above nvim-ufo fold markers

--- .config/nvim/lua/as/plugins/indentline.lua
+++ .config/nvim/lua/as/plugins/indentline.lua
@@ -3,6 +3,7 @@ return function()
     char = '│', -- ┆ ┊ 
     show_foldtext = false,
     context_char = '┃',
+    char_priority = 12,
     show_current_context = true,
     show_current_context_start = false,
     show_current_context_start_on_current_line = false,
