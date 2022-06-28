604b93f2e4062de95b829d02ca32edb2dc2674a8
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-06-28 13:59:18 +0200

    fix(nvim-ufo): don't explicitly set foldlevel
    this causes folds to be closed whenever a movement command is executed
    inside the fold

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -371,7 +371,6 @@ packer.startup({
         local bg = hl.alter_color(hl.get('Normal', 'bg'), -7)
         hl.plugin('ufo', { Folded = { bold = false, italic = false, bg = bg } })
         vim.opt.foldlevelstart = 2
-        vim.opt.foldlevel = 2
         vim.opt.sessionoptions:append('folds')
         local ufo = require('ufo')
         ufo.setup({ open_fold_hl_timeout = 0 })
