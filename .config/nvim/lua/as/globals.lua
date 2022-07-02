de53f87d9527d7a2d67ba2ebe37f73d0442a0d7d
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-03 00:28:15 +0200

    chore(globals): add warning to prevent User augroup

--- .config/nvim/lua/as/globals.lua
+++ .config/nvim/lua/as/globals.lua
@@ -238,6 +238,7 @@ end
 ---@param commands Autocommand[]
 ---@return number
 function as.augroup(name, commands)
+  assert(name ~= 'User', 'The name of an augroup CANNOT be User')
   local id = api.nvim_create_augroup(name, { clear = true })
   for _, autocmd in ipairs(commands) do
     validate_autocmd(name, autocmd)
