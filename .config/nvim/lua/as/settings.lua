cfc94f8ed70ab6f3b3cabc220e89e1fde63ef436
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-04 16:17:55 +0200

    refactor(kitty): improve active tab styling
    and remove some styling from vim't title so it's simpler

--- .config/nvim/lua/as/settings.lua
+++ .config/nvim/lua/as/settings.lua
@@ -198,7 +198,7 @@ vim.opt.guicursor = {
 function as.modified_icon()
   return vim.bo.modified and as.style.icons.misc.circle or ''
 end
-vim.opt.titlestring = ' ❐ %{fnamemodify(getcwd(), ":t")} %{v:lua.as.modified_icon()}'
+vim.opt.titlestring = '%{fnamemodify(getcwd(), ":t")} %{v:lua.as.modified_icon()}'
 vim.opt.titleold = fn.fnamemodify(vim.loop.os_getenv('SHELL'), ':t')
 vim.opt.title = true
 vim.opt.titlelen = 70
