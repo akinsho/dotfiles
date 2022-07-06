f22afc9df15be9bcd7e9154f373efab3da5a928e
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-06 23:24:58 +0200

    refactor(markdown): fix conceal queries
    these have been moved to the markdown parser and the nodes have changed
    names

---
+++ .config/nvim/after/queries/markdown/highlights.scm
@@ -0,0 +1,13 @@
+(list_item [
+  (list_marker_plus)
+  (list_marker_minus)
+  (list_marker_star)
+  (list_marker_dot)
+  (list_marker_parenthesis)
+] @conceal [
+    (task_list_marker_checked)
+    (task_list_marker_unchecked)
+](#set! conceal ""))
+
+((task_list_marker_checked) @conceal (#set! conceal ""))
+((task_list_marker_unchecked) @conceal (#set! conceal ""))
--- .config/nvim/after/queries/markdown_inline/highlights.scm
+++
@@ -1,2 +0,0 @@
-((shortcut_link) @conceal (#set! conceal "") (#eq? @conceal "[ ]"))
-((shortcut_link) @conceal (#set! conceal "") (#eq? @conceal "[x]"))
