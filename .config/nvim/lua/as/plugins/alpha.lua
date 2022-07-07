c2141caa6be98053d64a0fcaaac36bae4ae5d23f
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-07 19:35:38 +0200

    feat(alpha): add recent sessions to dashboard
    This uses autosession to list the sessions and then sort them and show
    the most recent 10

--- .config/nvim/lua/as/plugins/alpha.lua
+++ .config/nvim/lua/as/plugins/alpha.lua
@@ -5,6 +5,8 @@ return function()
   local hl = require('as.highlights')
 
   local f = string.format
+  local fn = vim.fn
+  local luv = vim.loop
   local DOTFILES = vim.env.DOTFILES
 
   local button = function(h, ...)
@@ -12,6 +14,7 @@ return function()
     local details = select(2, ...)
     local icon = details:match('[^%w%s]+') -- match non alphanumeric or space characters
     btn.opts.hl = { { h, 0, #icon + 1 } } -- add one space padding
+    btn.opts.hl_shortcut = 'Title'
     return btn
   end
 
@@ -61,6 +64,57 @@ return function()
     opts = { position = 'center', hl = 'NonText' },
   }
 
+  ---@param dir string
+  ---@param files table<string, string>
+  local function sort_sessions_by_mtime(dir, files)
+    table.sort(files, function(a, b)
+      if not a.path then return true end
+      if not b.path then return false end
+
+      local a_file, b_file = f('%s/%s', dir, a.path), f('%s/%s', dir, b.path)
+      local a_stat, b_stat = luv.fs_stat(a_file), luv.fs_stat(b_file)
+
+      if not a_stat then return true end
+      if not b_stat then return false end
+
+      local a_time, b_time = luv.fs_stat(a_file).mtime.sec, luv.fs_stat(b_file).mtime.sec
+      return a_time > b_time
+    end)
+  end
+
+  local function sessions()
+    local session = require('auto-session')
+    local files = session.get_session_files()
+    if not files or vim.tbl_isempty(files) then return end
+    files = vim.list_slice(files, 1, 10)
+
+    sort_sessions_by_mtime(session.get_root_dir():sub(1, -2), files)
+
+    local elements = as.map(function(item, key)
+      local name = item.display_name or item.path
+      if name:match("__") then
+        local parts = vim.split(name, "__")
+        name = parts[#parts]
+      else
+        name =  fn.fnamemodify(item.display_name, ':t')
+      end
+      return {
+        type = 'button',
+        val = as.truncate(name, 40),
+        on_press = function() session.RestoreSessionFromFile(item.display_name or item.path) end,
+        opts = {
+          hl = 'Directory',
+          position = 'center',
+          align_shortcut = 'right',
+          shortcut = key,
+          hl_shortcut = 'Title',
+          width = 50,
+        },
+      }
+    end, files)
+    return { type = 'group', val = elements }
+  end
+
   dashboard.section.buttons.val = {
     button('Directory', 'r', ' Restore last session', '<Cmd>RestoreSession<CR>'),
     button('Todo', 'p', ' Pick a session', '<Cmd>Autosession search<CR>'),
@@ -81,6 +135,8 @@ return function()
       installed_plugins,
       { type = 'padding', val = 2 },
       dashboard.section.buttons,
+      { type = 'text', val = 'Sessions', opts = { position = 'center', hl = 'NonText' } },
+      sessions(),
       dashboard.section.footer,
     },
     opts = { margin = 5 },
