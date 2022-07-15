25ce5cda14cc9afba7bed307378fbc25b795bcd6
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-15 13:29:57 +0200

    feat(hcl): add query to inject yaml syntax in terraform

---
+++ .config/nvim/after/queries/hcl/injections.scm
@@ -0,0 +1,7 @@
+(function_call
+  ((identifier) (#match? "yamldecode")
+  (function_arguments
+    (expression
+      (template_expr
+        (heredoc_template
+          (template_literal)  @yaml))))))
