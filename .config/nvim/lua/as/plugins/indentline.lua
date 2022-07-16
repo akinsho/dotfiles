f48d455e5c944a75e5260122bd231e1796746f8d
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-16 11:39:00 +0100

    feat(indentline): enable current context underline
    with new swanky kitty underline this looks much nicer

--- .config/nvim/lua/as/plugins/indentline.lua
+++ .config/nvim/lua/as/plugins/indentline.lua
@@ -2,10 +2,10 @@ return function()
   require('indent_blankline').setup({
     char = '│', -- ┆ ┊ 
     show_foldtext = false,
-    context_char = '┃',
+    context_char = '▎',
     char_priority = 12,
     show_current_context = true,
-    show_current_context_start = false,
+    show_current_context_start = true,
     show_current_context_start_on_current_line = false,
     show_first_indent_level = true,
     filetype_exclude = {
