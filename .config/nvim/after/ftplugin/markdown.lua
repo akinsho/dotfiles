b755313c172f98443cdde8697aa7261bf3dd5393
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-16 19:18:00 +0100

    refactor(ftplugin/markdown): convert to lua

---
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,45 @@
+vim.opt_local.spell = true
+-- no distractions in markdown files
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+
+local args = { buffer = 0 }
+
+as.onoremap('ih', [[:<c-u>execute "normal! ?^==\\+$\r:nohlsearch\rkvg_"<cr>]], args)
+as.onoremap('ah', [[:<c-u>execute "normal! ?^==\\+$\r:nohlsearch\rg_vk0"<cr>]], args)
+as.onoremap('aa', [[:<c-u>execute "normal! ?^--\\+$\r:nohlsearch\rg_vk0"<cr>]], args)
+as.onoremap('ia', [[:<c-u>execute "normal! ?^--\\+$\r:nohlsearch\rkvg_"<cr>]], args)
+
+if as.plugin_loaded('markdown-preview.nvim') then
+  as.nmap('<localleader>p', '<Plug>MarkdownPreviewToggle', args)
+end
+
+as.ftplugin_conf(
+  'cmp',
+  function(cmp)
+    cmp.setup.filetype('markdown', {
+      sources = cmp.config.sources({
+        { name = 'dictionary' },
+        { name = 'spell' },
+        { name = 'emoji' },
+      }, {
+        { name = 'buffer' },
+      }),
+    })
+  end
+)
+
+as.ftplugin_conf('nvim-surround', function(surround)
+  surround.buffer_setup({
+    delimiters = {
+      pairs = {
+        ['l'] = function()
+          return {
+            '[',
+            '](' .. vim.fn.getreg('*') .. ')',
+          }
+        end,
+      },
+    },
+  })
+end)
