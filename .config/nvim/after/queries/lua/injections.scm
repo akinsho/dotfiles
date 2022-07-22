65314c143902a03aeb17d142070c77745ea3944f
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-22 15:46:24 +0100

    feat(lua): add injection for as.augroup command strings
    highlight these as viml

---
+++ .config/nvim/after/queries/lua/injections.scm
@@ -0,0 +1,10 @@
+(function_call
+  name: (dot_index_expression
+          field: ((identifier) (#match? "augroup")))
+  arguments: (arguments
+        (table_constructor
+          (field
+            value: (table_constructor
+                     (field
+                       name: (identifier) @ident (#match? @ident "command")
+                       value: (string) @vim (#offset! @vim 0 1 0 -1)))))))
