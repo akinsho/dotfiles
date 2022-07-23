2aad896cf2152cac35801b938bf544447c0a4d4f
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-23 15:59:01 +0100

    style(highlights): use reverse for matchparen

--- .config/nvim/lua/as/highlights.lua
+++ .config/nvim/lua/as/highlights.lua
@@ -245,13 +245,7 @@ local function general_overrides()
     -- if either are specified this can lead to issues when a winhighlight is set
     SignColumn = { background = 'NONE' },
     EndOfBuffer = { background = 'NONE' },
-    MatchParen = {
-      background = 'NONE',
-      foreground = 'NONE',
-      bold = false,
-      underline = true,
-      sp = 'white',
-    },
+    MatchParen = { reverse = true },
     -----------------------------------------------------------------------------//
     -- Treesitter
     -----------------------------------------------------------------------------//
