b8839ce43a902357b7b2402cc0d5fa2661e5d8ea
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-25 10:04:58 +0100

    chore(go): remove inlay hints

--- .config/nvim/lua/as/plugins/go.lua
+++ .config/nvim/lua/as/plugins/go.lua
@@ -22,9 +22,6 @@ return function()
           dap_debug_keymap = false,
           textobjects = false,
           luasnip = true,
-          lsp_inlay_hints = {
-            enable = true,
-          },
         })
       end,
     },
