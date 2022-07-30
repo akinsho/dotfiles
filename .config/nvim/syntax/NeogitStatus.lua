321946d78d7f1a71b8e4d608e89c78a634e22623
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-07-30 23:31:12 +0100

    refactor(syntax): move neogit syntax to lua

---
+++ .config/nvim/syntax/NeogitStatus.lua
@@ -0,0 +1,8 @@
+local api = vim.api
+api.nvim_set_hl(0, 'NeogitDiffAdd', { link = 'DiffAdd' })
+api.nvim_set_hl(0, 'NeogitDiffDelete', { link = 'DiffDelete' })
+api.nvim_set_hl(0, 'NeogitDiffAddHighlight', { link = 'DiffAdd' })
+api.nvim_set_hl(0, 'NeogitDiffDeleteHighlight', { link = 'DiffDelete' })
+api.nvim_set_hl(0, 'NeogitDiffContextHighlight', { link = 'FloatNormal' })
+api.nvim_set_hl(0, 'NeogitHunkHeader', { link = 'TabLine' })
+api.nvim_set_hl(0, 'NeogitHunkHeaderHighlight', { link = 'DiffText' })
--- .config/nvim/syntax/NeogitStatus.vim
+++
@@ -1,7 +0,0 @@
-highlight link NeogitDiffAdd DiffAdd
-highlight link NeogitDiffDelete DiffDelete
-highlight link NeogitDiffAddHighlight DiffAdd
-highlight link NeogitDiffDeleteHighlight DiffDelete
-highlight link NeogitDiffContextHighlight ColorColumn
-highlight link NeogitHunkHeader TabLine
-highlight link NeogitHunkHeaderHighlight DiffText
