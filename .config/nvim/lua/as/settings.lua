d68a4e788b70e49c5734eb059a713d4356bcb86a
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-08-17 17:32:26 +0100

    revert(settings): use simpler symbols for scroll signs

--- .config/nvim/lua/as/settings.lua
+++ .config/nvim/lua/as/settings.lua
@@ -143,8 +143,8 @@ opt.list = true -- invisible chars
 opt.listchars = {
   eol = nil,
   tab = '  ', -- Alternatives: '▷▷',
-  extends = '»', -- Alternatives: … ›
-  precedes = '«', -- Alternatives: … ‹
+  extends = '›', -- Alternatives: … »
+  precedes = '‹', -- Alternatives: … «
   trail = '•', -- BULLET (U+2022, UTF-8: E2 80 A2)
 }
 -----------------------------------------------------------------------------//
