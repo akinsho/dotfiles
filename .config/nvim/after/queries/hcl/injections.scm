dcfde038f3bf1ba9c5e61373080bebdf2ade82fd
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-08-24 08:32:26 +0100

    chore(hcl): remove unused injections query
    The idea was to match against {yaml|json}decode
    and use the resulting value to set the language injection, but I don't
    think this is possible yet

--- .config/nvim/after/queries/hcl/injections.scm
+++ .config/nvim/after/queries/hcl/injections.scm
@@ -5,13 +5,3 @@
       (template_expr
         (heredoc_template
           (template_literal)  @yaml))))))
-
-; FIXME: this attemps to match the language up till "decode" and use that to
-; dynamically inject the language
-; (function_call
-;   (((identifier) @language (#lua-match? @language "(.-)decode"))
-;   (function_arguments
-;     (expression
-;       (template_expr
-;         (heredoc_template
-;           (template_literal)  (#set! "language" @language)))))))
