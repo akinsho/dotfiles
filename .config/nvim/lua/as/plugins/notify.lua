b6126e72c61690fcb18eb271c19eb9f1f8266104
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-08-29 16:40:48 +0100

    chore(notify): dogfood local PR

--- .config/nvim/lua/as/plugins/notify.lua
+++ .config/nvim/lua/as/plugins/notify.lua
@@ -16,79 +16,16 @@ return function()
 
   local notify = require('notify')
 
-  local stages_util = require('notify.stages.util')
-  local fade_in_slide_out_bottom = {
-    function(state)
-      local next_height = state.message.height + 2
-      local next_row =
-        stages_util.available_slot(state.open_windows, next_height, stages_util.DIRECTION.BOTTOM_UP)
-      if not next_row then return nil end
-      return {
-        relative = 'editor',
-        anchor = 'NE',
-        width = state.message.width,
-        height = state.message.height,
-        col = vim.opt.columns:get(),
-        row = next_row,
-        border = 'rounded',
-        style = 'minimal',
-        opacity = 0,
-      }
-    end,
-    function(state, win)
-      return {
-        opacity = { 100 },
-        col = { vim.opt.columns:get() },
-        row = {
-          stages_util.slot_after_previous(win, state.open_windows, stages_util.DIRECTION.BOTTOM_UP),
-          frequency = 3,
-          complete = function() return true end,
-        },
-      }
-    end,
-    function(state, win)
-      return {
-        col = { vim.opt.columns:get() },
-        time = true,
-        row = {
-          stages_util.slot_after_previous(win, state.open_windows, stages_util.DIRECTION.BOTTOM_UP),
-          frequency = 3,
-          complete = function() return true end,
-        },
-      }
-    end,
-    function(state, win)
-      return {
-        width = {
-          1,
-          frequency = 2.5,
-          damping = 0.9,
-          complete = function(cur_width) return cur_width < 3 end,
-        },
-        opacity = {
-          0,
-          frequency = 2,
-          complete = function(cur_opacity) return cur_opacity <= 4 end,
-        },
-        col = { vim.opt.columns:get() },
-        row = {
-          stages_util.slot_after_previous(win, state.open_windows, stages_util.DIRECTION.BOTTOM_UP),
-          frequency = 3,
-          complete = function() return true end,
-        },
-      }
-    end,
-  }
-
   notify.setup({
     timeout = 3000,
-    stages = fade_in_slide_out_bottom,
+    stages = 'slide',
+    direction = 'bottom_up',
     background_colour = 'NormalFloat',
     max_width = function() return math.floor(vim.o.columns * 0.8) end,
     max_height = function() return math.floor(vim.o.lines * 0.8) end,
     on_open = function(win)
       if api.nvim_win_is_valid(win) then
-        vim.api.nvim_win_set_config(win, { border = as.style.current.border })
+        api.nvim_win_set_config(win, { border = as.style.current.border })
       end
     end,
     render = function(...)
