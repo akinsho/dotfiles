1e0079e36cb684b6d16addf054bdfded104795ad
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-09-03 14:19:19 +0100

    feat(plugins): trial tint.nvim

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -449,6 +449,26 @@ packer.startup({
     })
 
     use({
+      'levouh/tint.nvim',
+      event = 'BufRead',
+      config = function()
+        require('tint').setup({
+          amt = -35,
+          ignore = { 'WinSeparator', 'Status.*', 'Comment', 'Beacon.*', 'Panel.*' },
+          ignorefunc = function(win_id)
+            if vim.fn.win_gettype(win_id) ~= '' then return true end
+            local buf = vim.api.nvim_win_get_buf(win_id)
+            local b = vim.bo[buf]
+            local ignore_bt = { 'terminal', 'prompt', 'nofile' }
+            local ignore_ft =
+              { 'neo-tree', 'packer', 'diff', 'toggleterm', 'Neogit.*', 'Telescope.*' }
+            return as.any(b.bt, ignore_bt) or as.any(b.ft, ignore_ft)
+          end,
+        })
+      end,
+    })
+
+    use({
       'B4mbus/todo-comments.nvim',
       config = function()
         require('todo-comments').setup()
