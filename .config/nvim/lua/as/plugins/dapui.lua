ff1a71e3b9be6d5463ecc63c00f15a849a693304
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-09-04 22:55:57 +0100

    chore(dap): don't open UI for certain filetypes
    dart being the only one currently as it runs via dap so don't need the
    space occupying UI always open

--- .config/nvim/lua/as/plugins/dapui.lua
+++ .config/nvim/lua/as/plugins/dapui.lua
@@ -5,8 +5,10 @@ return function()
   as.nnoremap('<localleader>duc', function() require('dapui').close() end, 'dap-ui: close')
   as.nnoremap('<localleader>dut', function() require('dapui').toggle() end, 'dap-ui: toggle')
 
+  local exclusions = { 'dart' }
   local dap = require('dap')
   dap.listeners.after.event_initialized['dapui_config'] = function()
+    if vim.tbl_contains(exclusions, vim.bo.filetype) then return end
     require('dapui').open()
     vim.api.nvim_exec_autocmds('User', { pattern = 'DapStarted' })
   end
