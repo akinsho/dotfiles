6447a2a488ff4348f8860fa19306d1a87afd5d08
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-09-05 23:35:50 +0100

    refactor(navic): factor out config

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -465,32 +465,7 @@ packer.startup({
     -- NOTE: Defer loading till telescope is loaded this as it implicitly loads telescope so needs to be delayed
     use({ 'stevearc/dressing.nvim', after = 'telescope.nvim', config = conf('dressing') })
 
-    use({
-      'SmiteshP/nvim-navic',
-      requires = 'neovim/nvim-lspconfig',
-      config = function()
-        vim.g.navic_silence = true
-        local highlights = require('as.highlights')
-        local s = as.style
-        local misc = s.icons.misc
-
-        require('as.highlights').plugin('navic', {
-          { NavicText = { bold = true } },
-          { NavicSeparator = { link = 'Directory' } },
-        })
-        local icons = as.map(function(icon, key)
-          highlights.set(('NavicIcons%s'):format(key), { link = s.lsp.highlights[key] })
-          return icon .. ' '
-        end, s.current.lsp_icons)
-
-        require('nvim-navic').setup({
-          icons = icons,
-          highlight = true,
-          depth_limit_indicator = misc.ellipsis,
-          separator = (' %s '):format(misc.arrow_right),
-        })
-      end,
-    })
+    use({ 'SmiteshP/nvim-navic', requires = 'neovim/nvim-lspconfig', config = conf('navic') })
 
     use({
       'kevinhwang91/nvim-ufo',
