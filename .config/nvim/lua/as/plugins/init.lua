477284031483ff72c2534a7967c6412a5df36dab
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-09-05 08:02:28 +0100

    chore(cmp-rg): use latest tag
    this fixes performance issues on large repos

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -344,7 +344,7 @@ packer.startup({
         { 'rcarriga/cmp-dap', after = 'nvim-cmp' },
         { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
         { 'dmitmel/cmp-cmdline-history', after = 'nvim-cmp' },
-        { 'lukas-reineke/cmp-rg', branch = 'bug/implement-chunk-size', after = 'nvim-cmp' },
+        { 'lukas-reineke/cmp-rg', tag = '*', after = 'nvim-cmp' },
         {
           'petertriho/cmp-git',
           after = 'nvim-cmp',
