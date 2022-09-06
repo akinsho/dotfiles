dd03a1af13917ed3bbb9d2f15394caa10d829b8a
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-09-06 09:11:08 +0100

    chore(dap): define signs separately

--- .config/nvim/lua/as/plugins/dap.lua
+++ .config/nvim/lua/as/plugins/dap.lua
@@ -10,44 +10,41 @@ function M.setup()
   local function run_last() require('dap').run_last() end
   local function toggle_breakpoint() require('dap').toggle_breakpoint() end
   local function set_breakpoint() require('dap').set_breakpoint(fn.input('Breakpoint condition: ')) end
-  as.nnoremap("<localleader>db", toggle_breakpoint, 'dap: toggle breakpoint')
-  as.nnoremap("<localleader>dB", set_breakpoint, 'dap: set breakpoint')
-  as.nnoremap("<localleader>dc", continue, 'dap: continue or start debugging')
-  as.nnoremap("<localleader>de", step_out, 'dap: step out')
-  as.nnoremap("<localleader>di", step_into, 'dap: step into')
-  as.nnoremap("<localleader>do", step_over, 'dap: step over')
-  as.nnoremap("<localleader>dl", run_last, 'dap REPL: run last')
-  as.nnoremap("<localleader>dt", repl_toggle, 'dap REPL: toggle')
+  as.nnoremap('<localleader>db', toggle_breakpoint, 'dap: toggle breakpoint')
+  as.nnoremap('<localleader>dB', set_breakpoint, 'dap: set breakpoint')
+  as.nnoremap('<localleader>dc', continue, 'dap: continue or start debugging')
+  as.nnoremap('<localleader>de', step_out, 'dap: step out')
+  as.nnoremap('<localleader>di', step_into, 'dap: step into')
+  as.nnoremap('<localleader>do', step_over, 'dap: step over')
+  as.nnoremap('<localleader>dl', run_last, 'dap REPL: run last')
+  as.nnoremap('<localleader>dt', repl_toggle, 'dap REPL: toggle')
 end
 
 function M.config()
-  local fn = vim.fn
-  local icons = as.style.icons
-
-  fn.sign_define({
-    {
-      name = 'DapBreakpoint',
-      text = icons.misc.bug,
-      texthl = 'DapBreakpoint',
-      linehl = '',
-      numhl = '',
-    },
-    {
-      name = 'DapStopped',
-      text = icons.misc.bookmark,
-      texthl = 'DapStopped',
-      linehl = '',
-      numhl = '',
-    },
-  })
+  local fn, icons = vim.fn, as.style.icons
 
   require('as.highlights').plugin('dap', {
     { DapBreakpoint = { foreground = as.style.palette.light_red } },
     { DapStopped = { foreground = as.style.palette.green } },
   })
 
+  fn.sign_define('DapBreakpoint', {
+    text = icons.misc.bug,
+    texthl = 'DapBreakpoint',
+    linehl = '',
+    numhl = '',
+  })
+
+  fn.sign_define('DapStopped', {
+    text = icons.misc.bookmark,
+    texthl = 'DapStopped',
+    linehl = '',
+    numhl = '',
+  })
+
   -- DON'T automatically stop at exceptions
   -- dap.defaults.fallback.exception_breakpoints = {}
+  require('dap').defaults.dart.exception_breakpoints = {}
   -- NOTE: the window options can be set directly in this function
 end
 
