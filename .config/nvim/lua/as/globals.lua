46ec886bad54aaaa0934333d1a9a1961bce5e03c
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-09-15 18:52:38 +0100

    refactor(globals): move toggle list functionality

--- .config/nvim/lua/as/globals.lua
+++ .config/nvim/lua/as/globals.lua
@@ -114,6 +114,31 @@ function as.is_vim_list_open()
   return false
 end
 
+---------------------------------------------------------------------------------
+-- Toggle list
+---------------------------------------------------------------------------------
+--- Utility function to toggle the location or the quickfix list
+---@param list_type '"quickfix"' | '"location"'
+---@return string?
+local function toggle_list(list_type)
+  local is_location_target = list_type == 'location'
+  local cmd = is_location_target and { 'lclose', 'lopen' } or { 'cclose', 'copen' }
+  local is_open = as.is_vim_list_open()
+  if is_open then return vim.cmd[cmd[1]]() end
+  local list = is_location_target and fn.getloclist(0) or fn.getqflist()
+  if vim.tbl_isempty(list) then
+    local msg_prefix = (is_location_target and 'Location' or 'QuickFix')
+    return vim.notify(msg_prefix .. ' List is Empty.', vim.log.levels.WARN)
+  end
+
+  local winnr = fn.winnr()
+  vim.cmd[cmd[2]]()
+  if fn.winnr() ~= winnr then vim.cmd.wincmd('p') end
+end
+
+function as.toggle_qf_list() toggle_list('quickfix') end
+function as.toggle_loc_list() toggle_list('location') end
+
 ---@param str string
 ---@param max_len integer
 ---@return string
--- .config/nvim/plugin/mappings.lua
+++ .config/nvim/plugin/mappings.lua
@@ -434,30 +434,9 @@ end
 nnoremap('gx', open_link)
 
 nnoremap('gf', '<Cmd>e <cfile><CR>')
----------------------------------------------------------------------------------
--- Toggle list
----------------------------------------------------------------------------------
---- Utility function to toggle the location or the quickfix list
----@param list_type '"quickfix"' | '"location"'
----@return string?
-function as.toggle_list(list_type)
-  local is_location_target = list_type == 'location'
-  local cmd = is_location_target and { 'lclose', 'lopen' } or { 'cclose', 'copen' }
-  local is_open = as.is_vim_list_open()
-  if is_open then return vim.cmd[cmd[1]]() end
-  local list = is_location_target and fn.getloclist(0) or fn.getqflist()
-  if vim.tbl_isempty(list) then
-    local msg_prefix = (is_location_target and 'Location' or 'QuickFix')
-    return vim.notify(msg_prefix .. ' List is Empty.', vim.log.levels.WARN)
-  end
 
-  local winnr = fn.winnr()
-  vim.cmd[cmd[2]]()
-  if fn.winnr() ~= winnr then vim.cmd.wincmd('p') end
-end
-
-nnoremap('<leader>ls', function() as.toggle_list('quickfix') end)
-nnoremap('<leader>li', function() as.toggle_list('location') end)
+nnoremap('<leader>ls', as.toggle_qf_list)
+nnoremap('<leader>li', as.toggle_loc_list)
 
 -----------------------------------------------------------------------------//
 -- Completion
