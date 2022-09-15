de30fbe7b106a1abb59decb937d1220048dfa4b4
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-09-15 16:06:19 +0100

    fix(go): make SQL injection more general

--- .config/nvim/queries/go/injections.scm
+++ .config/nvim/queries/go/injections.scm
@@ -2,7 +2,7 @@
 ; inject sql into any const string with word query in the name
 ; e.g. const query = `SELECT * FROM users WHERE name = 'John'`;
 (const_spec
-  name: (identifier) @_name (#match? @_name "[Q|q]uery$")
+  name: (identifier) @_name (#match? @_name "[Q|q]uery")
   value: (expression_list
     (raw_string_literal) @sql)
     (#offset! @sql 0 1 0 -1))
