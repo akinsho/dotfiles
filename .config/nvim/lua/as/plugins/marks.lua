6d787ee1ffa3616615d36ac3df4c786e3e25f977
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-07 09:04:05 +0000

    style(marks): show config inline

--- .config/nvim/lua/as/plugins/marks.lua
+++ .config/nvim/lua/as/plugins/marks.lua
@@ -1,23 +1,21 @@
-local function config()
-  require('as.highlights').plugin('marks', {
-    { MarkSignHL = { link = 'Directory' } },
-    { MarkSignNumHL = { link = 'Directory' } },
-  })
-  as.nnoremap('<leader>mb', '<Cmd>MarksListBuf<CR>', 'list buffer')
-  as.nnoremap('<leader>mg', '<Cmd>MarksQFListGlobal<CR>', 'list global')
-  as.nnoremap('<leader>m0', '<Cmd>BookmarksQFList 0<CR>', 'list bookmark')
+return {
+  {
+    'chentoast/marks.nvim',
+    config = function()
+      require('as.highlights').plugin('marks', {
+        { MarkSignHL = { link = 'Directory' } },
+        { MarkSignNumHL = { link = 'Directory' } },
+      })
+      as.nnoremap('<leader>mb', '<Cmd>MarksListBuf<CR>', 'list buffer')
+      as.nnoremap('<leader>mg', '<Cmd>MarksQFListGlobal<CR>', 'list global')
+      as.nnoremap('<leader>m0', '<Cmd>BookmarksQFList 0<CR>', 'list bookmark')
 
-  require('marks').setup({
-    force_write_shada = false, -- This can cause data loss
-    excluded_filetypes = { 'NeogitStatus', 'NeogitCommitMessage', 'toggleterm' },
-    bookmark_0 = {
-      sign = '⚑',
-      virt_text = '',
-    },
-    mappings = {
-      annotate = 'm?',
-    },
-  })
-end
-
-return { { 'chentoast/marks.nvim', config = config } }
+      require('marks').setup({
+        force_write_shada = false, -- This can cause data loss
+        excluded_filetypes = { 'NeogitStatus', 'NeogitCommitMessage', 'toggleterm' },
+        bookmark_0 = { sign = '⚑', virt_text = '' },
+        mappings = { annotate = 'm?' },
+      })
+    end,
+  },
+}
