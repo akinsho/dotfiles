5fcb72441871ab5669fbc4b318f5798db9101977
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-09 00:34:53 +0100

    style(neotest): add trigger keys

--- .config/nvim/lua/as/plugins/neotest.lua
+++ .config/nvim/lua/as/plugins/neotest.lua
@@ -1,60 +1,57 @@
-local function init()
-  local function open() require('neotest').output.open({ enter = true, short = false }) end
-  local function run_file() require('neotest').run.run(vim.fn.expand('%')) end
-  local function run_file_sync()
-    require('neotest').run.run({ vim.fn.expand('%'), concurrent = false })
-  end
-  local function nearest() require('neotest').run.run() end
-  local function next_failed() require('neotest').jump.prev({ status = 'failed' }) end
-  local function prev_failed() require('neotest').jump.next({ status = 'failed' }) end
-  local function toggle_summary() require('neotest').summary.toggle() end
-  local function cancel() require('neotest').run.stop({ interactive = true }) end
-
-  as.nnoremap('<localleader>ts', toggle_summary, 'neotest: toggle summary')
-  as.nnoremap('<localleader>to', open, 'neotest: output')
-  as.nnoremap('<localleader>tn', nearest, 'neotest: run')
-  as.nnoremap('<localleader>tf', run_file, 'neotest: run file')
-  as.nnoremap('<localleader>tF', run_file_sync, 'neotest: run file synchronously')
-  as.nnoremap('<localleader>tc', cancel, 'neotest: cancel')
-  as.nnoremap('[n', next_failed, 'jump to next failed test')
-  as.nnoremap(']n', prev_failed, 'jump to previous failed test')
-end
-
-local function config()
-  local namespace = vim.api.nvim_create_namespace('neotest')
-  vim.diagnostic.config({
-    virtual_text = {
-      format = function(diagnostic)
-        return diagnostic.message:gsub('\n', ' '):gsub('\t', ' '):gsub('%s+', ' '):gsub('^%s+', '')
-      end,
-    },
-  }, namespace)
-
-  require('neotest').setup({
-    discovery = { enabled = true },
-    diagnostic = {
-      enabled = true,
-    },
-    floating = { border = as.style.current.border },
-    adapters = {
-      require('neotest-plenary'),
-      require('neotest-dart')({
-        command = 'flutter',
-      }),
-      require('neotest-go')({
-        experimental = {
-          test_table = true,
-        },
-      }),
-    },
-  })
-end
+local function neotest() return require('neotest') end
+local function open() neotest().output.open({ enter = true, short = false }) end
+local function run_file() neotest().run.run(vim.fn.expand('%')) end
+local function run_file_sync() neotest().run.run({ vim.fn.expand('%'), concurrent = false }) end
+local function nearest() neotest().run.run() end
+local function next_failed() neotest().jump.prev({ status = 'failed' }) end
+local function prev_failed() neotest().jump.next({ status = 'failed' }) end
+local function toggle_summary() neotest().summary.toggle() end
+local function cancel() neotest().run.stop({ interactive = true }) end
 
 return {
   {
     'nvim-neotest/neotest',
-    init = init,
-    config = config,
+    keys = {
+      { '<localleader>ts', toggle_summary, desc = 'neotest: toggle summary' },
+      { '<localleader>to', open, desc = 'neotest: output' },
+      { '<localleader>tn', nearest, desc = 'neotest: run' },
+      { '<localleader>tf', run_file, desc = 'neotest: run file' },
+      { '<localleader>tF', run_file_sync, desc = 'neotest: run file synchronously' },
+      { '<localleader>tc', cancel, desc = 'neotest: cancel' },
+      { '[n', next_failed, desc = 'jump to next failed test' },
+      { ']n', prev_failed, desc = 'jump to previous failed test' },
+    },
+    config = function()
+      local namespace = vim.api.nvim_create_namespace('neotest')
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            return diagnostic.message
+              :gsub('\n', ' ')
+              :gsub('\t', ' ')
+              :gsub('%s+', ' ')
+              :gsub('^%s+', '')
+          end,
+        },
+      }, namespace)
+
+      require('neotest').setup({
+        discovery = { enabled = true },
+        diagnostic = { enabled = true },
+        floating = { border = as.style.current.border },
+        adapters = {
+          require('neotest-plenary'),
+          require('neotest-dart')({
+            command = 'flutter',
+          }),
+          require('neotest-go')({
+            experimental = {
+              test_table = true,
+            },
+          }),
+        },
+      })
+    end,
     dependencies = {
       { 'rcarriga/neotest-plenary' },
       { 'sidlatau/neotest-dart' },
