5021969eff6112a74a920cc981825c27c5b4f71c
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-09 23:19:56 +0100

    refactor(copilot): use lua version
    it's easier to have it interact with cmp this way

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -19,26 +19,21 @@ return {
     end,
   },
   {
-    'github/copilot.vim',
+    'zbirenbaum/copilot.lua',
     event = 'InsertEnter',
-    dependencies = { 'nvim-cmp' },
-    init = function() vim.g.copilot_no_tab_map = true end,
-    config = function()
-      as.imap('<Plug>(as-copilot-accept)', "copilot#Accept('<Tab>')", { expr = true })
-      as.inoremap('<M-]>', '<Plug>(copilot-next)')
-      as.inoremap('<M-[>', '<Plug>(copilot-previous)')
-      as.inoremap('<C-\\>', '<Cmd>vertical Copilot panel<CR>')
-      vim.g.copilot_filetypes = {
-        ['*'] = true,
-        gitcommit = false,
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        keymap = { accept = '<C-]>', next = '<M-]>', prev = '<M-[>', dismiss = '<C-\\>' },
+      },
+      filetypes = {
         NeogitCommitMessage = false,
         DressingInput = false,
         TelescopePrompt = false,
         ['neo-tree-popup'] = false,
         ['dap-repl'] = false,
-      }
-      highlight.plugin('copilot', { { CopilotSuggestion = { link = 'Comment' } } })
-    end,
+      },
+    },
   },
   'nvim-tree/nvim-web-devicons',
   {
