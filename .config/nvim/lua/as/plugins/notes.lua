f340867a7e8b9b97595eafd5eb1efa9552134c7d
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-13 10:10:40 +0000

    refactor(plugins): consolidate note taking plugin configs

---
+++ .config/nvim/lua/as/plugins/notes.lua
@@ -0,0 +1,86 @@
+local fn = vim.fn
+local fmt = string.format
+local function sync(path) return fmt('%s/%s', fn.expand('$SYNC_DIR'), path) end
+
+return {
+  {
+    'vhyrro/neorg',
+    event = 'VeryLazy',
+    build = ':Neorg sync-parsers',
+    dependencies = { 'vhyrro/neorg-telescope' },
+    opts = {
+      configure_parsers = true,
+      load = {
+        ['core.defaults'] = {},
+        ['core.integrations.telescope'] = {},
+        ['core.keybinds'] = {
+          config = {
+            default_keybinds = true,
+            neorg_leader = '<localleader>',
+            hook = function(keybinds)
+              keybinds.unmap('norg', 'n', '<C-s>')
+              keybinds.map_event('norg', 'n', '<C-x>', 'core.integrations.telescope.find_linkable')
+            end,
+          },
+        },
+        ['core.norg.completion'] = { config = { engine = 'nvim-cmp' } },
+        ['core.norg.concealer'] = {},
+        ['core.norg.dirman'] = {
+          config = {
+            workspaces = {
+              notes = sync('neorg/notes/'),
+              tasks = sync('neorg/tasks/'),
+              work = sync('neorg/work/'),
+              dotfiles = fn.expand('$DOTFILES/neorg/'),
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    'nvim-orgmode/orgmode',
+    lazy = false,
+    config = function()
+      local orgmode = require('orgmode')
+
+      as.highlight.plugin('org', {
+        { OrgDone = { fg = 'Green', bold = true } },
+        { OrgAgendaScheduled = { fg = 'Teal' } },
+      })
+
+      orgmode.setup_ts_grammar()
+      orgmode.setup({
+        org_agenda_files = { sync('org/**/*') },
+        org_default_notes_file = sync('org/refile.org'),
+        org_todo_keywords = { 'TODO(t)', 'WAITING', 'IN-PROGRESS', '|', 'DONE(d)', 'CANCELLED' },
+        org_todo_keyword_faces = {
+          ['IN-PROGRESS'] = ':foreground royalblue :weight bold',
+          CANCELLED = ':foreground darkred :weight bold',
+        },
+        org_hide_leading_stars = true,
+        org_agenda_skip_scheduled_if_done = true,
+        org_agenda_skip_deadline_if_done = true,
+        org_agenda_templates = {
+          t = { description = 'Task', template = '* TODO %?\n %u' },
+          l = { description = 'Link', template = '* %?\n%a' },
+          n = { description = 'Note', template = '* %?\n', target = sync('org/notes.org') },
+          p = {
+            description = 'Project Todo',
+            template = '* TODO %? \nSCHEDULED: %t',
+            target = sync('org/projects.org'),
+          },
+        },
+        mappings = { org = { org_global_cycle = '<leader><S-TAB>' } },
+        notifications = {
+          enabled = true,
+          repeater_reminder_time = false,
+          deadline_warning_reminder_time = true,
+          reminder_time = 10,
+          deadline_reminder = true,
+          scheduled_reminder = true,
+        },
+      })
+    end,
+  },
+}
