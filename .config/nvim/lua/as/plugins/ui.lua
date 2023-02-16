d1c6b77c748aff167b5f963c7757c0fff3887384
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-16 00:11:10 +0000

    refactor(plugins): move ufo to ui

--- .config/nvim/lua/as/plugins/ui.lua
+++ .config/nvim/lua/as/plugins/ui.lua
@@ -1,4 +1,5 @@
 local api = vim.api
+local strwidth = api.nvim_strwidth
 local highlight, border = as.highlight, as.ui.current.border
 
 return {
@@ -356,5 +357,62 @@ return {
         end,
       })
     end,
+  },
+  {
+    'kevinhwang91/nvim-ufo',
+    event = 'VeryLazy',
+    dependencies = { 'kevinhwang91/promise-async' },
+    keys = {
+      { 'zR', function() require('ufo').openAllFolds() end, 'open all folds' },
+      { 'zM', function() require('ufo').closeAllFolds() end, 'close all folds' },
+      { 'zP', function() require('ufo').peekFoldedLinesUnderCursor() end, 'preview fold' },
+    },
+    opts = {
+      open_fold_hl_timeout = 0,
+      enable_get_fold_virt_text = true,
+      fold_virt_text_handler = function(virt_text, _, end_lnum, width, truncate, ctx)
+        local result = {}
+        local padding = ''
+        local cur_width = 0
+        local suffix_width = strwidth(ctx.text)
+        local target_width = width - suffix_width
+
+        for _, chunk in ipairs(virt_text) do
+          local chunk_text = chunk[1]
+          local chunk_width = strwidth(chunk_text)
+          if target_width > cur_width + chunk_width then
+            table.insert(result, chunk)
+          else
+            chunk_text = truncate(chunk_text, target_width - cur_width)
+            local hl_group = chunk[2]
+            table.insert(result, { chunk_text, hl_group })
+            chunk_width = strwidth(chunk_text)
+            if cur_width + chunk_width < target_width then
+              padding = padding .. (' '):rep(target_width - cur_width - chunk_width)
+            end
+            break
+          end
+          cur_width = cur_width + chunk_width
+        end
+
+        local end_text = ctx.get_fold_virt_text(end_lnum)
+        -- reformat the end text to trim excess whitespace from
+        -- indentation usually the first item is indentation
+        if end_text[1] and end_text[1][1] then
+          end_text[1][1] = end_text[1][1]:gsub('[%s\t]+', '')
+        end
+
+        table.insert(result, { ' ⋯ ', 'UfoFoldedEllipsis' })
+        vim.list_extend(result, end_text)
+        table.insert(result, { padding, '' })
+
+        return result
+      end,
+      preview = { win_config = { winhighlight = 'Normal:Normal,FloatBorder:Normal' } },
+      provider_selector = function(_, filetype)
+        local ufo_ft_map = { dart = { 'lsp', 'treesitter' } }
+        return ufo_ft_map[filetype] or { 'treesitter', 'indent' }
+      end,
+    },
   },
 }
