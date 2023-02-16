0acc1f1dcb79f7205e549fc4036659f7d442c589
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-16 17:21:03 +0000

    chore(go): use opt_local for settings

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -1,12 +1,15 @@
-vim.bo.expandtab = false
-vim.bo.textwidth = 0 -- Go doesn't specify a max line length so don't force one
-vim.bo.softtabstop = 0
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
+local opt = vim.opt_local
+
+opt.expandtab = false
+opt.textwidth = 0 -- Go doesn't specify a max line length so don't force one
+opt.softtabstop = 0
+opt.tabstop = 4
+opt.shiftwidth = 4
 
 if not as then return end
 
 local with_desc = function(desc) return { buffer = 0, desc = desc } end
+
 as.nnoremap('<leader>gb', '<Cmd>GoBuild<CR>', with_desc('build'))
 as.nnoremap('<leader>gfs', '<Cmd>GoFillStruct<CR>', with_desc('fill struct'))
 as.nnoremap('<leader>gfp', '<Cmd>GoFixPlurals<CR>', with_desc('fix plurals'))
