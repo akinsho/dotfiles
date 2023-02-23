af8b54eb697c372d0d2c0d14981bf0de81aa7286
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-23 16:00:37 +0000

    chore(typescript): increase textwidth arbitrarily

--- .config/nvim/after/ftplugin/typescript.lua
+++ .config/nvim/after/ftplugin/typescript.lua
@@ -1,3 +1,7 @@
+local opt = vim.opt_local
+
+opt.textwidth = 100
+
 if pcall(require, 'typescript') then
   map('n', 'gd', 'TypescriptGoToSourceDefinition', { desc = 'typescript: go to source definition' })
 end
