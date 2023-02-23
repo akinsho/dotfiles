b56e137e379a6b4e9dc9f984749338254f699034
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-23 12:11:03 +0000

    chore(noice): show no search match errors as mini

--- .config/nvim/lua/as/plugins/noice.lua
+++ .config/nvim/lua/as/plugins/noice.lua
@@ -80,6 +80,14 @@ return {
         filter = { event = 'msg_show', min_height = 20 },
       },
       {
+        view = 'mini',
+        filter = {
+          any = {
+            { event = 'msg_show', find = '^E486:' }, -- minimise pattern not found messages
+          },
+        },
+      },
+      {
         view = 'notify',
         opts = { title = 'Error', level = L.ERROR, merge = true, replace = false },
         filter = {
