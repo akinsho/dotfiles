7b3e08588323626e09fe1af955be02e1cd030b83
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-25 18:54:39 +0000

    refactor(tools): let mason-null-ls do the driving
    It can similar to the lsp config plugin ensure tools are automagically
    configured with null ls without me needing to intervene pointlessly

--- .config/nvim/lua/as/plugins/tools.lua
+++ .config/nvim/lua/as/plugins/tools.lua
@@ -1,38 +1,20 @@
 return {
   {
-    'jay-babu/mason-null-ls.nvim',
-    dependencies = { 'mason.nvim' },
-    opts = { automatic_installation = true },
+    'jose-elias-alvarez/null-ls.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
   },
   {
-    'jose-elias-alvarez/null-ls.nvim',
+    'jay-babu/mason-null-ls.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = { 'nvim-lua/plenary.nvim', 'mason-null-ls.nvim' },
+    dependencies = { 'mason.nvim', 'null-ls.nvim' },
     config = function()
-      local null_ls = require('null-ls')
-      null_ls.setup({
-        debounce = 150,
-        sources = {
-          null_ls.builtins.diagnostics.buf,
-          null_ls.builtins.diagnostics.zsh,
-          null_ls.builtins.formatting.goimports,
-          null_ls.builtins.diagnostics.golangci_lint,
-          null_ls.builtins.formatting.pg_format,
-          null_ls.builtins.formatting.stylua.with({
-            condition = function()
-              return as.executable('stylua')
-                and not vim.tbl_isempty(vim.fs.find({ '.stylua.toml', 'stylua.toml' }, {
-                  path = vim.fn.expand('%:p'),
-                  upward = true,
-                }))
-            end,
-          }),
-          null_ls.builtins.formatting.prettier.with({
-            filetypes = { 'html', 'json', 'yaml', 'graphql', 'markdown' },
-            condition = function() return as.executable('prettier') end,
-          }),
-        },
+      require('mason-null-ls').setup({
+        automatic_setup = true,
+        automatic_installation = {},
+        ensure_installed = { 'buf', 'goimports', 'golangci_lint', 'stylua', 'prettier' },
       })
+      require('null-ls').setup()
+      require('mason-null-ls').setup_handlers()
     end,
   },
 }
