ad518906b3318c5bb72de7c3e89d3b64aa2e3690
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-02-28 21:30:34 +0000

    chore(completion): lazy load spell source

--- .config/nvim/lua/as/plugins/completion.lua
+++ .config/nvim/lua/as/plugins/completion.lua
@@ -11,7 +11,10 @@ return {
       { 'hrsh7th/cmp-cmdline', enabled = not as.nightly() },
       { 'dmitmel/cmp-cmdline-history', enabled = not as.nightly() },
       { 'hrsh7th/cmp-nvim-lsp-document-symbol', enabled = not as.nightly() },
-      { 'f3fora/cmp-spell' },
+      {
+        'f3fora/cmp-spell',
+        ft = { 'gitcommit', 'NeogitCommitMessage', 'markdown', 'norg', 'org' },
+      },
       { 'hrsh7th/cmp-path' },
       { 'hrsh7th/cmp-buffer' },
       { 'hrsh7th/cmp-emoji' },
