2f9994c63afb4ce8e477fd5fdea180d4357fbf98
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-05 14:14:38 +0000

    chore(hammerspoon): use default $HOME location
    rather than XDG_CONFIG_DIR because when installing spoons they still try
    to look in the wrong directory which is insufferable

--- macos/install.sh
+++ macos/install.sh
@@ -127,9 +127,6 @@ defaults write com.apple.ActivityMonitor ShowCategory -int 0
 # Sort Activity Monitor results by CPU usage
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
-
-# Setup hammerspoon config
-defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 ###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
