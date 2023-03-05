348387ae995074aa3c41184ebb4acc90aec244e7
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-05 14:29:38 +0000

    feat(hammerspoon): add reload config spoon

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -21,9 +21,9 @@ hs.hotkey.bind({ 'cmd', 'ctrl' }, 'a', function()
     end
   else
     hs.application.launchOrFocus('kitty')
-    app = hs.application.get('kitty')
+    hs.application.get('kitty')
   end
-
-  app:mainWindow():moveToUnit('[100,50,0,0]')
-  app:mainWindow().setShadows(false)
 end)
+
+hs.loadSpoon('ReloadConfiguration')
+spoon.ReloadConfiguration:start()
