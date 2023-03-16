8fee4f060d68197fd3f72c84987a2618adc8f567
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-16 20:53:27 +0000

    refactor(statuscolumn)!: re-use string components
    i.e. the mini plugin I'm essentially incubating to also render the
    statuscolumn.

--- .config/nvim/plugin/statuscolumn.lua
+++ .config/nvim/plugin/statuscolumn.lua
@@ -2,31 +2,26 @@ if not as or not as.has('nvim-0.9') then return end
 
 local fn, v, api, opt, optl = vim.fn, vim.v, vim.api, vim.opt, vim.opt_local
 local ui, separators = as.ui, as.ui.icons.separators
+local str = require('as.strings')
 
-local space = ' '
+local SIGN_COL_WIDTH, GIT_COL_WIDTH, space = 2, 1, ' '
 local fcs = opt.fillchars:get()
-local shade = separators.light_shade_block
-local separator = separators.left_thin_block -- '│'
-local fold_opened = fcs.foldopen
-local fold_closed = fcs.foldclose -- '▶'
-local sep_hl = '%#StatusColSep#'
+local fold_opened, fold_closed = fcs.foldopen, fcs.foldclose -- '▶'
+local shade, separator = separators.light_shade_block, separators.left_thin_block -- '│'
+local sep_hl = 'StatusColSep'
 
 ui.statuscolumn = {}
 
----@param group string
----@param text string
----@return string
-local function hl(group, text) return '%#' .. group .. '#' .. text .. '%*' end
-
-local function click(name, item) return '%@v:lua.as.ui.statuscolumn.' .. name .. '@' .. item end
-
 ---@param buf number
----@return {name:string, text:string, texthl:string}[]
+---@return {name:string, text:string, texthl:string}[][]
 local function get_signs(buf)
-  return vim.tbl_map(
-    function(sign) return fn.sign_getdefined(sign.name)[1] end,
-    fn.sign_getplaced(buf, { group = '*', lnum = v.lnum })[1].signs
-  )
+  return vim.tbl_map(function(sign)
+    local signs = fn.sign_getdefined(sign.name)
+    for _, s in ipairs(signs) do
+      if s.text then s.text = s.text:gsub('%s', '') end
+    end
+    return signs
+  end, fn.sign_getplaced(buf, { group = '*', lnum = v.lnum })[1].signs)
 end
 
 function ui.statuscolumn.toggle_breakpoint(_, _, _, mods)
@@ -47,46 +42,68 @@ local function fdm()
   return fn.foldclosed(v.lnum) == -1 and fold_closed or fold_opened
 end
 
+---@param win number
+---@return string
 local function nr(win)
   if v.virtnum < 0 then return shade end -- virtual line
   if v.virtnum > 0 then return space end -- wrapped line
   local num = vim.wo[win].relativenumber and not as.empty(v.relnum) and v.relnum or v.lnum
-  local lnum = fn.substitute(num, '\\d\\zs\\ze\\' .. '%(\\d\\d\\d\\)\\+$', ',', 'g')
+  local lnum = fn.substitute(num, '\\d\\zs\\ze\\%(\\d\\d\\d\\)\\+$', ',', 'g')
   local num_width = (vim.wo[win].numberwidth - 1) - api.nvim_strwidth(lnum)
   local padding = string.rep(space, num_width)
-  return click('toggle_breakpoint', padding .. lnum)
+  return padding .. lnum
 end
 
+---@return string
+---@return string|nil
 local function sep()
-  local separator_hl = v.virtnum >= 0 and as.empty(v.relnum) and sep_hl or ''
-  return separator_hl .. separator
+  local separator_hl = (v.virtnum >= 0 and as.empty(v.relnum)) and sep_hl or nil
+  return separator, separator_hl
+end
+
+---@param signs {name:string, text:string, texthl:string}[][]
+---@return StringComponent[] sgns non-git signs
+---@return StringComponent[] g_sgns list of git signs
+local function signs_by_type(signs)
+  local sgns, g_sgn, opts, i = {}, {}, { after = '' }, 1
+  while #sgns < SIGN_COL_WIDTH or #g_sgn < GIT_COL_WIDTH do
+    if i <= #signs then
+      local sn = signs[i]
+      if sn and sn[1].name:find('GitSign') then
+        table.insert(g_sgn, str.component(sn[1].text, sn[1].texthl, opts))
+      else
+        for _, s in ipairs(sn) do
+          table.insert(sgns, str.component(s.text, s.texthl, opts))
+        end
+      end
+    else
+      if #sgns < SIGN_COL_WIDTH then table.insert(sgns, str.spacer(1)) end
+      if #g_sgn < GIT_COL_WIDTH then table.insert(g_sgn, str.spacer(1)) end
+    end
+    i = i + 1
+  end
+  return sgns, g_sgn
 end
 
 function ui.statuscolumn.render()
   local curwin = api.nvim_get_current_win()
   local curbuf = api.nvim_win_get_buf(curwin)
-
-  local sign, git_sign
-  for _, s in ipairs(get_signs(curbuf)) do
-    if s.name:find('GitSign') then
-      git_sign = s
-    else
-      sign = s
-    end
-  end
-  local components = {
-    '%=',
-    space,
-    nr(curwin),
-    space,
-    sign and hl(sign.texthl, sign.text:gsub(space, '')) or space,
-    space,
-    git_sign and hl(git_sign.texthl, git_sign.text:gsub(space, '')) or space,
-    sep(),
-    fdm(),
-    space,
-  }
-  return table.concat(components, '')
+  local signs = get_signs(curbuf)
+  local sns, gitsign = signs_by_type(signs)
+  local statuscol = {}
+  local add = str.append(statuscol)
+  add(
+    str.separator(),
+    str.spacer(1),
+    str.component(nr(curwin), '', {
+      id = curwin,
+      click = 'v:lua.as.ui.statuscolumn.toggle_breakpoint',
+    })
+  )
+  add(unpack(sns))
+  add(unpack(gitsign))
+  add(str.component(sep()), str.component(fdm()))
+  return str.display(statuscol)
 end
 
 vim.o.statuscolumn = '%{%v:lua.as.ui.statuscolumn.render()%}'
