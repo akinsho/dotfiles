984124fd3424812b08f6fbaa731b3207f44dd26d
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-20 14:56:58 +0000

    chore: miscellaneous minor updates
    * spell word
    * add version tag
    * update lockfile

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -283,6 +283,7 @@ return {
   },
   {
     'kylechui/nvim-surround',
+    version = '*',
     keys = { { 's', mode = 'v' }, '<C-g>s', '<C-g>S', 'ys', 'yss', 'yS', 'cs', 'ds' },
     opts = { move_cursor = true, keymaps = { visual = 's' } },
   },
