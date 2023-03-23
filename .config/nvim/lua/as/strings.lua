ff6dc066248f6df16b98504a15f3ca1af1f5feae
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-23 21:42:00 +0000

    refactor(strings): simplify helpers & annotations

--- .config/nvim/lua/as/strings.lua
+++ .config/nvim/lua/as/strings.lua
@@ -6,47 +6,35 @@
 -- this way one small library to create these strings is useful.
 
 local api, L = vim.api, vim.log.levels
-local strwidth, fmt, empty = api.nvim_strwidth, string.format, as.falsy
+local strwidth, fmt, falsy = api.nvim_strwidth, string.format, as.falsy
 
 ---@alias StringComponent {component: string, length: integer, priority: integer}
 
 local M = {}
 
-local constants = { HL_END = '%*', ALIGN = '%=', END = '%<', CLICK_END = '%X' }
+local HL_END = '%*'
+local CLICK_END = '%X'
 local padding = ' '
-
-M.constants = constants
-
---- @param hl string
-local function wrap(hl)
-  if not hl then return '' end
-  return '%#' .. hl .. '#'
-end
 
 ----------------------------------------------------------------------------------------------------
 -- COMPONENTS
 ----------------------------------------------------------------------------------------------------
 
----@type fun(): StringComponent
-local function separator() return { component = constants.ALIGN, length = 0, priority = 0 } end
+---@return StringComponent
+local function separator() return { component = '%=', length = 0, priority = 0 } end
 
 ---@param func_name string
 ---@param id string
 ---@return string
 local function get_click_start(func_name, id)
   if not id then
-    vim.schedule(
-      function()
-        vim.notify_once(
-          fmt('An ID is needed to enable click handler %s to work', func_name),
-          L.ERROR,
-          { title = 'Statusline' }
-        )
-      end
-    )
+    vim.schedule(function()
+      local msg = fmt('An ID is needed to enable click handler %s to work', func_name)
+      vim.notify_once(msg, L.ERROR, { title = 'Statusline' })
+    end)
     return ''
   end
-  return '%' .. id .. '@' .. func_name .. '@'
+  return ('%%%d@%s@'):format(id, func_name)
 end
 
 --- Creates a spacer statusline component i.e. for padding
@@ -58,7 +46,7 @@ function M.spacer(size, opts)
   opts = opts or {}
   local filler = opts.filler or ' '
   local priority = opts.priority or 0
-  if not size or size == 0 then return end
+  if not size or size < 1 then return end
   local spacer = string.rep(filler, size)
   return { { { spacer } }, priority = priority, before = '', after = '' }
 end
@@ -66,34 +54,34 @@ end
 --- truncate with an ellipsis or if surrounded by quotes, replace contents of quotes with ellipsis
 --- @param str string
 --- @param max_size integer
---- @return function(string): string
+--- @return string
 local function truncate_str(str, max_size)
   if not max_size or strwidth(str) < max_size then return str end
   local match, count = str:gsub('([\'"]).*%1', '%1…%1')
   return count > 0 and match or str:sub(1, max_size - 1) .. '…'
 end
 
----@alias Subsection {[1]: string | number, [2]: string, max_size: integer?}[]
+---@alias Chunks {[1]: string | number, [2]: string, max_size: integer?}[]
 
----@param subsection Subsection
----@return string, string
-local function get_subsection(subsection)
-  if not subsection or not vim.tbl_islist(subsection) then return '', '' end
-  local section, items = {}, {}
-  for _, item in ipairs(subsection) do
+---@param chunks Chunks
+---@return string
+local function chunks_to_string(chunks)
+  if not chunks or not vim.tbl_islist(chunks) then return '' end
+  local strings = as.fold(function(acc, item, index)
     local text, hl = unpack(item)
-    if not empty(text) then
-      if type(text) == 'string' then item = tostring(item) end --- handle numeric inputs etc.
+    if not falsy(text) then
+      if type(text) == 'number' then item = tostring(item) end
       if item.max_size then text = truncate_str(text, item.max_size) end
-      table.insert(items, text)
-      table.insert(section, wrap(hl) .. text)
+      table.insert(acc, not falsy(hl) and ('%%#%s#%s'):format(hl, text) or text)
+      if #chunks == index then table.insert(acc, HL_END) end
     end
-  end
-  return table.concat(section), table.concat(items)
+    return acc
+  end, chunks)
+  return table.concat(strings)
 end
 
 --- @class ComponentOpts
---- @field [1] Subsection
+--- @field [1] Chunks
 --- @field priority number
 --- @field click string
 --- @field before string
@@ -105,7 +93,7 @@ end
 --- @return StringComponent?
 local function component(opts)
   assert(opts, 'component options are required')
-  if opts.cond ~= nil and empty(opts.cond) then return end
+  if opts.cond ~= nil and falsy(opts.cond) then return end
 
   local item = opts[1]
   assert(
@@ -116,19 +104,12 @@ local function component(opts)
   if not opts.priority then opts.priority = 10 end
   local before, after = opts.before or '', opts.after or padding
 
-  local item_str = get_subsection(item)
+  local item_str = chunks_to_string(item)
   if strwidth(item_str) == 0 then return end
 
   local click_start = opts.click and get_click_start(opts.click, tostring(opts.id)) or ''
-  local click_end = opts.click and constants.CLICK_END or ''
-  local component_str = table.concat({
-    click_start,
-    before,
-    item_str,
-    constants.HL_END,
-    after,
-    click_end,
-  })
+  local click_end = opts.click and CLICK_END or ''
+  local component_str = table.concat({ click_start, before, item_str, after, click_end })
   return {
     component = component_str,
     length = api.nvim_eval_statusline(component_str, { maxwidth = 0 }).width,
