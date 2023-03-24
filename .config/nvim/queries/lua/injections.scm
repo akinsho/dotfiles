ddaaf58870fdced52957a5205161a3cef584c32a
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-24 09:12:10 +0000

    fix(lua): update injects for as.augroup helper

--- .config/nvim/queries/lua/injections.scm
+++ .config/nvim/queries/lua/injections.scm
@@ -3,9 +3,7 @@
   name: (dot_index_expression
           field: ((identifier) (#lua-match? "augroup")))
   arguments: (arguments
-        (table_constructor
+       (table_constructor
           (field
-            value: (table_constructor
-                     (field
-                       name: (identifier) @ident (#match? @ident "command")
-                       value: (string) @vim (#offset! @vim 0 1 0 -1)))))))
+            name: (identifier) @ident (#lua-match? @ident "command")
+            value: (string) @vim (#offset! @vim 0 1 0 -1)))))
