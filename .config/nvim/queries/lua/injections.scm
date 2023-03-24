1be6808fbe406a0fc8879cfa718852d66458871d
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-24 10:51:46 +0000

    feat(lua): add injection for cmd alias

--- .config/nvim/queries/lua/injections.scm
+++ .config/nvim/queries/lua/injections.scm
@@ -5,3 +5,9 @@
   arguments: (arguments (table_constructor
                           (field name: (identifier) @_cmd  (#any-of? @_cmd "command")
                                  value: (string content: _ @injection.content) (#set! injection.language "vim")))))
+
+((function_call
+  name: (_) @__cmd
+  arguments: (arguments (string content: _ @injection.content)))
+  (#set! injection.language "vim")
+  (#any-of? @__cmd "cmd"))
