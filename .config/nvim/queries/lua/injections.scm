49cec920900aa70d29f729fb0b23d98b5cc108bf
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-24 12:33:09 +0000

    feat(lua): add more injections for lua patterns

--- .config/nvim/queries/lua/injections.scm
+++ .config/nvim/queries/lua/injections.scm
@@ -1,4 +1,6 @@
 ; extends
+; see: https://github.com/nvim-treesitter/nvim-treesitter/blob/1f087c91f5ca76a2257b855d72d371a2b5302986/queries/lua/injections.scm
+
 (function_call
   name: (dot_index_expression
           field: ((identifier) @_augroup (#any-of? @_augroup "augroup")))
@@ -11,3 +13,14 @@
   arguments: (arguments (string content: _ @injection.content)))
   (#set! injection.language "vim")
   (#any-of? @__cmd "cmd"))
+
+; Matches ("%d"):format(value)
+(function_call
+  (method_index_expression
+    table: (parenthesized_expression (string content: _ @injection.content) (#set! injection.language "luap"))
+    method: (identifier) @_method))
+
+; Matches fmt("%d", value)
+(function_call
+  name: (identifier) @_fmt (#any-of? @_fmt "fmt")
+  arguments: (arguments (string content: _ @injection.content) (#set! injection.language "luap")))
