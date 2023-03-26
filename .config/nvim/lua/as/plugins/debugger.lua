57d91250e358f3c0b7143e93e5a9810e8177d136
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-03-26 16:01:11 +0100

    refactor(debugger): improve/fix dap-ui setup

--- .config/nvim/lua/as/plugins/debugger.lua
+++ .config/nvim/lua/as/plugins/debugger.lua
@@ -1,36 +1,50 @@
 local fn = vim.fn
 local icons, highlight, border = as.ui.icons, as.highlight, as.ui.current.border
 
-local function repl_toggle() require('dap').repl.toggle(nil, 'botright split') end
-local function continue() require('dap').continue() end
-local function step_out() require('dap').step_out() end
-local function step_into() require('dap').step_into() end
-local function step_over() require('dap').step_over() end
-local function run_last() require('dap').run_last() end
-local function toggle_breakpoint() require('dap').toggle_breakpoint() end
-local function set_breakpoint() require('dap').set_breakpoint(fn.input('Breakpoint condition: ')) end
-local function log_breakpoint()
-  require('dap').set_breakpoint(nil, nil, fn.input('Log point message: '))
-end
+as.debug = { layout = { ft = { dart = 2 } } }
 
 return {
   {
     'mfussenegger/nvim-dap',
     keys = {
-      { '<localleader>dL', log_breakpoint, desc = 'dap: log breakpoint' },
-      { '<localleader>db', toggle_breakpoint, desc = 'dap: toggle breakpoint' },
-      { '<localleader>dB', set_breakpoint, desc = 'dap: set conditional breakpoint' },
-      { '<localleader>dc', continue, desc = 'dap: continue or start debugging' },
-      { '<localleader>de', step_out, desc = 'dap: step out' },
-      { '<localleader>di', step_into, desc = 'dap: step into' },
-      { '<localleader>do', step_over, desc = 'dap: step over' },
-      { '<localleader>dl', run_last, desc = 'dap REPL: run last' },
-      { '<localleader>dt', repl_toggle, desc = 'dap REPL: toggle' },
-      { '<localleader>duc', function() require('dapui').close() end, desc = 'dap ui: close' },
-      { '<localleader>dut', function() require('dapui').toggle() end, desc = 'dap ui: toggle' },
+      {
+        '<localleader>dL',
+        function() require('dap').set_breakpoint(nil, nil, fn.input('Log point message: ')) end,
+        desc = 'dap: log breakpoint',
+      },
+      {
+        '<localleader>db',
+        function() require('dap').toggle_breakpoint() end,
+        desc = 'dap: toggle breakpoint',
+      },
+      {
+        '<localleader>dB',
+        function() require('dap').set_breakpoint(fn.input('Breakpoint condition: ')) end,
+        desc = 'dap: set conditional breakpoint',
+      },
+      {
+        '<localleader>dc',
+        function() require('dap').continue() end,
+        desc = 'dap: continue or start debugging',
+      },
+      {
+        '<localleader>duc',
+        function() require('dapui').close(as.debug.layout.ft[vim.bo.ft]) end,
+        desc = 'dap ui: close',
+      },
+      {
+        '<localleader>dut',
+        function() require('dapui').toggle(as.debug.layout.ft[vim.bo.ft]) end,
+        desc = 'dap ui: toggle',
+      },
+      { '<localleader>de', function() require('dap').step_out() end, desc = 'dap: step out' },
+      { '<localleader>di', function() require('dap').step_into() end, desc = 'dap: step into' },
+      { '<localleader>do', function() require('dap').step_over() end, desc = 'dap: step over' },
+      { '<localleader>dl', function() require('dap').run_last() end, desc = 'dap REPL: run last' },
     },
     config = function()
-      require('dap') -- NOTE: Must be loaded before the signs can be tweaked
+      local dap = require('dap') -- NOTE: Must be loaded before the signs can be tweaked
+      local dapui = require('dapui')
 
       highlight.plugin('dap', {
         { DapBreakpoint = { fg = as.ui.palette.light_red } },
@@ -40,15 +54,15 @@ return {
       fn.sign_define({
         {
           name = 'DapBreakpoint',
-          text = icons.misc.bug,
           texthl = 'DapBreakpoint',
+          text = icons.misc.bug,
           linehl = '',
           numhl = '',
         },
         {
           name = 'DapStopped',
-          text = icons.misc.bookmark,
           texthl = 'DapStopped',
+          text = icons.misc.bookmark,
           linehl = '',
           numhl = '',
         },
@@ -56,24 +70,35 @@ return {
 
       -- DON'T automatically stop at exceptions
       -- dap.defaults.fallback.exception_breakpoints = {}
+
+      dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
+      dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open(as.debug.layout.ft[vim.bo.ft])
+      end
     end,
     dependencies = {
       {
-        'rcarriga/nvim-dap-ui',
-        config = function()
-          local dap = require('dap')
-          local dapui = require('dapui')
-
-          local exclusions = { 'dart' }
-
-          dapui.setup({ windows = { indent = 2 }, floating = { border = border } })
-
-          dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
-          dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
-          if not vim.tbl_contains(exclusions, vim.bo.filetype) then
-            dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
-          end
-        end,
+        {
+          'rcarriga/nvim-dap-ui',
+          opts = {
+            windows = { indent = 2 },
+            floating = { border = border },
+            layouts = {
+              {
+                elements = {
+                  { id = 'scopes', size = 0.25 },
+                  { id = 'breakpoints', size = 0.25 },
+                  { id = 'stacks', size = 0.25 },
+                  { id = 'watches', size = 0.25 },
+                },
+                position = 'left',
+                size = 20,
+              },
+              { elements = { { id = 'repl', size = 0.9 } }, position = 'bottom', size = 10 },
+            },
+          },
+        },
         { 'theHamsta/nvim-dap-virtual-text', opts = { all_frames = true } },
       },
     },
