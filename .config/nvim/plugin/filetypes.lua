f3b9021eeb86c019e0efe2ee58db4e828ebce08f
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-04-01 17:28:47 +0100

    chore(help): remove broken mappings
    These used to work and were super helpful but have broken mysteriously
    in the post treesitter world

--- .config/nvim/plugin/filetypes.lua
+++ .config/nvim/plugin/filetypes.lua
@@ -1,6 +1,6 @@
 if not as then return end
 local settings, highlight = as.filetype_settings, as.highlight
-local cmd, fn, api, env = vim.cmd, vim.fn, vim.api, vim.env
+local cmd, fn, api, env, opt_l = vim.cmd, vim.fn, vim.api, vim.env, vim.opt_local
 
 settings({
   checkhealth = {
@@ -74,18 +74,11 @@ settings({
       local opts = { buffer = args.buf }
       -- if this a vim help file create mappings to make navigation easier otherwise enable preferred editing settings
       if vim.startswith(fn.expand('%'), env.VIMRUNTIME) or vim.bo.readonly then
-        vim.opt_local.spell = false
+        opt_l.spell = false
         api.nvim_create_autocmd('BufWinEnter', { buffer = 0, command = 'wincmd L | vertical resize 80' })
-
         -- https://vim.fandom.com/wiki/Learn_to_use_help
         map('n', '<CR>', '<C-]>', opts)
         map('n', '<BS>', '<C-T>', opts)
-        -- search forwards and backwards for 'options'
-        map('n', 'o', [[/'\l\{2,\}'<CR>]], opts)
-        map('n', 'O', [[?'\l\{2,\}'<CR>]], opts)
-        -- search forwards and backwards for |subject|
-        map('n', 's', [[/\|\zs\S+\ze\|<CR>]], opts)
-        map('n', 'S', [[?\|\zs\S+\ze\|<CR>]], opts)
       else
         map('n', '<leader>ml', 'maGovim:tw=78:ts=8:noet:ft=help:norl:<esc>`a', opts)
       end
