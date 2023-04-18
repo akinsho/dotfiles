1ec95a7fc4904c84783cd2addf07d804fadcb4f0
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-04-18 17:07:45 +0100

    feat(debugger): js/ts debug adapter

--- .config/nvim/lua/as/plugins/debugger.lua
+++ .config/nvim/lua/as/plugins/debugger.lua
@@ -91,4 +91,35 @@ return {
       },
     },
   },
+  {
+    'mxsdev/nvim-dap-vscode-js',
+    ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    dependencies = { 'mfussenegger/nvim-dap' },
+    opts = {
+      adapters = { 'chrome', 'pwa-node', 'pwa-chrome', 'node-terminal', 'pwa-extensionHost' },
+      node_path = 'node',
+      debugger_cmd = { 'js-debug-adapter' },
+    },
+    config = function(_, opts)
+      require('dap-vscode-js').setup(opts)
+      for _, language in ipairs({ 'typescript', 'typescriptreact', 'javascript' }) do
+        require('dap').configurations[language] = {
+          {
+            type = 'chrome',
+            request = 'launch',
+            name = 'Launch Chrome against localhost',
+            url = 'http://localhost:3000',
+            webRoot = '${workspaceFolder}',
+          },
+          {
+            type = 'pwa-node',
+            request = 'attach',
+            name = 'Attach',
+            processId = require('dap.utils').pick_process,
+            cwd = '${workspaceFolder}',
+          },
+        }
+      end
+    end,
+  },
 }
