f6a0c730b11e1f6051de0610c534bd339285ff25
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-04-20 11:33:08 +0100

    fix(ftplugin): mappings should be bufferlocal

--- .config/nvim/after/ftplugin/dart.lua
+++ .config/nvim/after/ftplugin/dart.lua
@@ -2,14 +2,15 @@ vim.bo.syntax = ''
 vim.bo.textwidth = 100
 vim.opt_local.spell = true
 
-map('n', '<leader>cc', '<Cmd>Telescope flutter commands<CR>', { desc = 'flutter: commands' })
-map('n', '<leader>dd', '<Cmd>FlutterDevices<CR>', { desc = 'flutter: devices' })
-map('n', '<leader>de', '<Cmd>FlutterEmulators<CR>', { desc = 'flutter: emulators' })
-map('n', '<leader>do', '<Cmd>FlutterOutline<CR>', { desc = 'flutter: outline' })
-map('n', '<leader>dq', '<Cmd>FlutterQuit<CR>', { desc = 'flutter: quit' })
-map('n', '<leader>drn', '<Cmd>FlutterRun<CR>', { desc = 'flutter: server run' })
-map('n', '<leader>drs', '<Cmd>FlutterRestart<CR>', { desc = 'flutter: server restart' })
-map('n', '<leader>rn', '<Cmd>FlutterRename<CR>', { desc = 'flutter: rename class (& file)' })
+map('n', '<leader>cc', '<Cmd>Telescope flutter commands<CR>', { desc = 'flutter: commands', buffer = 0 })
+map('n', '<leader>dd', '<Cmd>FlutterDevices<CR>', { desc = 'flutter: devices', buffer = 0 })
+map('n', '<leader>de', '<Cmd>FlutterEmulators<CR>', { desc = 'flutter: emulators', buffer = 0 })
+map('n', '<leader>do', '<Cmd>FlutterOutline<CR>', { desc = 'flutter: outline', buffer = 0 })
+map('n', '<leader>dq', '<Cmd>FlutterQuit<CR>', { desc = 'flutter: quit', buffer = 0 })
+map('n', '<leader>drn', '<Cmd>FlutterRun<CR>', { desc = 'flutter: server run', buffer = 0 })
+map('n', '<leader>drs', '<Cmd>FlutterRestart<CR>', { desc = 'flutter: server restart', buffer = 0 })
+map('n', '<leader>rn', '<Cmd>FlutterRename<CR>', { desc = 'flutter: rename class (& file)', buffer = 0 })
 map('n', '<leader>db', "<Cmd>TermExec cmd='flutter pub run build_runner watch'<CR>", {
   desc = 'flutter: run code generation',
+  buffer = 0,
 })
