64b452908605955f4b0c824f04065dfbbe2222e5
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-04-25 20:22:24 +0100

    chore(ftplugin): delay removing vim syntax

--- .config/nvim/after/ftplugin/vim.lua
+++ .config/nvim/after/ftplugin/vim.lua
@@ -1,11 +1,9 @@
 local opt, fn = vim.opt_local, vim.fn
 opt.spell = true
-opt.syntax = 'off'
 
 map('n', '<leader>so', function()
   vim.cmd.source('%')
   vim.notify('Sourced ' .. fn.expand('%'))
 end)
 
--- TODO: if the syntax isn't delayed it still gets enabled
--- vim.schedule(function() opt.syntax = 'off' end)
+vim.schedule(function() opt.syntax = 'off' end) -- FIXME: if the syntax isn't delayed it still gets enabled
