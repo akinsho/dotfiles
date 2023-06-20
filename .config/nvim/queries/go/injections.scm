363f84ee29a0c083524ab80a8f2421350e275018
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-06-20 08:47:22 +0200

    fix(go): remove query variable injection

--- .config/nvim/queries/go/injections.scm
+++ .config/nvim/queries/go/injections.scm
@@ -1,11 +1,4 @@
 ; extends
-; inject sql into any const string with word query in the name
-; e.g. const query = `SELECT * FROM users WHERE name = 'John'`;
-(const_spec
-  name: (identifier) @_name (#match? @_name "[Q|q]uery")
-  value: (expression_list
-    (raw_string_literal) @sql)
-    (#offset! @sql 0 1 0 -1))
 
 ; inject sql in single line strings
 ; e.g. db.GetContext(ctx, "SELECT * FROM users WHERE name = 'John'")
