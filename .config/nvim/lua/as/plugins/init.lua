9d1afe61d852e48205dbc157afafe787d5d76956
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-07-08 16:02:12 +0100

    chore(flash): add new mappings

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -202,9 +202,6 @@ return {
     'folke/flash.nvim',
     opts = {
       modes = {
-        search = {
-          search = { trigger = ';' },
-        },
         char = {
           keys = { 'f', 'F', 't', 'T', ';' }, -- remove "," from keys
         },
@@ -214,6 +211,13 @@ return {
       { 's', function() require('flash').jump() end, mode = { 'n', 'x', 'o' } },
       { 'S', function() require('flash').treesitter() end, mode = { 'o', 'x' } },
       { 'r', function() require('flash').remote() end, mode = 'o', desc = 'Remote Flash' },
+      { '<c-s>', function() require('flash').toggle() end, mode = { 'c' }, desc = 'Toggle Flash Search' },
+      {
+        'R',
+        function() require('flash').treesitter_search() end,
+        mode = { 'o', 'x' },
+        desc = 'Flash Treesitter Search',
+      },
     },
   },
   {
