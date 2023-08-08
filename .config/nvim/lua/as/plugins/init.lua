ce2201d399786e9b5156575034b8800ac00e2c8e
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-08-08 09:31:43 +0100

    fix(smart-splits): fix description tags

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -39,10 +39,10 @@ return {
       { '<C-k>', function() require('smart-splits').move_cursor_up() end },
       { '<C-l>', function() require('smart-splits').move_cursor_right() end },
       -- swapping buffers between windows
-      { '<leader><leader>h', function() require('smart-splits').swap_buf_left() end, desc = { 'swap left' } },
-      { '<leader><leader>j', function() require('smart-splits').swap_buf_down() end, { desc = 'swap down' } },
-      { '<leader><leader>k', function() require('smart-splits').swap_buf_up() end, { desc = 'swap up' } },
-      { '<leader><leader>l', function() require('smart-splits').swap_buf_right() end, { desc = 'swap right' } },
+      { '<leader><leader>h', function() require('smart-splits').swap_buf_left() end, desc = 'swap left' },
+      { '<leader><leader>j', function() require('smart-splits').swap_buf_down() end, desc = 'swap down' },
+      { '<leader><leader>k', function() require('smart-splits').swap_buf_up() end, desc = 'swap up' },
+      { '<leader><leader>l', function() require('smart-splits').swap_buf_right() end, desc = 'swap right' },
     },
   },
   -- }}}
