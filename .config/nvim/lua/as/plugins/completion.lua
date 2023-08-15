b9d6ab7eae49a0b4e6e8a25b1247f0f2ce9afd82
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-08-15 14:18:22 +0100

    revert(copilot): trial the lua version (again)
    This reverts commit fd5f78d7cacc7f3c2b39abd42f0b3d9a982817cc.

--- .config/nvim/lua/as/plugins/completion.lua
+++ .config/nvim/lua/as/plugins/completion.lua
@@ -1,5 +1,5 @@
 local highlight, ui, k = as.highlight, as.ui, as.replace_termcodes
-local api = vim.api
+local api, fn = vim.api, vim.fn
 local border = ui.current.border
 
 return {
@@ -44,11 +44,7 @@ return {
         cmp.confirm()
       end
 
-      local function copilot()
-        local suggestion = require('copilot.suggestion')
-        if suggestion.is_visible() then return suggestion.accept() end
-        api.nvim_feedkeys(k('<Tab>'), 'n', false)
-      end
+      local function copilot() api.nvim_feedkeys(fn['copilot#Accept'](k('<Tab>')), 'n', true) end
 
       cmp.setup({
         window = {
@@ -124,28 +120,42 @@ return {
     end,
   },
   {
-    'zbirenbaum/copilot.lua',
+    'github/copilot.vim',
     event = 'InsertEnter',
     dependencies = { 'nvim-cmp' },
-    opts = {
-      panel = {
-        enabled = true,
-        auto_refresh = true,
-        keymap = { open = '<M-CR>' },
-        layout = { position = 'right', ratio = 0.4 },
-      },
-      suggestion = {
-        auto_trigger = true,
-        keymap = { accept = false, accept_word = '<M-w>', accept_line = '<M-l>' },
-      },
-      filetypes = {
+    init = function() vim.g.copilot_no_tab_map = true end,
+    config = function()
+      local function accept_word()
+        fn['copilot#Accept']('')
+        local output = fn['copilot#TextQueuedForInsertion']()
+        return fn.split(output, [[[ .]\zs]])[1]
+      end
+
+      local function accept_line()
+        fn['copilot#Accept']('')
+        local output = fn['copilot#TextQueuedForInsertion']()
+        return fn.split(output, [[[\n]\zs]])[1]
+      end
+      map('i', '<Plug>(as-copilot-accept)', "copilot#Accept('<Tab>')", {
+        expr = true,
+        remap = true,
+        silent = true,
+      })
+      map('i', '<M-]>', '<Plug>(copilot-next)', { desc = 'next suggestion' })
+      map('i', '<M-[>', '<Plug>(copilot-previous)', { desc = 'previous suggestion' })
+      map('i', '<C-\\>', '<Cmd>vertical Copilot panel<CR>', { desc = 'open copilot panel' })
+      map('i', '<M-w>', accept_word, { expr = true, remap = false, desc = 'accept word' })
+      map('i', '<M-l>', accept_line, { expr = true, remap = false, desc = 'accept line' })
+      vim.g.copilot_filetypes = {
+        ['*'] = true,
         gitcommit = false,
         NeogitCommitMessage = false,
         DressingInput = false,
         TelescopePrompt = false,
         ['neo-tree-popup'] = false,
         ['dap-repl'] = false,
-      },
-    },
+      }
+      highlight.plugin('copilot', { { CopilotSuggestion = { link = 'Comment' } } })
+    end,
   },
 }
