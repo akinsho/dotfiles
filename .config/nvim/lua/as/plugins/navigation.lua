4f572065fffe8ba2c4148db108721e1b56cf7739
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-08-31 10:46:19 +0100

    feat(marks.nvim): re-add now upstream shada fixed

--- .config/nvim/lua/as/plugins/navigation.lua
+++ .config/nvim/lua/as/plugins/navigation.lua
@@ -178,4 +178,24 @@ return {
       },
     },
   },
+  {
+    'chentoast/marks.nvim',
+    event = 'VeryLazy',
+    config = function()
+      as.highlight.plugin('marks', {
+        { MarkSignHL = { link = 'Directory' } },
+        { MarkSignNumHL = { link = 'Directory' } },
+      })
+      map('n', '<leader>mb', '<Cmd>MarksListBuf<CR>', { desc = 'list buffer' })
+      map('n', '<leader>mg', '<Cmd>MarksQFListGlobal<CR>', { desc = 'list global' })
+      map('n', '<leader>m0', '<Cmd>BookmarksQFList 0<CR>', { desc = 'list bookmark' })
+
+      require('marks').setup({
+        force_write_shada = false, -- This can cause data loss
+        excluded_filetypes = { 'NeogitStatus', 'NeogitCommitMessage', 'toggleterm' },
+        bookmark_0 = { sign = '⚑', virt_text = '' },
+        mappings = { annotate = 'm?' },
+      })
+    end,
+  },
 }
