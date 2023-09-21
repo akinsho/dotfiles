d8b6525330e6bcc431f45b8bbbba8f76cc4225fd
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-09-21 09:36:57 +0100

    chore(noice)!: disable stylize markdown overrides

--- .config/nvim/lua/as/plugins/noice.lua
+++ .config/nvim/lua/as/plugins/noice.lua
@@ -34,8 +34,8 @@ return {
         silent = true,
       },
       override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = false,
+        ['vim.lsp.util.stylize_markdown'] = false,
         ['cmp.entry.get_documentation'] = true,
       },
     },
