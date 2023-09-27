1a58db83707c4cd989bca7d730b1c80ab489bc2c
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-09-27 14:58:30 +0100

    chore(plugins): trial nvim-lint

--- .config/nvim/lua/as/plugins/tools.lua
+++ .config/nvim/lua/as/plugins/tools.lua
@@ -25,4 +25,20 @@ return {
       end
     end,
   },
+  {
+    'mfussenegger/nvim-lint',
+    event = 'BufReadPre',
+    init = function()
+      vim.api.nvim_create_autocmd({ 'TextChanged' }, {
+        callback = function() require('lint').try_lint() end,
+      })
+    end,
+    config = function()
+      require('lint').linters_by_ft = {
+        javascript = { 'eslint' },
+        markdown = { 'markdownlint' },
+        go = { 'golangcilint' },
+      }
+    end,
+  },
 }
