6e6484c5e6418ce1d2ca8e61773155723a5de891
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-11-21 11:29:51 +0000

    chore: add symbol usage plugin

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -188,6 +188,55 @@ return {
       as.command('TodoDots', ('TodoQuickFix cwd=%s keywords=TODO,FIXME'):format(vim.g.vim_dir))
     end,
   },
+  {
+    'Wansmer/symbol-usage.nvim',
+    event = 'LspAttach',
+    init = function()
+      highlight.plugin('SymbolUsage', {
+        { SymbolUsageRounding = { fg = { from = 'CursorLine', attr = 'bg' }, italic = true } },
+        { SymbolUsageContent = { bg = { from = 'CursorLine' }, fg = { from = 'Comment' } } },
+        { SymbolUsageRef = { fg = { from = 'Function' }, bg = { from = 'CursorLine' }, italic = true } },
+        { SymbolUsageDef = { fg = { from = 'Type' }, bg = { from = 'CursorLine' }, italic = true } },
+        { SymbolUsageImpl = { fg = { from = 'Keyword' }, bg = { from = 'CursorLine' }, italic = true } },
+      })
+    end,
+    config = {
+      text_format = function(symbol)
+        local res = {}
+        local ins = table.insert
+
+        local round_start = { '', 'SymbolUsageRounding' }
+        local round_end = { '', 'SymbolUsageRounding' }
+
+        if symbol.references then
+          local usage = symbol.references <= 1 and 'usage' or 'usages'
+          local num = symbol.references == 0 and 'no' or symbol.references
+          ins(res, round_start)
+          ins(res, { '󰌹 ', 'SymbolUsageRef' })
+          ins(res, { ('%s %s'):format(num, usage), 'SymbolUsageContent' })
+          ins(res, round_end)
+        end
+
+        if symbol.definition then
+          if #res > 0 then table.insert(res, { ' ', 'NonText' }) end
+          ins(res, round_start)
+          ins(res, { '󰳽 ', 'SymbolUsageDef' })
+          ins(res, { symbol.definition .. ' defs', 'SymbolUsageContent' })
+          ins(res, round_end)
+        end
+
+        if symbol.implementation then
+          if #res > 0 then table.insert(res, { ' ', 'NonText' }) end
+          ins(res, round_start)
+          ins(res, { '󰡱 ', 'SymbolUsageImpl' })
+          ins(res, { symbol.implementation .. ' impls', 'SymbolUsageContent' })
+          ins(res, round_end)
+        end
+
+        return res
+      end,
+    },
+  },
   -- }}}
   --------------------------------------------------------------------------------
   -- Utilities {{{1
