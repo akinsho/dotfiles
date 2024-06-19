a67db5335dc145ce4292f4a9c0bb4ccc6a119dc4
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2024-06-19 15:58:02 +0100

    chore: update kitty plugins

--- .config/kitty/split_window.py
+++ .config/kitty/split_window.py
@@ -1,8 +1,8 @@
-from kitty.key_encoding import KeyEvent, parse_shortcut
 from kittens.tui.handler import result_handler
+from kitty.key_encoding import KeyEvent, parse_shortcut
 
 
-def main():
+def main(args):
     pass
 
 
@@ -22,14 +22,28 @@ def encode_key_mapping(window, key_mapping):
     return window.encoded_key(event)
 
 
+def split_window(boss, direction):
+    if direction == 'up' or direction == 'down':
+        boss.launch('--cwd=current', '--location=hsplit')
+    else:
+        boss.launch('--cwd=current', '--location=vsplit')
+
+    if direction == 'up' or direction == 'left':
+        boss.active_tab.move_window(direction)
+
+
 @result_handler(no_ui=True)
 def handle_result(args, result, target_window_id, boss):
     window = boss.window_id_map.get(target_window_id)
 
+    if window is None:
+        return
+
+    direction = args[1]
     cmd = window.child.foreground_cmdline[0]
     if cmd == 'tmux':
         keymap = args[2]
         encoded = encode_key_mapping(window, keymap)
         window.write_to_child(encoded)
     else:
-        boss.active_tab.neighboring_window(args[1])
+        split_window(boss, direction)
