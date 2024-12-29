ee9248d6bab35699924143372f1bed2cd45b6bb9
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2024-12-29 16:44:15 +0000

    chore: clean up some comments

--- .config/nvim/lua/as/plugins/init.lua
+++ .config/nvim/lua/as/plugins/init.lua
@@ -116,14 +116,8 @@ return {
   },
   {
     'folke/lazydev.nvim',
-    ft = 'lua', -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      },
-    },
+    ft = 'lua',
+    opts = { library = { { path = 'luvit-meta/library', words = { 'vim%.uv' } } } },
   },
 
   {
