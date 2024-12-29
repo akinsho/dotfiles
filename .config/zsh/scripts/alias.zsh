cdebde22255ced976c6cd5d7887e85a5f63ac2ea
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2024-12-29 16:44:00 +0000

    chore(tools): add yazi and alias

--- .config/zsh/scripts/alias.zsh
+++ .config/zsh/scripts/alias.zsh
@@ -154,3 +154,12 @@ function grename() {
     git push --set-upstream origin "$2"
   fi
 }
+
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+          builtin cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
+}
