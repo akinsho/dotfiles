098b1aa7b5ed7e94cba91b98a8e41d561e42d1c3
Akin Sowemimo <akinzo@amazon.com>
2025-02-21 11:04:32 +0000

    feat(vscode): update mappings

--- .config/nvim/lua/as/vscode/init.lua
+++ .config/nvim/lua/as/vscode/init.lua
@@ -7,7 +7,7 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.undofile = true
 vim.opt.undolevels = 1000
 vim.opt.virtualedit = 'block'
-vim.opt.wildmode = 'longest:full,full' -- Command-line completion mode
+vim.opt.wildmode = 'longest:full,full'
 
 map('n', '<tab>', '<Cmd>Tabnext<CR>')
 map('n', '<S-tab>', '<Cmd>Tabprevious<CR>')
@@ -21,25 +21,20 @@ map({ 'n', 'x' }, '<C-h>', function() vscode.action('workbench.action.navigateLe
 map({ 'n', 'x' }, '<C-j>', function() vscode.action('workbench.action.navigateDown') end, { silent = true })
 map({ 'n', 'x' }, '<C-k>', function() vscode.action('workbench.action.navigateUp') end, { silent = true })
 map({ 'n', 'x' }, '<C-l>', function() vscode.action('workbench.action.navigateRight') end, { silent = true })
+map('n', '[d', function() vscode.action('editor.action.marker.prev') end, { silent = true })
+map('n', ']d', function() vscode.action('editor.action.marker.next') end, { silent = true })
+map('n', '<leader>ff', function() vscode.action('workbench.action.quickOpen') end, { silent = true })
+map('n', '<leader>ca', function() vscode.action('editor.action.quickFix') end, { silent = true })
 
 -- better up/down
 map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-map('n', '<leader>nf', function() vscode.call('workbench.action.files.newUntitledFile') end, {
-  desc = 'New file',
-})
 
-map('n', '<leader>ff', function() vscode.call('workbench.action.quickOpen') end, {
-  desc = 'Open file finder',
-})
+map('n', '<leader>nf', function() vscode.call('workbench.action.files.newUntitledFile') end, { desc = 'New file' })
 
-map('n', '<leader>fs', function() vscode.call('workbench.action.findInFiles') end, {
-  desc = 'Search in files',
-})
+map('n', '<leader>ff', function() vscode.call('workbench.action.quickOpen') end, { desc = 'Open file finder' })
 
-map('n', '[h', function() vscode.call('workbench.action.editor.previousChange') end, {
-  desc = 'Previous change',
-})
-map('n', ']h', function() vscode.call('workbench.action.editor.nextChange') end, {
-  desc = 'Next change',
-})
+map('n', '<leader>fs', function() vscode.call('workbench.action.findInFiles') end, { desc = 'Search in files', })
+
+map('n', '[h', function() vscode.call('workbench.action.editor.previousChange') end, { desc = 'Previous change' })
+map('n', ']h', function() vscode.call('workbench.action.editor.nextChange') end, { desc = 'Next change' })
