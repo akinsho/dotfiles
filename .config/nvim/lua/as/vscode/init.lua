0f63f055961955dd32eedd4553fc37dd3bdbf77b
Akin Sowemimo <akinzo@amazon.com>
2025-02-28 11:48:30 +0000

    feat(vscode): add more mappings

--- .config/nvim/lua/as/vscode/init.lua
+++ .config/nvim/lua/as/vscode/init.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 local vscode = require('vscode')
 local map = vim.keymap.set
 
@@ -9,14 +10,14 @@ vim.opt.undolevels = 1000
 vim.opt.virtualedit = 'block'
 vim.opt.wildmode = 'longest:full,full'
 
-map('n', '<tab>', '<Cmd>Tabnext<CR>')
-map('n', '<S-tab>', '<Cmd>Tabprevious<CR>')
 map('n', '<leader>qq', '<Cmd>Tabclose<CR>')
 
 -- add space line
 map('n', ']<space>', "<Cmd>put =repeat(nr2char(10), v:count1) <Bar> '[-1<CR>")
 map('n', '[<space>', "<Cmd>put! =repeat(nr2char(10), v:count1) <Bar> ']+1<CR>")
 
+map('n', '<tab>', function() vscode.action('workbench.action.nextEditor') end, { silent = true })
+map('n', '<S-tab>', function() vscode.action('workbench.action.previousEditor') end, { silent = true })
 map({ 'n', 'x' }, '<C-h>', function() vscode.action('workbench.action.navigateLeft') end, { silent = true })
 map({ 'n', 'x' }, '<C-j>', function() vscode.action('workbench.action.navigateDown') end, { silent = true })
 map({ 'n', 'x' }, '<C-k>', function() vscode.action('workbench.action.navigateUp') end, { silent = true })
@@ -26,18 +27,28 @@ map('n', ']d', function() vscode.action('editor.action.marker.next') end, { sile
 map('n', '<leader>ff', function() vscode.action('workbench.action.quickOpen') end, { silent = true })
 map('n', '<leader>ca', function() vscode.action('editor.action.quickFix') end, { silent = true })
 map('n', '<leader>gr', function() vscode.action('editor.action.rename') end, { silent = true })
+map('n', '<leader>rf', function() vscode.action('editor.action.formatDocument') end, { silent = true })
+map('v', '<leader>rf', function() vscode.action('editor.action.formatSelection') end, { silent = true })
 map('n', 'gr', function() vscode.action('editor.action.goToReferences') end, { silent = true })
+map('n', '<localleader>gs', function() vscode.action('magit.status') end, { silent = true })
+
+map('n', '<leader>sv', function()
+  vscode.action('vscode-neovim.restart')
+  vim.notify('Restarting Neovim', vim.log.levels.INFO, { title = 'vscode' })
+end, { silent = true })
+
+map('n', '<leader>ev', function()
+  vim.cmd('edit $HOME/.config/nvim/init.lua')
+  vim.notify('Opening NVIM config file', vim.log.levels.INFO, { title = 'vscode' })
+end, { silent = true })
 
 -- better up/down
 map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 map('n', '<leader>nf', function() vscode.call('workbench.action.files.newUntitledFile') end, { desc = 'New file' })
-
 map('n', '<leader>ff', function() vscode.call('workbench.action.quickOpen') end, { desc = 'Open file finder' })
-
 map('n', '<leader>fs', function() vscode.call('workbench.action.findInFiles') end, { desc = 'Search in files' })
-
 map('n', '[h', function() vscode.call('workbench.action.editor.previousChange') end, { desc = 'Previous change' })
 map('n', ']h', function() vscode.call('workbench.action.editor.nextChange') end, { desc = 'Next change' })
 
